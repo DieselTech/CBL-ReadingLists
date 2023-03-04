62b4b8212dab2e07ed9faa1fd4b4672dee29bbfd
DieselTech <30128380+DieselTech@users.noreply.github.com>
2023-03-04 00:40:36 -0500

    additions
    Adding files that were found.

---
+++ Other/Dynamite/Game of Thrones.cbl
@@ -0,0 +1,157 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>Game of Thrones</Name>
+  <Books>
+    <Book Series="The Hedge Knight: The Graphic Novel" Number="1" Volume="2013" Year="2013">
+      <Id>fe25e356-1fb9-4a03-b38d-93086d07a9a4</Id>
+    </Book>
+    <Book Series="The Sworn Sword: The Graphic Novel" Number="1" Volume="2014" Year="2014">
+      <Id>dbbb2bba-19bb-4554-9cb7-4747963c6c33</Id>
+    </Book>
+    <Book Series="The Mystery Knight" Number="1" Volume="2017" Year="2017">
+      <Id>498467df-070b-42cb-bf88-f03562c6d863</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="1" Volume="2011" Year="2011">
+      <Id>51b16d47-e729-4fd7-86d3-e62230562c0d</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="2" Volume="2011" Year="2011">
+      <Id>1c985f5a-ea2e-430c-8fc6-87b487009fc4</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="3" Volume="2011" Year="2011">
+      <Id>e3239a28-36aa-45c3-9791-888a1d510366</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="4" Volume="2011" Year="2011">
+      <Id>c6002b37-39b3-4e3f-ba40-637ca46b7a93</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="5" Volume="2011" Year="2012">
+      <Id>86181ba0-320b-40e0-9c84-a3352674d6e1</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="6" Volume="2011" Year="2012">
+      <Id>4d01501e-1e83-4f18-be9c-c6c1c08bfa1d</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="7" Volume="2011" Year="2012">
+      <Id>6850e87e-049f-44ec-8d35-a50a86a12bd0</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="8" Volume="2011" Year="2012">
+      <Id>c359d360-2b89-4f1a-b3be-46e582d5dc1f</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="9" Volume="2011" Year="2012">
+      <Id>9918f447-fe80-4e77-9490-a5c19548b527</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="10" Volume="2011" Year="2012">
+      <Id>981a78ec-a08d-4d5d-b38d-7df1c87cc298</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="11" Volume="2011" Year="2012">
+      <Id>05ddff71-d86e-435d-a8b2-a47db4b39824</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="12" Volume="2011" Year="2013">
+      <Id>5c06c96e-f463-4528-b86b-a16d7a2ea78a</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="13" Volume="2011" Year="2013">
+      <Id>e0426512-7923-492e-97ec-c3587716a4a8</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="14" Volume="2011" Year="2013">
+      <Id>880f5890-0fa7-4951-bfcf-790c33b3694a</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="15" Volume="2011" Year="2013">
+      <Id>d70242c8-bf2f-45be-9be4-8a46db981959</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="16" Volume="2011" Year="2013">
+      <Id>3a67fcbd-456f-4a5a-8e39-f2962df45e88</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="17" Volume="2011" Year="2013">
+      <Id>5f5be581-844e-4e26-933d-d407505c4e60</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="18" Volume="2011" Year="2014">
+      <Id>77e482bc-093a-4858-a91c-dfb8c979a785</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="19" Volume="2011" Year="2014">
+      <Id>f376b4a1-6809-4d75-a0af-dbb55ccc2ad4</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="20" Volume="2011" Year="2014">
+      <Id>c8266de6-8315-422d-a221-10f0a3db4c76</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="21" Volume="2011" Year="2014">
+      <Id>f07f7e91-c909-41ec-a452-c2ea33f8e811</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="22" Volume="2011" Year="2014">
+      <Id>19e51825-d195-40c4-ade6-aea9b2026c3e</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="23" Volume="2011" Year="2014">
+      <Id>b16d58e2-8b6f-48c9-8a6b-109a346584f5</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Game of Thrones" Number="24" Volume="2011" Year="2015">
+      <Id>0d081f04-7681-473b-83fd-a814ac2cb3d3</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="1" Volume="2017" Year="2017">
+      <Id>fcf85a12-4580-4162-915e-78bfe8d1b7b6</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="2" Volume="2017" Year="2017">
+      <Id>f63396cb-7e4f-4e8e-82fa-86eebff1ffdf</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="3" Volume="2017" Year="2017">
+      <Id>f19e6546-4e3f-433d-98bb-61c25544aded</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="4" Volume="2017" Year="2017">
+      <Id>cbf6fae0-01a7-42bf-a6f5-b937630e8341</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="5" Volume="2017" Year="2017">
+      <Id>a221e670-b798-4c51-b10d-9f38c26c0570</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="6" Volume="2017" Year="2017">
+      <Id>d9335155-a200-437f-9adf-660981a0288b</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="7" Volume="2017" Year="2017">
+      <Id>245f106c-a5f4-4a21-8639-805849ae14db</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="8" Volume="2017" Year="2018">
+      <Id>7620686f-6546-45e8-9857-856bf0f13575</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="9" Volume="2017" Year="2018">
+      <Id>e126fc56-e308-4bcb-ae5f-aeab40e460d8</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="10" Volume="2017" Year="2018">
+      <Id>ffc832bc-b18e-4f1b-b5c1-0eb67203aec6</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="11" Volume="2017" Year="2018">
+      <Id>bdb4f79e-48c0-42d3-8f96-f37ce80ef959</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="12" Volume="2017" Year="2018">
+      <Id>f310a4f6-7952-4fa4-a518-ee9f053ae500</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="13" Volume="2017" Year="2018">
+      <Id>da33d257-0346-4513-9715-9a1af5256a5f</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="14" Volume="2017" Year="2018">
+      <Id>e7e1fb1a-24ca-40f1-aeaf-df95eb8c19b7</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="15" Volume="2017" Year="2019">
+      <Id>17be7650-0ba5-4f6e-b046-b000776c0dc8</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="16" Volume="2017" Year="2019">
+      <Id>d18f50d6-1bc3-4494-ac77-2c3c6281ab0f</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="1" Volume="2020" Year="2020">
+      <Id>086c7dda-b48c-47f2-83d4-003be4cb610d</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="2" Volume="2020" Year="2020">
+      <Id>321c7793-a235-4845-907a-593a6a52ae26</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="3" Volume="2020" Year="2020">
+      <Id>6acd0299-a1b6-4e47-a925-7ce2c94eb0f6</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="4" Volume="2020" Year="2020">
+      <Id>7ab5f1b1-eea0-4118-ac2c-ae0124fb6606</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="5" Volume="2020" Year="2020">
+      <Id>15e484a6-618c-4689-a357-378195f80978</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="6" Volume="2020" Year="2020">
+      <Id>f0571f75-5e2a-4286-a17a-fd950376ca00</Id>
+    </Book>
+    <Book Series="George R.R. Martin's A Clash of Kings" Number="7" Volume="2020" Year="2020">
+      <Id>25aaa5b1-e208-40f1-a7cb-fdaeb2516cff</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
---
+++ Other/Dynamite/Vampirella.cbl
@@ -0,0 +1,94 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>Vampirella</Name>
+  <Books>
+    <Book Series="Vampirella: The Essential Warren Years" Number="1" Volume="2014" Year="2014">
+      <Id>3e0ce081-35a8-425f-9928-3e3cf9a69703</Id>
+    </Book>
+    <Book Series="Vampirella: Masters Series" Number="1" Volume="2010" Year="2010">
+      <Id>606a3477-208c-43e5-b0f8-e9b51ea648ed</Id>
+    </Book>
+    <Book Series="Vampirella: Masters Series" Number="2" Volume="2010" Year="2010">
+      <Id>351ed449-3925-4c79-adc8-4bd915748dea</Id>
+    </Book>
+    <Book Series="Vampirella: Masters Series" Number="3" Volume="2010" Year="2011">
+      <Id>74c02cf7-cd6d-40b5-8dc8-8af6bbecd6c0</Id>
+    </Book>
+    <Book Series="Vampirella: Masters Series" Number="4" Volume="2010" Year="2011">
+      <Id>29c4910e-94e9-4d1d-9d71-17927a4f8f4f</Id>
+    </Book>
+    <Book Series="Vampirella: Masters Series" Number="5" Volume="2010" Year="2011">
+      <Id>35cdff97-22de-499c-9ad5-29c8764d260b</Id>
+    </Book>
+    <Book Series="Vampirella: Masters Series" Number="6" Volume="2010" Year="2011">
+      <Id>1f85bbfb-365f-4315-b160-02d08648cde8</Id>
+    </Book>
+    <Book Series="Vampirella: Masters Series" Number="7" Volume="2010" Year="2012">
+      <Id>c3daa252-0939-4bdc-9395-22669b25ead1</Id>
+    </Book>
+    <Book Series="Vampirella: Masters Series" Number="8" Volume="2010" Year="2013">
+      <Id>15f03b54-7f53-43e6-9f0c-c0abb9d78afd</Id>
+    </Book>
+    <Book Series="Vampirella: The Dynamite Years Omnibus" Number="1" Volume="2017" Year="2017">
+      <Id>08d63e16-4101-473f-b9d1-7620b5dd5e64</Id>
+    </Book>
+    <Book Series="Vampirella: The Dynamite Years Omnibus" Number="2" Volume="2017" Year="2018">
+      <Id>274c23a6-46f2-45f9-a438-f8f29fa5e826</Id>
+    </Book>
+    <Book Series="Vampirella: The Dynamite Years Omnibus" Number="3" Volume="2017" Year="2018">
+      <Id>40190c36-d783-4dc6-81ca-179763775480</Id>
+    </Book>
+    <Book Series="Vampirella: Feary Tales" Number="1" Volume="2015" Year="2015">
+      <Id>674c7db9-6eb9-49d3-a8e6-ba3811e23651</Id>
+    </Book>
+    <Book Series="Vampirella Strikes" Number="1" Volume="2013" Year="2013">
+      <Id>c543efe6-1d5e-45b1-8d50-354cb693854d</Id>
+    </Book>
+    <Book Series="Legenderry: Vampirella" Number="1" Volume="2015" Year="2015" Format="Trade Paper Back">
+      <Id>f242ee46-4037-4c42-8dd7-4b0de7e3e3ad</Id>
+    </Book>
+    <Book Series="Dawn/Vampirella" Number="1" Volume="2014" Year="2014">
+      <Id>3402b36f-261f-414b-a758-d54a720e896b</Id>
+    </Book>
+    <Book Series="Dawn/Vampirella" Number="2" Volume="2014" Year="2014">
+      <Id>7f44f78b-e60b-46bc-99a8-16bf2484bd96</Id>
+    </Book>
+    <Book Series="Dawn/Vampirella" Number="3" Volume="2014" Year="2015">
+      <Id>409faefa-5cdb-45de-8322-6afec3cbd8d3</Id>
+    </Book>
+    <Book Series="Dawn/Vampirella" Number="4" Volume="2014" Year="2015">
+      <Id>60dbd683-a657-41e3-9f90-a87e11b8cbc8</Id>
+    </Book>
+    <Book Series="Dawn/Vampirella" Number="5" Volume="2014" Year="2015">
+      <Id>22130fc9-0f74-4883-a1d2-6e17b2e81f07</Id>
+    </Book>
+    <Book Series="Vampirella: Hollywood Horror" Number="1" Volume="2016" Year="2016">
+      <Id>57e7d210-a3e8-4b67-ad29-9c785d434df7</Id>
+    </Book>
+    <Book Series="Vampirella: Forbidden Fruit" Number="1" Volume="2017" Year="2017">
+      <Id>41c038e0-56cb-4e68-b2a0-ef5070ad3530</Id>
+    </Book>
+    <Book Series="Vampirella: The God You Know" Number="1" Volume="2018" Year="2018">
+      <Id>95829a71-fdbd-43bf-aacd-5d664aebd943</Id>
+    </Book>
+    <Book Series="Aliens/Vampirella" Number="1" Volume="2016" Year="2016">
+      <Id>320f36ed-d34b-4fa0-b1f9-f9cfeaa53095</Id>
+    </Book>
+    <Book Series="Hack/Slash vs. Vampirella" Number="1" Volume="2017" Year="2017">
+      <Id>8014cc84-3240-49e2-b827-58e5d59b4b8d</Id>
+    </Book>
+    <Book Series="Hack/Slash vs. Vampirella" Number="2" Volume="2017" Year="2017">
+      <Id>c8b8b075-7bc0-478b-9f5c-f74e778dac7c</Id>
+    </Book>
+    <Book Series="Hack/Slash vs. Vampirella" Number="3" Volume="2017" Year="2017">
+      <Id>8c8db2c0-3e17-43ff-b667-31c46edcafa4</Id>
+    </Book>
+    <Book Series="Hack/Slash vs. Vampirella" Number="4" Volume="2017" Year="2018">
+      <Id>350c0886-edc9-49a7-a8b3-a7743ea5083c</Id>
+    </Book>
+    <Book Series="Hack/Slash vs. Vampirella" Number="5" Volume="2017" Year="2018">
+      <Id>3cb35d1f-eca0-4027-a6b9-2e2faddad459</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
