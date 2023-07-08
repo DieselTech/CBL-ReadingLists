402718907bd78ce287ef82e3926c0620159cf3bf
jakem742 <jakemad@gmail.com>
2023-07-08 22:48:42 +1000

    First official reading lists added

---
+++ Marvel/Events/Official/[Marvel] (2016) Inhumans vs. X-Men (Official).cbl
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Name>[Marvel] Inhumans vs. X-Men (Official)</Name>
+    <NumIssues>20</NumIssues>
+    <Books>
+        <Book Series="IvX" Number="0" Volume="2016" Year="2017">
+            <Database Name="cv" Series="96144" Issue="562597" />
+        </Book>
+        <Book Series="IvX" Number="1" Volume="2016" Year="2017">
+            <Database Name="cv" Series="96144" Issue="566713" />
+        </Book>
+        <Book Series="Extraordinary X-Men" Number="17" Volume="2015" Year="2017">
+            <Database Name="cv" Series="85756" Issue="571665" />
+        </Book>
+        <Book Series="Uncanny X-Men" Number="16" Volume="2016" Year="2017">
+            <Database Name="cv" Series="87190" Issue="569354" />
+        </Book>
+        <Book Series="IvX" Number="2" Volume="2016" Year="2017">
+            <Database Name="cv" Series="96144" Issue="575870" />
+        </Book>
+        <Book Series="IvX" Number="3" Volume="2016" Year="2017">
+            <Database Name="cv" Series="96144" Issue="578465" />
+        </Book>
+        <Book Series="All-New X-Men" Number="17" Volume="2015" Year="2017">
+            <Database Name="cv" Series="86334" Issue="576612" />
+        </Book>
+        <Book Series="Deadpool &#38; The Mercs For Money" Number="7" Volume="2016" Year="2017">
+            <Database Name="cv" Series="92360" Issue="576620" />
+        </Book>
+        <Book Series="Extraordinary X-Men" Number="18" Volume="2015" Year="2017">
+            <Database Name="cv" Series="85756" Issue="578459" />
+        </Book>
+        <Book Series="Uncanny Inhumans" Number="18" Volume="2015" Year="2017">
+            <Database Name="cv" Series="81092" Issue="576641" />
+        </Book>
+        <Book Series="Uncanny X-Men" Number="17" Volume="2016" Year="2017">
+            <Database Name="cv" Series="87190" Issue="575887" />
+        </Book>
+        <Book Series="IvX" Number="4" Volume="2016" Year="2017">
+            <Database Name="cv" Series="96144" Issue="580755" />
+        </Book>
+        <Book Series="All-New X-Men" Number="18" Volume="2015" Year="2017">
+            <Database Name="cv" Series="86334" Issue="579303" />
+        </Book>
+        <Book Series="Deadpool &#38; The Mercs For Money" Number="8" Volume="2016" Year="2017">
+            <Database Name="cv" Series="92360" Issue="579313" />
+        </Book>
+        <Book Series="IvX" Number="5" Volume="2016" Year="2017">
+            <Database Name="cv" Series="96144" Issue="582534" />
+        </Book>
+        <Book Series="Extraordinary X-Men" Number="19" Volume="2015" Year="2017">
+            <Database Name="cv" Series="85756" Issue="582529" />
+        </Book>
+        <Book Series="Uncanny Inhumans" Number="19" Volume="2015" Year="2017">
+            <Database Name="cv" Series="81092" Issue="581573" />
+        </Book>
+        <Book Series="Uncanny X-Men" Number="18" Volume="2016" Year="2017">
+            <Database Name="cv" Series="87190" Issue="581574" />
+        </Book>
+        <Book Series="IvX" Number="6" Volume="2016" Year="2017">
+            <Database Name="cv" Series="96144" Issue="585080" />
+        </Book>
+        <Book Series="Uncanny Inhumans" Number="20" Volume="2015" Year="2017">
+            <Database Name="cv" Series="81092" Issue="588578" />
+        </Book>
+    </Books>
+    <Matchers />
+</ReadingList>
