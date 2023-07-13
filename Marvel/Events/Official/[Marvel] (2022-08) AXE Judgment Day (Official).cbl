a3fb26554a8df32cf73d84d65481db454c8846a6
jakem742 <jakemad@gmail.com>
2023-07-13 15:56:30 +1000

    More official lists added

--- Marvel/Events/Official/[Marvel] (2022-08) AXE Judgment Day (Official).cbl
+++ Marvel/Events/Official/[Marvel] (2022-08) AXE Judgment Day (Official).cbl
@@ -2,7 +2,6 @@
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
     <Name>[Marvel] (2022) A.X.E. Judgment Day (Official)</Name>
-    <NumIssues>38</NumIssues>
     <Books>
         <Book Series="Free Comic Book Day 2022: Avengers/X-Men" Number="1" Volume="2022" Year="2022">
             <Database Name="cv" Series="142841" Issue="922405" />
