e256b71061bf18a84cf0d3b82e92ee245d29481c
jakem742 <jakemad@gmail.com>
2023-07-13 21:47:07 +1000

    More official reading lists

---
+++ Marvel/Events/Official/[Marvel] (2018-11) Spider-Geddon (Official).cbl
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- 
+    Event based on https://www.marvel.com/comics/discover/1395/spider-geddon-the-complete-event
+-->
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Name>[Marvel] Spider-Geddon (Official)</Name>
+    <Books>
+    </Books>
+    <Matchers />
+</ReadingList>
---
+++ Marvel/Events/Official/[Marvel] (2020-01) Annihilation - Scourge (Official).cbl
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- 
+    Event based on https://www.marvel.com/comics/discover/1872/annihilation-scourge
+-->
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Name>[Marvel] Annihilation - Scourge (Official)</Name>
+    <Books>
+        <Book Series="Annihilation - Scourge Alpha" Number="1" Volume="2019" Year="2020">
+            <Database Name="cv" Series="122986" Issue="728414" />
+        </Book>
+        <Book Series="Annihilation - Scourge: Fantastic Four" Number="1" Volume="2019" Year="2020">
+            <Database Name="cv" Series="123263" Issue="729673" />
+        </Book>
+        <Book Series="Annihilation - Scourge: Nova" Number="1" Volume="2019" Year="2020">
+            <Database Name="cv" Series="123264" Issue="729674" />
+        </Book>
+        <Book Series="Annihilation - Scourge: Beta Ray Bill" Number="1" Volume="2019" Year="2020">
+            <Database Name="cv" Series="123407" Issue="730327" />
+        </Book>
+        <Book Series="Annihilation - Scourge: Silver Surfer" Number="1" Volume="2019" Year="2020">
+            <Database Name="cv" Series="123408" Issue="730328" />
+        </Book>
+        <Book Series="Annihilation - Scourge Omega" Number="1" Volume="2019" Year="2020">
+            <Database Name="cv" Series="123709" Issue="731504" />
+        </Book>
+    </Books>
+    <Matchers />
+</ReadingList>
