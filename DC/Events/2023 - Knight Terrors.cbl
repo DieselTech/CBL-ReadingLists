a757d8422d485138b3bd843b2db0635f639f1a80
Francois Wilhelmy <ice_mouton@hotmail.com>
2023-09-15 19:25:00 -0400

    ADDED: 2023 - Knight Terrors
    source: https://leagueofcomicgeeks.com/comics/event/14215/knight-terrors

---
+++ DC/Events/2023 - Knight Terrors.cbl
@@ -0,0 +1,148 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>2023 - Knight Terrors</Name>
+  <Books>
+    <Book Series="Dawn of DC Knight Terrors FCBD Special Edition: 2023" Number="1" Volume="2023" Year="2023">
+      <Id>dbac13af-dfc3-4f5e-a42d-585c6e8237f3</Id>
+    </Book>
+    <Book Series="Knight Terrors" Number="1" Volume="2023" Year="2023">
+      <Id>933a01ed-016e-4ffe-abdf-7ad488564264</Id>
+    </Book>
+    <Book Series="Knight Terrors" Number="2" Volume="2023" Year="2023">
+      <Id>94d39cff-2fdf-4c87-999b-a5fb4e50ffcc</Id>
+    </Book>
+    <Book Series="Knight Terrors: Action Comics" Number="1" Volume="2023" Year="2023">
+      <Id>0c8de343-8e02-4119-be12-576868161e92</Id>
+    </Book>
+    <Book Series="Knight Terrors: Angel Breaker" Number="1" Volume="2023" Year="2023">
+      <Id>ae3d553a-a0d7-4843-b69c-b50b95123179</Id>
+    </Book>
+    <Book Series="Knight Terrors: Batman" Number="1" Volume="2023" Year="2023">
+      <Id>a0dd46ce-431b-4e1b-bea7-517b09847344</Id>
+    </Book>
+    <Book Series="Knight Terrors: Black Adam" Number="1" Volume="2023" Year="2023">
+      <Id>3a78c2e3-a4da-4c02-add5-24c4d7c7bc35</Id>
+    </Book>
+    <Book Series="Knight Terrors: Catwoman" Number="1" Volume="2023" Year="2023">
+      <Id>009bf379-40ce-4541-865f-e377a24c307a</Id>
+    </Book>
+    <Book Series="Knight Terrors: Detective Comics" Number="1" Volume="2023" Year="2023">
+      <Id>bef55344-6fb9-46e9-99ea-3bf91af29d7f</Id>
+    </Book>
+    <Book Series="Knight Terrors: First Blood" Number="1" Volume="2023" Year="2023">
+      <Id>31972a85-209c-4bcb-afc6-8ca20956a7ca</Id>
+    </Book>
+    <Book Series="Knight Terrors: Green Lantern" Number="1" Volume="2023" Year="2023">
+      <Id>7242c1ee-ad46-4d56-9b19-2b921f4d6467</Id>
+    </Book>
+    <Book Series="Knight Terrors: Harley Quinn" Number="1" Volume="2023" Year="2023">
+      <Id>7ff52229-5516-495d-9727-09e26c67f678</Id>
+    </Book>
+    <Book Series="Knight Terrors: Nightwing" Number="1" Volume="2023" Year="2023">
+      <Id>ccf330d7-08dd-4ad9-b026-615165b42210</Id>
+    </Book>
+    <Book Series="Knight Terrors: Poison Ivy" Number="1" Volume="2023" Year="2023">
+      <Id>b6a0ca9d-916c-4688-9582-ff58debd74e6</Id>
+    </Book>
+    <Book Series="Knight Terrors: Punchline" Number="1" Volume="2023" Year="2023">
+      <Id>c4790456-9908-471a-b8bc-27bd2496c8a2</Id>
+    </Book>
+    <Book Series="Knight Terrors: Ravager" Number="1" Volume="2023" Year="2023">
+      <Id>52872904-c93b-446e-aeb8-d4c93300ed9d</Id>
+    </Book>
+    <Book Series="Knight Terrors: Robin" Number="1" Volume="2023" Year="2023">
+      <Id>8e9e8806-2a68-4049-8272-44eea5b76e74</Id>
+    </Book>
+    <Book Series="Knight Terrors: Shazam!" Number="1" Volume="2023" Year="2023">
+      <Id>6719e888-5ce0-4929-aec8-7c35670c1a8d</Id>
+    </Book>
+    <Book Series="Knight Terrors: Superman" Number="1" Volume="2023" Year="2023">
+      <Id>ae70ae44-2309-4de6-81fc-c8f876dfae0c</Id>
+    </Book>
+    <Book Series="Knight Terrors: The  Flash" Number="1" Volume="2023" Year="2023">
+      <Id>ffa23c6b-9092-4ae1-8832-a6b2aa39487e</Id>
+    </Book>
+    <Book Series="Knight Terrors: The Joker" Number="1" Volume="2023" Year="2023">
+      <Id>b0da7825-a55a-4872-9892-71c1427dd939</Id>
+    </Book>
+    <Book Series="Knight Terrors: Titans" Number="1" Volume="2023" Year="2023">
+      <Id>1ce809e2-4ce3-46b0-b9fd-bad61a582f33</Id>
+    </Book>
+    <Book Series="Knight Terrors: Wonder Woman" Number="1" Volume="2023" Year="2023">
+      <Id>07b615ff-3ec7-44ff-b77e-9276fdc9f473</Id>
+    </Book>
+    <Book Series="Knight Terrors: Zatanna" Number="1" Volume="2023" Year="2023">
+      <Id>651577bb-7641-4a63-addd-1b576e78d283</Id>
+    </Book>
+    <Book Series="Knight Terrors: Poison Ivy" Number="2" Volume="2023" Year="2023">
+      <Id>94621c5d-7cba-4869-a6b1-fceb0aa08e30</Id>
+    </Book>
+    <Book Series="Knight Terrors: Ravager" Number="2" Volume="2023" Year="2023">
+      <Id>005608f0-0247-46cf-91e8-bf4201b3bad2</Id>
+    </Book>
+    <Book Series="Knight Terrors: The Joker" Number="2" Volume="2023" Year="2023">
+      <Id>d8d81676-e081-4cb5-9edd-1b1b24a009b3</Id>
+    </Book>
+    <Book Series="Knight Terrors: Batman" Number="2" Volume="2023" Year="2023">
+      <Id>76e6709d-4b7a-4660-a001-4368a5e50425</Id>
+    </Book>
+    <Book Series="Knight Terrors: Black Adam" Number="2" Volume="2023" Year="2023">
+      <Id>1f6fb447-e9cb-46a7-a045-de6db692bcc3</Id>
+    </Book>
+    <Book Series="Knight Terrors" Number="3" Volume="2023" Year="2023">
+      <Id>ec37c0af-935e-4f52-a99a-7b839406ce00</Id>
+    </Book>
+    <Book Series="Knight Terrors: Robin" Number="2" Volume="2023" Year="2023">
+      <Id>ea9d6986-1ce3-45b6-9b80-d0c21d0d6bd4</Id>
+    </Book>
+    <Book Series="Knight Terrors: The  Flash" Number="2" Volume="2023" Year="2023">
+      <Id>db2a5173-a600-4b3c-b3b9-e313f55b001b</Id>
+    </Book>
+    <Book Series="Knight Terrors: Zatanna" Number="2" Volume="2023" Year="2023">
+      <Id>453eb0c8-9adc-4fb4-a72c-04cd7f2c68af</Id>
+    </Book>
+    <Book Series="Knight Terrors: Shazam!" Number="2" Volume="2023" Year="2023">
+      <Id>63f98aa3-c113-4525-94db-fc36da2f85c3</Id>
+    </Book>
+    <Book Series="Knight Terrors: Green Lantern" Number="2" Volume="2023" Year="2023">
+      <Id>e0ace8ac-0b6b-41bd-a436-cb5aacc20c4e</Id>
+    </Book>
+    <Book Series="Knight Terrors: Wonder Woman" Number="2" Volume="2023" Year="2023">
+      <Id>d9011f6b-b169-4f7d-8ccd-c331879fe313</Id>
+    </Book>
+    <Book Series="Knight Terrors: Superman" Number="2" Volume="2023" Year="2023">
+      <Id>492558d6-a04b-4a75-975b-5d17b9d73611</Id>
+    </Book>
+    <Book Series="Knight Terrors: Nightwing" Number="2" Volume="2023" Year="2023">
+      <Id>cc1c933f-271b-4532-b35e-60145c19f208</Id>
+    </Book>
+    <Book Series="Knight Terrors: Catwoman" Number="2" Volume="2023" Year="2023">
+      <Id>9a671195-3872-40c3-8fe6-2ebf7c669a56</Id>
+    </Book>
+    <Book Series="Knight Terrors: Punchline" Number="2" Volume="2023" Year="2023">
+      <Id>7c424b60-bc57-4423-a9f4-3ff8666f68fa</Id>
+    </Book>
+    <Book Series="Knight Terrors: Titans" Number="2" Volume="2023" Year="2023">
+      <Id>4b6b4f35-49e4-46b8-ae18-0ee5cc398ae0</Id>
+    </Book>
+    <Book Series="Knight Terrors: Action Comics" Number="2" Volume="2023" Year="2023">
+      <Id>21192363-2ac6-405e-8fda-a3d5d58d06d1</Id>
+    </Book>
+    <Book Series="Knight Terrors: Detective Comics" Number="2" Volume="2023" Year="2023">
+      <Id>074053a2-7876-48e9-bfe7-146507fd04dc</Id>
+    </Book>
+    <Book Series="Knight Terrors: Harley Quinn" Number="2" Volume="2023" Year="2023">
+      <Id>fee3ac84-6a93-42a5-a865-e3ab63bb2aef</Id>
+    </Book>
+    <Book Series="Knight Terrors: Angel Breaker" Number="2" Volume="2023" Year="2023">
+      <Id>f47aa47f-2a23-4966-b1b0-adbc904a3b8c</Id>
+    </Book>
+    <Book Series="Knight Terrors" Number="4" Volume="2023" Year="2023">
+      <Id>447799d0-86b9-486c-ba8e-dfda64d00af6</Id>
+    </Book>
+    <Book Series="Knight Terrors: Night's End" Number="1" Volume="2023" Year="2023">
+      <Id>8c5fcd77-83e2-4aa4-af41-6ab69807b288</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
