6a565521d20b37ca24393633d172de8e3a9c7ee2
DieselTech <30128380+DieselTech@users.noreply.github.com>
2023-09-16 21:41:34 -0400

    Adding lists to marvel unsorted timeline
    092 - Battle of The Atom
    093 - Infinity

---
+++ Marvel/Events/unsorted/093 Infinity.cbl
@@ -0,0 +1,184 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>2013 - Infinity </Name>
+  <Books>
+    <Book Series="Thanos Rising" Number="1" Volume="2013" Year="2013" Format="Limited Series">
+      <Id>ee208fe6-7200-4dc2-b0a1-73af199a4e82</Id>
+    </Book>
+    <Book Series="Thanos Rising" Number="2" Volume="2013" Year="2013" Format="Limited Series">
+      <Id>3035f792-aa14-4312-a09b-ed9d7e7280f7</Id>
+    </Book>
+    <Book Series="Thanos Rising" Number="3" Volume="2013" Year="2013" Format="Limited Series">
+      <Id>987858b2-243b-4f7e-9f66-aedf98d67710</Id>
+    </Book>
+    <Book Series="Thanos Rising" Number="4" Volume="2013" Year="2013" Format="Limited Series">
+      <Id>289ebf28-b350-4003-82a0-a0f7f084eaa6</Id>
+    </Book>
+    <Book Series="Thanos Rising" Number="5" Volume="2013" Year="2013" Format="Limited Series">
+      <Id>2f8829be-426e-4890-bafa-80932baa0b89</Id>
+    </Book>
+    <Book Series="Free Comic Book Day 2013 (Infinity)" Number="1" Volume="2013" Year="2013" Format="Crossover">
+      <Id>15e30874-599c-4edd-94e5-fbf5d30c86d1</Id>
+    </Book>
+    <Book Series="Avengers" Number="14" Volume="2013" Year="2013" Format="Main Series">
+      <Id>2af20e87-ad01-4dd9-bad9-448f1d5f890a</Id>
+    </Book>
+    <Book Series="Avengers" Number="15" Volume="2013" Year="2013" Format="Main Series">
+      <Id>c28cdbc4-c335-48e3-920d-4483cfbeecea</Id>
+    </Book>
+    <Book Series="Avengers" Number="16" Volume="2013" Year="2013" Format="Main Series">
+      <Id>3e236aeb-4aca-4414-a06a-e479d9fde842</Id>
+    </Book>
+    <Book Series="New Avengers" Number="8" Volume="2013" Year="2013" Format="Main Series">
+      <Id>d03fbbef-0f9a-4465-8b30-779993762821</Id>
+    </Book>
+    <Book Series="Avengers" Number="17" Volume="2013" Year="2013" Format="Main Series">
+      <Id>4033369f-86c6-4fbb-a0ed-3869412cdd15</Id>
+    </Book>
+    <Book Series="Infinity" Number="1" Volume="2013" Year="2013" Format="Crossover">
+      <Id>8e61e68d-1c8f-419b-a65a-f055f8a100bc</Id>
+    </Book>
+    <Book Series="Infinity: Against the Tide" Number="1" Volume="2013" Year="2013" Format="Crossover">
+      <Id>c380f704-bdc3-42ef-b28d-21775bbbe41e</Id>
+    </Book>
+    <Book Series="Infinity: Against the Tide" Number="2" Volume="2013" Year="2013" Format="Crossover">
+      <Id>f1359e7e-a409-450a-94cb-d06ddea59bde</Id>
+    </Book>
+    <Book Series="Avengers" Number="18" Volume="2013" Year="2013" Format="Main Series">
+      <Id>e87301c2-2127-42eb-8de7-a5eba19bfe9b</Id>
+    </Book>
+    <Book Series="Avengers Assemble" Number="18" Volume="2012" Year="2013" Format="Main Series">
+      <Id>737b709f-1ef5-40e9-b111-f88d964d3686</Id>
+    </Book>
+    <Book Series="New Avengers" Number="9" Volume="2013" Year="2013" Format="Main Series">
+      <Id>7c381f4d-2cbe-4222-907d-8bf25b4a7265</Id>
+    </Book>
+    <Book Series="Captain Marvel" Number="15" Volume="2012" Year="2013" Format="Main Series">
+      <Id>ee3b0838-53a2-4377-8ea7-4f010f4b080b</Id>
+    </Book>
+    <Book Series="Infinity" Number="2" Volume="2013" Year="2013" Format="Crossover">
+      <Id>a7428579-d5bf-4c6d-a19b-46e8c232f052</Id>
+    </Book>
+    <Book Series="Avengers" Number="19" Volume="2013" Year="2013" Format="Main Series">
+      <Id>e7c65fbd-13b1-489e-8a71-c465db147728</Id>
+    </Book>
+    <Book Series="New Avengers" Number="10" Volume="2013" Year="2013" Format="Main Series">
+      <Id>aefef54c-4df0-43fc-95b8-c66cf733d03c</Id>
+    </Book>
+    <Book Series="Thunderbolts" Number="14" Volume="2013" Year="2013" Format="Main Series">
+      <Id>5aba6080-ac64-43ef-999f-0eac5fb6c6d4</Id>
+    </Book>
+    <Book Series="Thunderbolts" Number="15" Volume="2013" Year="2013" Format="Main Series">
+      <Id>2d07e962-7edb-4f50-99f5-0f76e2e6be6f</Id>
+    </Book>
+    <Book Series="Thunderbolts" Number="16" Volume="2013" Year="2013" Format="Main Series">
+      <Id>6d7c6d83-f2e7-427e-b2d2-d754a0d705bf</Id>
+    </Book>
+    <Book Series="Thunderbolts" Number="17" Volume="2013" Year="2013" Format="Main Series">
+      <Id>d85bf346-012c-476b-8864-1d2783b6a3bb</Id>
+    </Book>
+    <Book Series="Thunderbolts" Number="18" Volume="2013" Year="2014" Format="Main Series">
+      <Id>ffaaa4c8-58e0-4e36-8371-3c92f31f187a</Id>
+    </Book>
+    <Book Series="Nova" Number="8" Volume="2013" Year="2013" Format="Main Series">
+      <Id>3a9d5ce1-b528-4597-bd64-898eeb1b3183</Id>
+    </Book>
+    <Book Series="Nova" Number="9" Volume="2013" Year="2013" Format="Main Series">
+      <Id>9f39b5f6-2dee-40dd-a0c0-565b5728efef</Id>
+    </Book>
+    <Book Series="Mighty Avengers" Number="1" Volume="2013" Year="2013" Format="Main Series">
+      <Id>f4919bd1-34fc-4ee7-80c8-88cdeae12f2f</Id>
+    </Book>
+    <Book Series="Mighty Avengers" Number="2" Volume="2013" Year="2013" Format="Main Series">
+      <Id>90ce9b22-8dbe-4dbb-b393-03ba5c4b8475</Id>
+    </Book>
+    <Book Series="Mighty Avengers" Number="3" Volume="2013" Year="2014" Format="Main Series">
+      <Id>38512e44-4b54-48e7-84fc-8ed061423311</Id>
+    </Book>
+    <Book Series="Infinity: The Hunt" Number="1" Volume="2013" Year="2013" Format="Crossover">
+      <Id>b2125489-4886-46b1-a410-80b6c72ce8ff</Id>
+    </Book>
+    <Book Series="Infinity: The Hunt" Number="2" Volume="2013" Year="2013" Format="Crossover">
+      <Id>3f6c6210-66ee-43c9-b73f-032c01714602</Id>
+    </Book>
+    <Book Series="Infinity: The Hunt" Number="3" Volume="2013" Year="2013" Format="Crossover">
+      <Id>4d10a01f-d9cc-4ad9-99fc-698c55c59693</Id>
+    </Book>
+    <Book Series="Infinity: The Hunt" Number="4" Volume="2013" Year="2014" Format="Crossover">
+      <Id>36752be1-2555-4331-85bf-8b6b8f2b7acf</Id>
+    </Book>
+    <Book Series="Infinity" Number="3" Volume="2013" Year="2013" Format="Crossover">
+      <Id>529281c3-d23e-4027-8d94-9b31b1514dc2</Id>
+    </Book>
+    <Book Series="Captain Marvel" Number="16" Volume="2012" Year="2013" Format="Main Series">
+      <Id>9311c504-a6c8-4e00-b8ae-7d89432376a2</Id>
+    </Book>
+    <Book Series="Avengers" Number="20" Volume="2013" Year="2013" Format="Main Series">
+      <Id>cfec081a-72a0-4071-b481-7fcde7d452ee</Id>
+    </Book>
+    <Book Series="Avengers Assemble" Number="19" Volume="2012" Year="2013" Format="Main Series">
+      <Id>26bb7511-1073-4bd4-bb7d-a614fa9c1154</Id>
+    </Book>
+    <Book Series="Infinity" Number="4" Volume="2013" Year="2013" Format="Crossover">
+      <Id>58f27760-4d4f-4963-bdaa-6eabd958f1a6</Id>
+    </Book>
+    <Book Series="Fearless Defenders" Number="10" Volume="2013" Year="2013" Format="Main Series">
+      <Id>12725445-7444-4f44-8bc4-cd9945b6dd73</Id>
+    </Book>
+    <Book Series="Superior Spider-Man Team-Up" Number="3" Volume="2013" Year="2013" Format="Main Series">
+      <Id>5c0c7354-97c0-443b-b389-72ed21757f4c</Id>
+    </Book>
+    <Book Series="Superior Spider-Man Team-Up" Number="4" Volume="2013" Year="2013" Format="Main Series">
+      <Id>c4acabdf-ace0-4d0c-967f-ce10541a0d7f</Id>
+    </Book>
+    <Book Series="Infinity: Heist" Number="1" Volume="2013" Year="2013" Format="Crossover">
+      <Id>ba5c0517-2328-401d-9370-51a496b4952a</Id>
+    </Book>
+    <Book Series="Infinity: Heist" Number="2" Volume="2013" Year="2013" Format="Crossover">
+      <Id>23144888-6299-45a9-ae08-ec6d1f38dd08</Id>
+    </Book>
+    <Book Series="Infinity: Heist" Number="3" Volume="2013" Year="2014" Format="Crossover">
+      <Id>e31cbd1f-a22b-4b3e-a1ee-d2db3a6a3632</Id>
+    </Book>
+    <Book Series="Infinity: Heist" Number="4" Volume="2013" Year="2014" Format="Crossover">
+      <Id>d66e7a3f-4121-422c-8209-78d8b95d99ec</Id>
+    </Book>
+    <Book Series="Avengers" Number="21" Volume="2013" Year="2013" Format="Main Series">
+      <Id>a2c962e5-3502-46e1-bc9c-f5c1fb38fa12</Id>
+    </Book>
+    <Book Series="Avengers Assemble" Number="20" Volume="2012" Year="2013" Format="Main Series">
+      <Id>d78b0519-1644-4323-bda6-27d35ef93331</Id>
+    </Book>
+    <Book Series="New Avengers" Number="11" Volume="2013" Year="2013" Format="Main Series">
+      <Id>e2facd79-cf88-46f1-bf1c-66174133544e</Id>
+    </Book>
+    <Book Series="Secret Avengers" Number="10" Volume="2013" Year="2013" Format="Main Series">
+      <Id>7716039e-56ec-4175-8a39-0de2164b4aad</Id>
+    </Book>
+    <Book Series="Secret Avengers" Number="11" Volume="2013" Year="2014" Format="Main Series">
+      <Id>f548c301-52db-4ca5-932c-b1efdda200e2</Id>
+    </Book>
+    <Book Series="Infinity" Number="5" Volume="2013" Year="2013" Format="Crossover">
+      <Id>df315a26-8058-44c1-b3a7-4e3eb66b78c8</Id>
+    </Book>
+    <Book Series="Avengers" Number="22" Volume="2013" Year="2013" Format="Main Series">
+      <Id>5a48ee85-cba3-4517-9f6a-01a696c7f7ec</Id>
+    </Book>
+    <Book Series="Avengers" Number="23" Volume="2013" Year="2014" Format="Main Series">
+      <Id>dee59d27-2d1c-4021-9ecc-10d62f4a995a</Id>
+    </Book>
+    <Book Series="Guardians of the Galaxy" Number="8" Volume="2013" Year="2013" Format="Main Series">
+      <Id>b959df7a-aeda-4a12-84d6-a386b68d9fb8</Id>
+    </Book>
+    <Book Series="Guardians of the Galaxy" Number="9" Volume="2013" Year="2014" Format="Main Series">
+      <Id>1c15b0f5-df0e-4c11-a2e5-3d7c134fca7b</Id>
+    </Book>
+    <Book Series="Infinity" Number="6" Volume="2013" Year="2014" Format="Crossover">
+      <Id>d0f91bf3-ef66-4ae9-8a06-c4adc75ebdfb</Id>
+    </Book>
+    <Book Series="New Avengers" Number="12" Volume="2013" Year="2014" Format="Main Series">
+      <Id>dac4c66f-f7f5-453a-8176-0ab8053a4e85</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
