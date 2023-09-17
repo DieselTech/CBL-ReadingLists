ea76da22b991bb11edccacc3f7f3da915f04d90b
jakem742 <jakemad@gmail.com>
2023-09-17 18:59:51 +1000

    Star wars initial cleanup

---
+++ Other/Star Wars/Events/[Marvel] (2022) Crimson Reign.cbl
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Name>Crimson Reign </Name>
+    <Source>[Marvel] (2022) Crimson Reign (Official)</Source>
+    <StartYear>2022</StartYear>
+    <Books>
+        <Book Series="Star Wars: Darth Vader" Number="18" Volume="2020" Year="2022">
+            <Database Name="cv" Series="124821" Issue="896644" />
+        </Book>
+        <Book Series="Star Wars: Bounty Hunters" Number="18" Volume="2020" Year="2022">
+            <Database Name="cv" Series="125679" Issue="896643" />
+        </Book>
+        <Book Series="Star Wars: Doctor Aphra" Number="16" Volume="2020" Year="2022">
+            <Database Name="cv" Series="126644" Issue="894455" />
+        </Book>
+        <Book Series="Star Wars" Number="19" Volume="2020" Year="2022">
+            <Database Name="cv" Series="123860" Issue="897512" />
+        </Book>
+        <Book Series="Star Wars: Crimson Reign" Number="1" Volume="2022" Year="2022">
+            <Database Name="cv" Series="140398" Issue="897890" />
+        </Book>
+        <Book Series="Star Wars: Bounty Hunters" Number="19" Volume="2020" Year="2022">
+            <Database Name="cv" Series="125679" Issue="900398" />
+        </Book>
+        <Book Series="Star Wars: Doctor Aphra" Number="17" Volume="2020" Year="2022">
+            <Database Name="cv" Series="126644" Issue="901351" />
+        </Book>
+        <Book Series="Star Wars: Darth Vader" Number="19" Volume="2020" Year="2022">
+            <Database Name="cv" Series="124821" Issue="899198" />
+        </Book>
+        <Book Series="Star Wars" Number="20" Volume="2020" Year="2022">
+            <Database Name="cv" Series="123860" Issue="902531" />
+        </Book>
+        <Book Series="Star Wars: Bounty Hunters" Number="20" Volume="2020" Year="2022">
+            <Database Name="cv" Series="125679" Issue="902529" />
+        </Book>
+        <Book Series="Star Wars: Doctor Aphra" Number="18" Volume="2020" Year="2022">
+            <Database Name="cv" Series="126644" Issue="903921" />
+        </Book>
+        <Book Series="Star Wars: Crimson Reign" Number="2" Volume="2022" Year="2022">
+            <Database Name="cv" Series="140398" Issue="905797" />
+        </Book>
+        <Book Series="Star Wars" Number="21" Volume="2020" Year="2022">
+            <Database Name="cv" Series="123860" Issue="909807" />
+        </Book>
+        <Book Series="Star Wars: Crimson Reign" Number="3" Volume="2022" Year="2022">
+            <Database Name="cv" Series="140398" Issue="910631" />
+        </Book>
+        <Book Series="Star Wars: Darth Vader" Number="20" Volume="2020" Year="2022">
+            <Database Name="cv" Series="124821" Issue="906639" />
+        </Book>
+        <Book Series="Star Wars: Doctor Aphra" Number="19" Volume="2020" Year="2022">
+            <Database Name="cv" Series="126644" Issue="912251" />
+        </Book>
+        <Book Series="Star Wars" Number="22" Volume="2020" Year="2022">
+            <Database Name="cv" Series="123860" Issue="916408" />
+        </Book>
+        <Book Series="Star Wars: Bounty Hunters" Number="21" Volume="2020" Year="2022">
+            <Database Name="cv" Series="125679" Issue="913174" />
+        </Book>
+        <Book Series="Star Wars: Bounty Hunters" Number="22" Volume="2020" Year="2022">
+            <Database Name="cv" Series="125679" Issue="914650" />
+        </Book>
+        <Book Series="Star Wars: Crimson Reign" Number="4" Volume="2022" Year="2022">
+            <Database Name="cv" Series="140398" Issue="919481" />
+        </Book>
+        <Book Series="Star Wars" Number="23" Volume="2020" Year="2022">
+            <Database Name="cv" Series="123860" Issue="921118" />
+        </Book>
+        <Book Series="Star Wars: Darth Vader" Number="21" Volume="2020" Year="2022">
+            <Database Name="cv" Series="124821" Issue="912159" />
+        </Book>
+        <Book Series="Star Wars: Darth Vader" Number="22" Volume="2020" Year="2022">
+            <Database Name="cv" Series="124821" Issue="916409" />
+        </Book>
+        <Book Series="Star Wars: Doctor Aphra" Number="20" Volume="2020" Year="2022">
+            <Database Name="cv" Series="126644" Issue="925008" />
+        </Book>
+        <Book Series="Star Wars: Doctor Aphra" Number="21" Volume="2020" Year="2022">
+            <Database Name="cv" Series="126644" Issue="933089" />
+        </Book>
+        <Book Series="Star Wars" Number="24" Volume="2020" Year="2022">
+            <Database Name="cv" Series="123860" Issue="929654" />
+        </Book>
+        <Book Series="Star Wars: Crimson Reign" Number="5" Volume="2022" Year="2022">
+            <Database Name="cv" Series="140398" Issue="931634" />
+        </Book>
+        <Book Series="Star Wars: Bounty Hunters" Number="23" Volume="2020" Year="2022">
+            <Database Name="cv" Series="125679" Issue="928440" />
+        </Book>
+        <Book Series="Star Wars: Darth Vader" Number="23" Volume="2020" Year="2022">
+            <Database Name="cv" Series="124821" Issue="928441" />
+        </Book>
+        <Book Series="Star Wars: Darth Vader" Number="24" Volume="2020" Year="2022">
+            <Database Name="cv" Series="124821" Issue="933092" />
+        </Book>
+        <Book Series="Star Wars: Bounty Hunters" Number="24" Volume="2020" Year="2022">
+            <Database Name="cv" Series="125679" Issue="930982" />
+        </Book>
+    </Books>
+    <Matchers />
+</ReadingList>
