5a61f1f78de168571f2649e4404e5e9c8438a5cc
DieselTech <30128380+DieselTech@users.noreply.github.com>
2023-09-16 21:39:32 -0400

    Adding new lists to other publishers
    Creation of new lists:
    
    Bacchus
    Concrete
    Game of Thrones
    Vampirella
    The Dark Tower

---
+++ Other/unsorted/Dark Horse/Bacchus.cbl
@@ -0,0 +1,22 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>Bacchus</Name>
+  <Books>
+    <Book Series="Bacchus" Number="1" Volume="2014" Year="2014">
+      <Id>6b97b408-590e-4eaf-a1d0-62b910973bff</Id>
+    </Book>
+    <Book Series="Bacchus" Number="2" Volume="2014" Year="2014">
+      <Id>3be91914-174f-4175-8292-2c8a918b06d0</Id>
+    </Book>
+    <Book Series="Bacchus" Number="3" Volume="2014" Year="2014">
+      <Id>989e9cde-863b-4701-a3ca-caf7a05aaa7e</Id>
+    </Book>
+    <Book Series="Bacchus" Number="4" Volume="2014" Year="2014">
+      <Id>39428b45-ab41-4489-be0f-0b58cc9cb77e</Id>
+    </Book>
+    <Book Series="Bacchus" Number="5" Volume="2014" Year="2014">
+      <Id>9da28290-2b47-4dd4-973e-a92c012792e0</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
---
+++ Other/unsorted/Stephen King's The Dark Tower.cbl
@@ -0,0 +1,196 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>Stephen King's The Dark Tower</Name>
+  <Books>
+    <Book Series="Dark Tower: The Gunslinger Born" Number="1" Volume="2007" Year="2007">
+      <Id>3e99657e-4b3b-48f9-a000-7936a6ff75d4</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger Born" Number="2" Volume="2007" Year="2007">
+      <Id>93635b9a-eeb5-4257-9b58-269ef353b1be</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger Born" Number="3" Volume="2007" Year="2007">
+      <Id>47f93a10-ebdc-4417-a948-3902503fbb03</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger Born" Number="4" Volume="2007" Year="2007">
+      <Id>87acfec5-fa85-4504-b423-b72ffa1227db</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger Born" Number="5" Volume="2007" Year="2007">
+      <Id>6ae30dd2-5285-4471-8c20-b4dbf82234d9</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger Born" Number="6" Volume="2007" Year="2007">
+      <Id>93e471e5-7cfd-49e6-b010-3088ab9a8dd1</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger Born" Number="7" Volume="2007" Year="2007">
+      <Id>6592354b-6e32-4f9e-90c3-824f3a31999d</Id>
+    </Book>
+    <Book Series="Dark Tower: The Long Road Home" Number="1" Volume="2008" Year="2008">
+      <Id>2b471bc6-72ad-416c-acab-bd0ee608e248</Id>
+    </Book>
+    <Book Series="Dark Tower: The Long Road Home" Number="2" Volume="2008" Year="2008">
+      <Id>6617526b-f95f-4d0f-bb96-800a4002552f</Id>
+    </Book>
+    <Book Series="Dark Tower: The Long Road Home" Number="3" Volume="2008" Year="2008">
+      <Id>239eae66-b62d-483d-92b5-caee4e6eb0de</Id>
+    </Book>
+    <Book Series="Dark Tower: The Long Road Home" Number="4" Volume="2008" Year="2008">
+      <Id>7dd92329-1e57-440d-8993-5d3170cc0a8a</Id>
+    </Book>
+    <Book Series="Dark Tower: The Long Road Home" Number="5" Volume="2008" Year="2008">
+      <Id>b376592a-c9ae-4b13-b7e0-8ce75b0dbe53</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger: Treachery" Number="1" Volume="2009" Year="2009">
+      <Id>1e1dbde1-3c8b-4ae6-940a-1178cbfb66ae</Id>
+    </Book>
+    <Book Series="Dark Tower: The Sorcerer" Number="1" Volume="2009" Year="2009">
+      <Id>5221eb07-0f23-4117-8c1d-0e2661a60049</Id>
+    </Book>
+    <Book Series="Dark Tower: Fall of Gilead" Number="1" Volume="2009" Year="2009">
+      <Id>b2b5ce48-1f0f-4ee8-af4a-edd31fca07bf</Id>
+    </Book>
+    <Book Series="Dark Tower: Fall of Gilead" Number="2" Volume="2009" Year="2009">
+      <Id>77155c3e-9feb-472b-9119-b6f9ae04eca6</Id>
+    </Book>
+    <Book Series="Dark Tower: Fall of Gilead" Number="3" Volume="2009" Year="2009">
+      <Id>bfa6388a-01b8-40d7-8a21-3153afd2f6db</Id>
+    </Book>
+    <Book Series="Dark Tower: Fall of Gilead" Number="4" Volume="2009" Year="2009">
+      <Id>7cce3047-b2e2-4265-9f4c-91b6d46ec694</Id>
+    </Book>
+    <Book Series="Dark Tower: Fall of Gilead" Number="5" Volume="2009" Year="2009">
+      <Id>35cc0df2-3b7e-46ec-8a24-8b710d28dea6</Id>
+    </Book>
+    <Book Series="Dark Tower: Fall of Gilead" Number="6" Volume="2009" Year="2010">
+      <Id>8b83776a-3d5f-44e7-9390-0c4311a39204</Id>
+    </Book>
+    <Book Series="Dark Tower: Battle of Jericho Hill" Number="1" Volume="2010" Year="2010">
+      <Id>c5ac11fd-a137-4006-a921-e916921d06a2</Id>
+    </Book>
+    <Book Series="Dark Tower: Battle of Jericho Hill" Number="2" Volume="2010" Year="2010">
+      <Id>ffbcd6cb-9877-42c4-85f0-0a3624a1f4ee</Id>
+    </Book>
+    <Book Series="Dark Tower: Battle of Jericho Hill" Number="3" Volume="2010" Year="2010">
+      <Id>8eaa0499-5bf4-445b-8780-857539b18572</Id>
+    </Book>
+    <Book Series="Dark Tower: Battle of Jericho Hill" Number="4" Volume="2010" Year="2010">
+      <Id>bec36a8e-c2e3-4b22-900c-788d086cfc8b</Id>
+    </Book>
+    <Book Series="Dark Tower: Battle of Jericho Hill" Number="5" Volume="2010" Year="2010">
+      <Id>b180e67e-9e6e-46d8-8466-378d27b7edd8</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger - The Journey Begins" Number="1" Volume="2011" Year="2012">
+      <Id>953ef73b-80b7-42a5-a1b3-4c4ab2579eb8</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Gunslinger - The Little Sisters of Eluria" Number="1" Volume="2013" Year="2013">
+      <Id>74bc8a63-1273-498f-9fbb-5dd93cf7c51d</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger: The Battle of Tull" Number="1" Volume="2011" Year="2012">
+      <Id>6d746338-5f00-4364-8bbd-3adde06fe4f7</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Gunslinger - The Way Station" Number="1" Volume="2013" Year="2013">
+      <Id>e18761a6-efd8-4a6a-986f-0637f086987b</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Gunslinger - The Man In Black" Number="1" Volume="2012" Year="2012">
+      <Id>30127529-20bf-431f-ba95-76000130c387</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger - Sheemie's Tale" Number="1" Volume="2013" Year="2013">
+      <Id>c3c3f6f1-dae1-47a2-b4a0-cbee9518901c</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger - Sheemie's Tale" Number="2" Volume="2013" Year="2013">
+      <Id>a0d511a9-92e7-4b8b-b093-092fb728454c</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Gunslinger - Evil Ground" Number="1" Volume="2013" Year="2013">
+      <Id>94c52961-0cc0-4874-99af-0a3422b04282</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Gunslinger - Evil Ground" Number="2" Volume="2013" Year="2013">
+      <Id>53fed94d-13cb-42aa-801a-4c5b40184943</Id>
+    </Book>
+    <Book Series="Dark Tower: The Gunslinger - So Fell Lord Perth" Number="1" Volume="2013" Year="2013">
+      <Id>ae7c5c73-f42f-4793-9539-357acb8d258f</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - The Prisoner" Number="1" Volume="2014" Year="2014">
+      <Id>f7f194ef-ee36-4f8a-9073-69aba95a1a2f</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - The Prisoner" Number="2" Volume="2014" Year="2014">
+      <Id>216eb687-03e5-40f8-a88f-91dfbe6bfd75</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - The Prisoner" Number="3" Volume="2014" Year="2014">
+      <Id>aa91628a-008b-4c03-a6cd-1d2255811579</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - The Prisoner" Number="4" Volume="2014" Year="2015">
+      <Id>9aae52ba-93f1-4024-9d57-4362e14c444c</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - The Prisoner" Number="5" Volume="2014" Year="2015">
+      <Id>3df04e0d-d6d8-473a-9f50-5719b4cc722f</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - House of Cards" Number="1" Volume="2015" Year="2015">
+      <Id>3ad421ad-8d57-4439-a82c-01fc372c9b8f</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - House of Cards" Number="2" Volume="2015" Year="2015">
+      <Id>3093b224-1d57-47b0-b1c4-a5fcc58d1227</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - House of Cards" Number="3" Volume="2015" Year="2015">
+      <Id>941aebb0-0bff-4cf9-b8c4-1be70f7fec39</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - House of Cards" Number="4" Volume="2015" Year="2015">
+      <Id>8d6cb326-18b0-411c-8018-1b5c481d24bf</Id>
+    </Book>
+    <Book Series="The Dark Tower: The Drawing of the Three - House of Cards" Number="5" Volume="2015" Year="2015">
+      <Id>7897539c-bcc4-425e-ae3d-51811c908817</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Lady of Shadows" Number="1" Volume="2015" Year="2015">
+      <Id>62dadb4f-a322-44bb-b349-c722f5d7fc99</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Lady of Shadows" Number="2" Volume="2015" Year="2015">
+      <Id>b1bc24bc-a971-4acd-a08b-a43f90ce37d8</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Lady of Shadows" Number="3" Volume="2015" Year="2016">
+      <Id>70dc6535-d301-4e32-a578-f095a2b25ebb</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Lady of Shadows" Number="4" Volume="2015" Year="2016">
+      <Id>ccf21d65-c3d3-4620-b7b5-a0bdd694b209</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Lady of Shadows" Number="5" Volume="2015" Year="2016">
+      <Id>ac2d8e40-cdcd-446b-85b9-b226839962ab</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Bitter Medicine" Number="1" Volume="2016" Year="2016">
+      <Id>b0191edf-0934-42d9-a5d2-f6627cb55bc9</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Bitter Medicine" Number="2" Volume="2016" Year="2016">
+      <Id>fba83449-98b8-4554-8b50-c7a3c39941f3</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Bitter Medicine" Number="3" Volume="2016" Year="2016">
+      <Id>e84ea570-5334-4011-9bd1-b81453ccc692</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Bitter Medicine" Number="4" Volume="2016" Year="2016">
+      <Id>2298c73d-2615-4eca-ad15-315390b5a167</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - Bitter Medicine" Number="5" Volume="2016" Year="2016">
+      <Id>9d2516c2-1f1e-4f73-97c2-061048131ef7</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - The Sailor" Number="1" Volume="2016" Year="2016">
+      <Id>7aae04a3-248c-44f7-858d-e4215f29ae48</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - The Sailor" Number="2" Volume="2016" Year="2017">
+      <Id>280de98f-72b6-4b38-817c-16e7f4ba6df9</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - The Sailor" Number="3" Volume="2016" Year="2017">
+      <Id>18648b7a-0510-4156-827a-4914d76160da</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - The Sailor" Number="4" Volume="2016" Year="2017">
+      <Id>202b8536-43f0-4d30-888d-e276ecbfb5ae</Id>
+    </Book>
+    <Book Series="Dark Tower: The Drawing of the Three - The Sailor" Number="5" Volume="2016" Year="2017">
+      <Id>f8ac9006-9c34-4cd5-a1ab-284c1bdf0bbd</Id>
+    </Book>
+    <Book Series="Dark Tower: Gunslinger's Guidebook" Number="1" Volume="2007" Year="2007">
+      <Id>f5f8e638-010b-4e38-ba05-218eaba1c2c7</Id>
+    </Book>
+    <Book Series="Dark Tower: Guide To Gilead" Number="1" Volume="2009" Year="2009">
+      <Id>5e26ac9a-3487-4289-96c5-e40432aa9f8b</Id>
+    </Book>
+    <Book Series="Marvel Spotlight: Dark Tower" Number="1" Volume="2007" Year="2007" Format="Preview">
+      <Id>b02a6d2d-58da-4b70-98ed-71863990ce48</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
