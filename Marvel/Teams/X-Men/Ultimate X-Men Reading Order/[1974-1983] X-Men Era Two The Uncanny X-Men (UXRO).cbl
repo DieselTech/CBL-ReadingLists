52c7aff9654d1083bd13886fd6d737e5d48e2972
flips22 <1898114+flips22@users.noreply.github.com>
2024-01-04 15:28:00 -0600

    X-Men Era 2 Fixes

--- Marvel/Teams/X-Men/Ultimate X-Men Reading Order/[1974-1983] X-Men Era Two The Uncanny X-Men (UXRO).cbl
+++ Marvel/Teams/X-Men/Ultimate X-Men Reading Order/[1974-1983] X-Men Era Two The Uncanny X-Men (UXRO).cbl
@@ -21,8 +21,8 @@
 <Book Series="Giant-Size X-Men" Number="1" Volume="1975" Year="1975">
 <Database Name="cv" Series="2763" Issue="14890" />
 </Book>
-<Book Series="Giant-Size X-Men" Number="1" Volume="1975" Year="1975">
-<Database Name="cv" Series="2763" Issue="14890" />
+<Book Series="Classic X-Men" Number="1" Volume="1986" Year="1986">
+<Database Name="cv" Series="3626" Issue="47389" />
 </Book>
 <Book Series="The X-Men" Number="94" Volume="1963" Year="1975">
 <Database Name="cv" Series="2133" Issue="15508" />
@@ -96,28 +96,28 @@
 <Book Series="The X-Men" Number="113" Volume="1963" Year="1978">
 <Database Name="cv" Series="2133" Issue="18837" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="114" Volume="1981" Year="1978">
+<Book Series="The X-Men" Number="114" Volume="1963" Year="1978">
 <Database Name="cv" Series="2133" Issue="18916" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="115" Volume="1981" Year="1978">
+<Book Series="The X-Men" Number="115" Volume="1963" Year="1978">
 <Database Name="cv" Series="2133" Issue="18985" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="116" Volume="1981" Year="1978">
+<Book Series="The X-Men" Number="116" Volume="1963" Year="1978">
 <Database Name="cv" Series="2133" Issue="19060" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="117" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="117" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19210" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="118" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="118" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19317" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="119" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="119" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19386" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="120" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="120" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19455" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="121" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="121" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19525" />
 </Book>
 <Book Series="Power Man and Iron Fist" Number="56" Volume="1978" Year="1979">
@@ -126,13 +126,13 @@
 <Book Series="Power Man and Iron Fist" Number="57" Volume="1978" Year="1979">
 <Database Name="cv" Series="2959" Issue="19584" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="122" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="122" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19592" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="123" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="123" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="123628" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="124" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="124" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19726" />
 </Book>
 <Book Series="X-Men Annual" Number="3" Volume="1970" Year="1979">
@@ -150,49 +150,49 @@
 <Book Series="The Avengers" Number="187" Volume="1963" Year="1979">
 <Database Name="cv" Series="2128" Issue="19774" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="125" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="125" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19794" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="126" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="126" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19865" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="127" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="127" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="19934" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="128" Volume="1981" Year="1979">
+<Book Series="The X-Men" Number="128" Volume="1963" Year="1979">
 <Database Name="cv" Series="2133" Issue="20006" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="129" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="129" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20114" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="130" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="130" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20186" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="131" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="131" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20258" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="132" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="132" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20333" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="133" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="133" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20403" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="134" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="134" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20477" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="135" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="135" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20546" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="136" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="136" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20613" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="137" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="137" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20681" />
 </Book>
 <Book Series="Classic X-Men" Number="43" Volume="1986" Year="1990">
 <Database Name="cv" Series="3626" Issue="47430" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="138" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="138" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20751" />
 </Book>
 <Book Series="X-Men Annual" Number="4" Volume="1970" Year="1980">
@@ -201,13 +201,13 @@
 <Book Series="Marvel Team-Up" Number="100" Volume="1972" Year="1980">
 <Database Name="cv" Series="2576" Issue="20866" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="139" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="139" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20812" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="140" Volume="1981" Year="1980">
+<Book Series="The X-Men" Number="140" Volume="1963" Year="1980">
 <Database Name="cv" Series="2133" Issue="20884" />
 </Book>
-<Book Series="The Uncanny X-Men" Number="141" Volume="1981" Year="1981">
+<Book Series="The X-Men" Number="141" Volume="1963" Year="1981">
 <Database Name="cv" Series="2133" Issue="20988" />
 </Book>
 <Book Series="The Uncanny X-Men" Number="142" Volume="1981" Year="1981">
