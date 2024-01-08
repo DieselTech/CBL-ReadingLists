b517c2b758d17ba6a8d34f5974a50db2fc4202a7
DieselTech <30128380+DieselTech@users.noreply.github.com>
2024-01-08 15:36:01 -0500

    Create Locke and Key.cbl
    File contributed by Reginald C. Puppington III on Discord

---
+++ Other/unsorted/Locke and Key.cbl
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>Locke and Key</Name>
+<NumIssues>47</NumIssues>
+<Books>
+<Book Series="Locke &amp; Key: Welcome to Lovecraft" Number="1" Volume="2008" Year="2008">
+<Database Name="cv" Series="20701" Issue="124110" />
+</Book>
+<Book Series="Locke &amp; Key: Welcome to Lovecraft" Number="2" Volume="2008" Year="2008">
+<Database Name="cv" Series="20701" Issue="125226" />
+</Book>
+<Book Series="Locke &amp; Key: Welcome to Lovecraft" Number="3" Volume="2008" Year="2008">
+<Database Name="cv" Series="20701" Issue="127130" />
+</Book>
+<Book Series="Locke &amp; Key: Welcome to Lovecraft" Number="4" Volume="2008" Year="2008">
+<Database Name="cv" Series="20701" Issue="130470" />
+</Book>
+<Book Series="Locke &amp; Key: Welcome to Lovecraft" Number="5" Volume="2008" Year="2008">
+<Database Name="cv" Series="20701" Issue="131497" />
+</Book>
+<Book Series="Locke &amp; Key: Welcome to Lovecraft" Number="6" Volume="2008" Year="2008">
+<Database Name="cv" Series="20701" Issue="134657" />
+</Book>
+<Book Series="Locke &amp; Key: Head Games" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="25426" Issue="150048" />
+</Book>
+<Book Series="Locke &amp; Key: Head Games" Number="2" Volume="2009" Year="2009">
+<Database Name="cv" Series="25426" Issue="151719" />
+</Book>
+<Book Series="Locke &amp; Key: Head Games" Number="3" Volume="2009" Year="2009">
+<Database Name="cv" Series="25426" Issue="153733" />
+</Book>
+<Book Series="Locke &amp; Key: Head Games" Number="4" Volume="2009" Year="2009">
+<Database Name="cv" Series="25426" Issue="155177" />
+</Book>
+<Book Series="Locke &amp; Key: Head Games" Number="5" Volume="2009" Year="2009">
+<Database Name="cv" Series="25426" Issue="157918" />
+</Book>
+<Book Series="Locke &amp; Key: Head Games" Number="6" Volume="2009" Year="2009">
+<Database Name="cv" Series="25426" Issue="162549" />
+</Book>
+<Book Series="Locke &amp; Key: Crown of Shadows" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="29689" Issue="182789" />
+</Book>
+<Book Series="Locke &amp; Key: Crown of Shadows" Number="2" Volume="2009" Year="2009">
+<Database Name="cv" Series="29689" Issue="188318" />
+</Book>
+<Book Series="Locke &amp; Key: Crown of Shadows" Number="3" Volume="2009" Year="2010">
+<Database Name="cv" Series="29689" Issue="199562" />
+</Book>
+<Book Series="Locke &amp; Key: Crown of Shadows" Number="4" Volume="2009" Year="2010">
+<Database Name="cv" Series="29689" Issue="205859" />
+</Book>
+<Book Series="Locke &amp; Key: Crown of Shadows" Number="5" Volume="2009" Year="2010">
+<Database Name="cv" Series="29689" Issue="210310" />
+</Book>
+<Book Series="Locke &amp; Key: Crown of Shadows" Number="6" Volume="2009" Year="2010">
+<Database Name="cv" Series="29689" Issue="224931" />
+</Book>
+<Book Series="Locke &amp; Key: Keys to the Kingdom" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="34853" Issue="228976" />
+</Book>
+<Book Series="Locke &amp; Key: Keys to the Kingdom" Number="2" Volume="2010" Year="2010">
+<Database Name="cv" Series="34853" Issue="238493" />
+</Book>
+<Book Series="Locke &amp; Key: Keys to the Kingdom" Number="3" Volume="2010" Year="2010">
+<Database Name="cv" Series="34853" Issue="246541" />
+</Book>
+<Book Series="Locke &amp; Key: Keys to the Kingdom" Number="4" Volume="2010" Year="2011">
+<Database Name="cv" Series="34853" Issue="258683" />
+</Book>
+<Book Series="Locke &amp; Key: Keys to the Kingdom" Number="5" Volume="2010" Year="2011">
+<Database Name="cv" Series="34853" Issue="264785" />
+</Book>
+<Book Series="Locke &amp; Key: Keys to the Kingdom" Number="6" Volume="2010" Year="2011">
+<Database Name="cv" Series="34853" Issue="269159" />
+</Book>
+<Book Series="Locke &amp; Key: Clockworks" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="41484" Issue="281126" />
+</Book>
+<Book Series="Locke &amp; Key: Clockworks" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="41484" Issue="290951" />
+</Book>
+<Book Series="Locke &amp; Key: Clockworks" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="41484" Issue="300950" />
+</Book>
+<Book Series="Locke &amp; Key: Clockworks" Number="4" Volume="2011" Year="2012">
+<Database Name="cv" Series="41484" Issue="313917" />
+</Book>
+<Book Series="Locke &amp; Key: Clockworks" Number="5" Volume="2011" Year="2012">
+<Database Name="cv" Series="41484" Issue="321577" />
+</Book>
+<Book Series="Locke &amp; Key: Clockworks" Number="6" Volume="2011" Year="2012">
+<Database Name="cv" Series="41484" Issue="336434" />
+</Book>
+<Book Series="Locke &amp; Key: Omega" Number="1" Volume="2012" Year="2012">
+<Database Name="cv" Series="53936" Issue="367748" />
+</Book>
+<Book Series="Locke &amp; Key: Omega" Number="2" Volume="2012" Year="2012">
+<Database Name="cv" Series="53936" Issue="373342" />
+</Book>
+<Book Series="Locke &amp; Key: Omega" Number="3" Volume="2012" Year="2013">
+<Database Name="cv" Series="53936" Issue="387286" />
+</Book>
+<Book Series="Locke &amp; Key: Omega" Number="4" Volume="2012" Year="2013">
+<Database Name="cv" Series="53936" Issue="395724" />
+</Book>
+<Book Series="Locke &amp; Key: Omega" Number="5" Volume="2012" Year="2013">
+<Database Name="cv" Series="53936" Issue="409022" />
+</Book>
+<Book Series="Locke &amp; Key: Alpha" Number="1" Volume="2013" Year="2013">
+<Database Name="cv" Series="67225" Issue="425046" />
+</Book>
+<Book Series="Locke &amp; Key: Alpha" Number="2" Volume="2013" Year="2013">
+<Database Name="cv" Series="67225" Issue="437528" />
+</Book>
+<Book Series="Locke &amp; Key: Small World" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="97057" Issue="569370" />
+</Book>
+<Book Series="Locke &amp; Key: ...In Pale Battalions Go...." Number="1" Volume="2020" Year="2020">
+<Database Name="cv" Series="129895" Issue="796421" />
+</Book>
+<Book Series="Locke &amp; Key: ...In Pale Battalions Go...." Number="2" Volume="2020" Year="2020">
+<Database Name="cv" Series="129895" Issue="807446" />
+</Book>
+<Book Series="Locke &amp; Key: ...In Pale Battalions Go...." Number="3" Volume="2020" Year="2020">
+<Database Name="cv" Series="129895" Issue="821407" />
+</Book>
+<Book Series="Locke &amp; Key/The Sandman" Number="0" Volume="2020" Year="2020">
+<Database Name="cv" Series="132506" Issue="820733" />
+</Book>
+<Book Series="Locke &amp; Key/The Sandman: Hell &amp; Gone" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="135355" Issue="842646" />
+</Book>
+<Book Series="Locke &amp; Key/The Sandman: Hell &amp; Gone" Number="2" Volume="2021" Year="2021">
+<Database Name="cv" Series="135355" Issue="887013" />
+</Book>
+<Book Series="Locke &amp; Key: Grindhouse" Number="1" Volume="2012" Year="2012">
+<Database Name="cv" Series="51624" Issue="354069" />
+</Book>
+<Book Series="Locke &amp; Key: Dog Days" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="122638" Issue="726079" />
+</Book>
+<Book Series="Locke &amp; Key: Guide to the Known Keys" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="44160" Issue="303725" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
