0efbef2c456b06c7302af82bbe1b5663ccd9ba3f
flips22 <1898114+flips22@users.noreply.github.com>
2024-01-28 16:32:15 -0600

    Two user submitted lists.

---
+++ Other/American Vampire/[Vertigo][2010-2021] American Vampire (HTLC).cbl
@@ -0,0 +1,212 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[Vertigo][2010-2021] American Vampire (HTLC)</Name>
+<NumIssues>68</NumIssues>
+<Books>
+<Book Series="American Vampire" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="32051" Issue="201042" />
+</Book>
+<Book Series="American Vampire" Number="2" Volume="2010" Year="2010">
+<Database Name="cv" Series="32051" Issue="208781" />
+</Book>
+<Book Series="American Vampire" Number="3" Volume="2010" Year="2010">
+<Database Name="cv" Series="32051" Issue="214907" />
+</Book>
+<Book Series="American Vampire" Number="4" Volume="2010" Year="2010">
+<Database Name="cv" Series="32051" Issue="220561" />
+</Book>
+<Book Series="American Vampire" Number="5" Volume="2010" Year="2010">
+<Database Name="cv" Series="32051" Issue="226784" />
+</Book>
+<Book Series="American Vampire" Number="6" Volume="2010" Year="2010">
+<Database Name="cv" Series="32051" Issue="233579" />
+</Book>
+<Book Series="American Vampire" Number="7" Volume="2010" Year="2010">
+<Database Name="cv" Series="32051" Issue="237013" />
+</Book>
+<Book Series="American Vampire" Number="8" Volume="2010" Year="2010">
+<Database Name="cv" Series="32051" Issue="241781" />
+</Book>
+<Book Series="American Vampire" Number="9" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="247293" />
+</Book>
+<Book Series="American Vampire" Number="10" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="250157" />
+</Book>
+<Book Series="American Vampire" Number="11" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="259142" />
+</Book>
+<Book Series="American Vampire" Number="12" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="263706" />
+</Book>
+<Book Series="American Vampire" Number="13" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="267062" />
+</Book>
+<Book Series="American Vampire" Number="14" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="268958" />
+</Book>
+<Book Series="American Vampire" Number="15" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="271418" />
+</Book>
+<Book Series="American Vampire" Number="16" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="276565" />
+</Book>
+<Book Series="American Vampire" Number="17" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="281721" />
+</Book>
+<Book Series="American Vampire" Number="18" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="288472" />
+</Book>
+<Book Series="American Vampire: Survival of the Fittest" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40521" Issue="273069" />
+</Book>
+<Book Series="American Vampire: Survival of the Fittest" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40521" Issue="278678" />
+</Book>
+<Book Series="American Vampire: Survival of the Fittest" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40521" Issue="285541" />
+</Book>
+<Book Series="American Vampire: Survival of the Fittest" Number="4" Volume="2011" Year="2011">
+<Database Name="cv" Series="40521" Issue="292570" />
+</Book>
+<Book Series="American Vampire: Survival of the Fittest" Number="5" Volume="2011" Year="2011">
+<Database Name="cv" Series="40521" Issue="294831" />
+</Book>
+<Book Series="American Vampire" Number="19" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="293578" />
+</Book>
+<Book Series="American Vampire" Number="20" Volume="2010" Year="2011">
+<Database Name="cv" Series="32051" Issue="300976" />
+</Book>
+<Book Series="American Vampire" Number="21" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="306482" />
+</Book>
+<Book Series="American Vampire" Number="22" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="308406" />
+</Book>
+<Book Series="American Vampire" Number="23" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="312699" />
+</Book>
+<Book Series="American Vampire" Number="24" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="316637" />
+</Book>
+<Book Series="American Vampire" Number="25" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="324769" />
+</Book>
+<Book Series="American Vampire" Number="26" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="333400" />
+</Book>
+<Book Series="American Vampire" Number="27" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="337483" />
+</Book>
+<Book Series="American Vampire" Number="28" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="342858" />
+</Book>
+<Book Series="American Vampire: Lord of Nightmares" Number="1" Volume="2012" Year="2012">
+<Database Name="cv" Series="49636" Issue="340152" />
+</Book>
+<Book Series="American Vampire: Lord of Nightmares" Number="2" Volume="2012" Year="2012">
+<Database Name="cv" Series="49636" Issue="345468" />
+</Book>
+<Book Series="American Vampire: Lord of Nightmares" Number="3" Volume="2012" Year="2012">
+<Database Name="cv" Series="49636" Issue="349724" />
+</Book>
+<Book Series="American Vampire: Lord of Nightmares" Number="4" Volume="2012" Year="2012">
+<Database Name="cv" Series="49636" Issue="356846" />
+</Book>
+<Book Series="American Vampire: Lord of Nightmares" Number="5" Volume="2012" Year="2012">
+<Database Name="cv" Series="49636" Issue="362161" />
+</Book>
+<Book Series="American Vampire" Number="29" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="347361" />
+</Book>
+<Book Series="American Vampire" Number="30" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="354147" />
+</Book>
+<Book Series="American Vampire" Number="31" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="358833" />
+</Book>
+<Book Series="American Vampire" Number="32" Volume="2010" Year="2012">
+<Database Name="cv" Series="32051" Issue="364096" />
+</Book>
+<Book Series="American Vampire" Number="33" Volume="2010" Year="2013">
+<Database Name="cv" Series="32051" Issue="370275" />
+</Book>
+<Book Series="American Vampire" Number="34" Volume="2010" Year="2013">
+<Database Name="cv" Series="32051" Issue="376604" />
+</Book>
+<Book Series="American Vampire: The Long Road to Hell" Number="1" Volume="2013" Year="2013">
+<Database Name="cv" Series="63233" Issue="410272" />
+</Book>
+<Book Series="American Vampire Anthology" Number="1" Volume="2013" Year="2013">
+<Database Name="cv" Series="66766" Issue="423627" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="1" Volume="2014" Year="2014">
+<Database Name="cv" Series="72399" Issue="447946" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="2" Volume="2014" Year="2014">
+<Database Name="cv" Series="72399" Issue="450520" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="3" Volume="2014" Year="2014">
+<Database Name="cv" Series="72399" Issue="453353" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="4" Volume="2014" Year="2014">
+<Database Name="cv" Series="72399" Issue="459113" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="5" Volume="2014" Year="2014">
+<Database Name="cv" Series="72399" Issue="466977" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="6" Volume="2014" Year="2015">
+<Database Name="cv" Series="72399" Issue="478561" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="7" Volume="2014" Year="2015">
+<Database Name="cv" Series="72399" Issue="486104" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="8" Volume="2014" Year="2015">
+<Database Name="cv" Series="72399" Issue="493752" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="9" Volume="2014" Year="2015">
+<Database Name="cv" Series="72399" Issue="496853" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="10" Volume="2014" Year="2015">
+<Database Name="cv" Series="72399" Issue="501587" />
+</Book>
+<Book Series="American Vampire: Second Cycle" Number="11" Volume="2014" Year="2016">
+<Database Name="cv" Series="72399" Issue="506616" />
+</Book>
+<Book Series="American Vampire Anthology" Number="2" Volume="2013" Year="2016">
+<Database Name="cv" Series="66766" Issue="552966" />
+</Book>
+<Book Series="American Vampire 1976" Number="1" Volume="2020" Year="2020">
+<Database Name="cv" Series="130865" Issue="806762" />
+</Book>
+<Book Series="American Vampire 1976" Number="2" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="817444" />
+</Book>
+<Book Series="American Vampire 1976" Number="3" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="820392" />
+</Book>
+<Book Series="American Vampire 1976" Number="4" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="824265" />
+</Book>
+<Book Series="American Vampire 1976" Number="5" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="827747" />
+</Book>
+<Book Series="American Vampire 1976" Number="6" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="834432" />
+</Book>
+<Book Series="American Vampire 1976" Number="7" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="842619" />
+</Book>
+<Book Series="American Vampire 1976" Number="8" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="848254" />
+</Book>
+<Book Series="American Vampire 1976" Number="9" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="858432" />
+</Book>
+<Book Series="American Vampire 1976" Number="10" Volume="2020" Year="2021">
+<Database Name="cv" Series="130865" Issue="876253" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
