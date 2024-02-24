e1ab732b101ff54fc40a3bf9520ceafc31482555
flips22 <1898114+flips22@users.noreply.github.com>
2024-02-24 08:40:52 -0600

    CBRO Transformers (IDW)

---
+++ Other/IDW/Transformers/[IDW] [2005-2017] Transformers (CBRO).cbl
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[IDW] [2005-2017] Transformers (CBRO)</Name>
+<NumIssues>0</NumIssues>
+<Books>
+</Books>
+<Matchers />
+</ReadingList>
