869211e9845e7935b65690a3c2ebf76e9560d960
Cducharme84 <131784515+Cducharme84@users.noreply.github.com>
2024-04-10 08:20:07 -0400

    Space Goat's brief, but productive Evil Dead 2 run
    Space Goat for a short time had the license to the Evil Dead 2 comic rights, and boy did they shoot some books out in that 2 year span! Tales of Ex-Mortis is mostly self contained one shot issues not really in the continuity but as it's the only release they had not explicitly in the same continuity I threw it in for a complete reading of the company's Evil Dead offering.

---
+++ Other/unsorted/Space Goat Evil Dead 2.cbl
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>Space Goat Evil Dead 2</Name>
+<NumIssues>20</NumIssues>
+<Books>
+<Book Series="Evil Dead 2: Beyond Dead By Dawn" Number="1" Volume="2015" Year="2015">
+<Database Name="cv" Series="82869" Issue="493129" />
+</Book>
+<Book Series="Evil Dead 2: Beyond Dead By Dawn" Number="2" Volume="2015" Year="2015">
+<Database Name="cv" Series="82869" Issue="494430" />
+</Book>
+<Book Series="Evil Dead 2: Beyond Dead By Dawn" Number="3" Volume="2015" Year="2015">
+<Database Name="cv" Series="82869" Issue="505031" />
+</Book>
+<Book Series="Evil Dead 2: Revenge of Hitler" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="89158" Issue="521406" />
+</Book>
+<Book Series="Evil Dead 2: Revenge of Dracula" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="95010" Issue="554049" />
+</Book>
+<Book Series="Evil Dead 2: Revenge of the Martians" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="95011" Issue="554050" />
+</Book>
+<Book Series="Evil Dead 2: Revenge of Jack the Ripper" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="95218" Issue="555612" />
+</Book>
+<Book Series="Evil Dead 2: Revenge of Krampus" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="95592" Issue="557444" />
+</Book>
+<Book Series="Evil Dead 2: A Merry Deadite Christmas" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="96285" Issue="563798" />
+</Book>
+<Book Series="Evil Dead 2: Cradle of the Damned" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="87313" Issue="511025" />
+</Book>
+<Book Series="Evil Dead 2: Cradle of the Damned" Number="2" Volume="2016" Year="2016">
+<Database Name="cv" Series="87313" Issue="521405" />
+</Book>
+<Book Series="Evil Dead 2: Cradle of the Damned" Number="3" Volume="2016" Year="2016">
+<Database Name="cv" Series="87313" Issue="527232" />
+</Book>
+<Book Series="Evil Dead 2: Dark Ones Rising" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="95009" Issue="554048" />
+</Book>
+<Book Series="Evil Dead 2: Dark Ones Rising" Number="2" Volume="2016" Year="2016">
+<Database Name="cv" Series="95009" Issue="555611" />
+</Book>
+<Book Series="Evil Dead 2: Dark Ones Rising" Number="3" Volume="2016" Year="2016">
+<Database Name="cv" Series="95009" Issue="571745" />
+</Book>
+<Book Series="Evil Dead 2: Revenge of Evil Ed" Number="1" Volume="2017" Year="2017">
+<Database Name="cv" Series="99036" Issue="582618" />
+</Book>
+<Book Series="Evil Dead 2: Revenge of Evil Ed" Number="2" Volume="2017" Year="2017">
+<Database Name="cv" Series="99036" Issue="599948" />
+</Book>
+<Book Series="Evil Dead 2: Tales of the Ex-Mortis" Number="1" Volume="2015" Year="2015">
+<Database Name="cv" Series="84270" Issue="499115" />
+</Book>
+<Book Series="Evil Dead 2: Tales of the Ex-Mortis" Number="2" Volume="2015" Year="2015">
+<Database Name="cv" Series="84270" Issue="505032" />
+</Book>
+<Book Series="Evil Dead 2: Tales of the Ex-Mortis" Number="3" Volume="2015" Year="2016">
+<Database Name="cv" Series="84270" Issue="513733" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
