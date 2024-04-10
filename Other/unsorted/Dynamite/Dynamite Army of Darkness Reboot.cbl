a97e216f00fb34ad0769c47f09f4cf99158822b9
Cducharme84 <131784515+Cducharme84@users.noreply.github.com>
2024-04-10 07:47:05 -0400

    Add files via upload

---
+++ Other/unsorted/Dynamite/Dynamite Army of Darkness Reboot.cbl
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>Dynamite Army of Darkness Reboot</Name>
+<NumIssues>19</NumIssues>
+<Books>
+<Book Series="Ash and The Army of Darkness" Number="25" Volume="2014" Year="2013">
+<Database Name="cv" Series="68723" Issue="431420" />
+</Book>
+<Book Series="Ash and The Army of Darkness" Number="2" Volume="2014" Year="2013">
+<Database Name="cv" Series="68723" Issue="435594" />
+</Book>
+<Book Series="Ash and The Army of Darkness" Number="3" Volume="2014" Year="2014">
+<Database Name="cv" Series="68723" Issue="441424" />
+</Book>
+<Book Series="Ash and The Army of Darkness" Number="4" Volume="2014" Year="2014">
+<Database Name="cv" Series="68723" Issue="445825" />
+</Book>
+<Book Series="Ash and the Army of Darkness Annual" Number="1" Volume="2014" Year="2014">
+<Database Name="cv" Series="72584" Issue="448983" />
+</Book>
+<Book Series="Ash and The Army of Darkness" Number="5" Volume="2014" Year="2014">
+<Database Name="cv" Series="68723" Issue="449603" />
+</Book>
+<Book Series="Ash and The Army of Darkness" Number="6" Volume="2014" Year="2014">
+<Database Name="cv" Series="68723" Issue="451097" />
+</Book>
+<Book Series="Ash and The Army of Darkness" Number="7" Volume="2014" Year="2014">
+<Database Name="cv" Series="68723" Issue="452861" />
+</Book>
+<Book Series="Ash and The Army of Darkness" Number="8" Volume="2014" Year="2014">
+<Database Name="cv" Series="68723" Issue="456035" />
+</Book>
+<Book Series="Army Of Darkness: Ash Gets Hitched" Number="1" Volume="2015" Year="2014">
+<Database Name="cv" Series="75717" Issue="459687" />
+</Book>
+<Book Series="Army Of Darkness: Ash Gets Hitched" Number="2" Volume="2015" Year="2014">
+<Database Name="cv" Series="75717" Issue="463492" />
+</Book>
+<Book Series="Army of Darkness" Number="1992.1" Volume="2014" Year="2014">
+<Database Name="cv" Series="78049" Issue="469846" />
+</Book>
+<Book Series="Army Of Darkness: Ash Gets Hitched" Number="3" Volume="2015" Year="2014">
+<Database Name="cv" Series="75717" Issue="468094" />
+</Book>
+<Book Series="Army Of Darkness: Ash Gets Hitched" Number="4" Volume="2015" Year="2014">
+<Database Name="cv" Series="75717" Issue="470438" />
+</Book>
+<Book Series="Army of Darkness" Number="1" Volume="2014" Year="2014">
+<Database Name="cv" Series="78551" Issue="471971" />
+</Book>
+<Book Series="Army of Darkness" Number="2" Volume="2014" Year="2015">
+<Database Name="cv" Series="78551" Issue="475467" />
+</Book>
+<Book Series="Army of Darkness" Number="3" Volume="2014" Year="2015">
+<Database Name="cv" Series="78551" Issue="479269" />
+</Book>
+<Book Series="Army of Darkness" Number="4" Volume="2014" Year="2015">
+<Database Name="cv" Series="78551" Issue="481619" />
+</Book>
+<Book Series="Army of Darkness" Number="5" Volume="2014" Year="2015">
+<Database Name="cv" Series="78551" Issue="484912" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
