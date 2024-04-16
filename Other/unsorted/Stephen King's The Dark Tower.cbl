a421e58dd118a63f0790444b43cf4dd821436e74
Cducharme84 <131784515+Cducharme84@users.noreply.github.com>
2024-04-16 10:40:50 -0400

    Stephen King's DT - Removed all TPBs replaced with singles
    Also added the End-World almanac.

--- Other/unsorted/Stephen King's The Dark Tower.cbl
+++ Other/unsorted/Stephen King's The Dark Tower.cbl
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 <Name>Stephen King's The Dark Tower</Name>
-<NumIssues>63</NumIssues>
+<NumIssues>89</NumIssues>
 <Books>
 <Book Series="Dark Tower: The Gunslinger Born" Number="1" Volume="2007" Year="2007">
 <Database Name="cv" Series="18161" Issue="106362" />
@@ -39,8 +39,23 @@
 <Book Series="Dark Tower: The Long Road Home" Number="5" Volume="2008" Year="2008">
 <Database Name="cv" Series="20800" Issue="132362" />
 </Book>
-<Book Series="Dark Tower: The Gunslinger: Treachery" Number="1" Volume="2009" Year="2009">
-<Database Name="cv" Series="92520" Issue="540969" />
+<Book Series="The Dark Tower: Treachery" Number="1" Volume="2008" Year="2008">
+<Database Name="cv" Series="22982" Issue="138580" />
+</Book>
+<Book Series="The Dark Tower: Treachery" Number="2" Volume="2008" Year="2008">
+<Database Name="cv" Series="22982" Issue="140151" />
+</Book>
+<Book Series="The Dark Tower: Treachery" Number="3" Volume="2008" Year="2009">
+<Database Name="cv" Series="22982" Issue="142873" />
+</Book>
+<Book Series="The Dark Tower: Treachery" Number="4" Volume="2008" Year="2009">
+<Database Name="cv" Series="22982" Issue="149470" />
+</Book>
+<Book Series="The Dark Tower: Treachery" Number="5" Volume="2008" Year="2009">
+<Database Name="cv" Series="22982" Issue="150704" />
+</Book>
+<Book Series="The Dark Tower: Treachery" Number="6" Volume="2008" Year="2009">
+<Database Name="cv" Series="22982" Issue="152810" />
 </Book>
 <Book Series="Dark Tower: The Sorcerer" Number="1" Volume="2009" Year="2009">
 <Database Name="cv" Series="7658" Issue="155480" />
@@ -78,20 +93,80 @@
 <Book Series="Dark Tower: Battle of Jericho Hill" Number="5" Volume="2010" Year="2010">
 <Database Name="cv" Series="30212" Issue="208766" />
 </Book>
-<Book Series="Dark Tower: The Gunslinger - The Journey Begins" Number="1" Volume="2011" Year="2012">
-<Database Name="cv" Series="76497" Issue="462619" />
+<Book Series="Dark Tower: The Gunslinger - The Journey Begins" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="33239" Issue="214854" />
 </Book>
-<Book Series="The Dark Tower: The Gunslinger - The Little Sisters of Eluria" Number="1" Volume="2013" Year="2013">
-<Database Name="cv" Series="55898" Issue="380698" />
+<Book Series="Dark Tower: The Gunslinger - The Journey Begins" Number="2" Volume="2010" Year="2010">
+<Database Name="cv" Series="33239" Issue="219975" />
 </Book>
-<Book Series="Dark Tower: The Gunslinger: The Battle of Tull" Number="1" Volume="2011" Year="2012">
-<Database Name="cv" Series="92519" Issue="540968" />
+<Book Series="Dark Tower: The Gunslinger - The Journey Begins" Number="3" Volume="2010" Year="2010">
+<Database Name="cv" Series="33239" Issue="226226" />
 </Book>
-<Book Series="The Dark Tower: The Gunslinger - The Way Station" Number="1" Volume="2013" Year="2013">
-<Database Name="cv" Series="65188" Issue="417208" />
+<Book Series="Dark Tower: The Gunslinger - The Journey Begins" Number="4" Volume="2010" Year="2010">
+<Database Name="cv" Series="33239" Issue="230931" />
 </Book>
-<Book Series="The Dark Tower: The Gunslinger - The Man In Black" Number="1" Volume="2012" Year="2012">
-<Database Name="cv" Series="55606" Issue="379156" />
+<Book Series="Dark Tower: The Gunslinger - The Journey Begins" Number="5" Volume="2010" Year="2010">
+<Database Name="cv" Series="33239" Issue="235728" />
+</Book>
+<Book Series="The Dark Tower: The Gunslinger - The Little Sisters of Eluria" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="37406" Issue="248807" />
+</Book>
+<Book Series="The Dark Tower: The Gunslinger - The Little Sisters of Eluria" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="37406" Issue="259061" />
+</Book>
+<Book Series="The Dark Tower: The Gunslinger - The Little Sisters of Eluria" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="37406" Issue="262483" />
+</Book>
+<Book Series="The Dark Tower: The Gunslinger - The Little Sisters of Eluria" Number="4" Volume="2011" Year="2011">
+<Database Name="cv" Series="37406" Issue="266056" />
+</Book>
+<Book Series="The Dark Tower: The Gunslinger - The Little Sisters of Eluria" Number="5" Volume="2011" Year="2011">
+<Database Name="cv" Series="37406" Issue="268340" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Battle of Tull" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="41365" Issue="279507" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Battle of Tull" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="41365" Issue="282398" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Battle of Tull" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="41365" Issue="290939" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Battle of Tull" Number="4" Volume="2011" Year="2011">
+<Database Name="cv" Series="41365" Issue="294341" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Battle of Tull" Number="5" Volume="2011" Year="2011">
+<Database Name="cv" Series="41365" Issue="301084" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Way Station" Number="1" Volume="2012" Year="2012">
+<Database Name="cv" Series="45071" Issue="310233" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Way Station" Number="2" Volume="2012" Year="2012">
+<Database Name="cv" Series="45071" Issue="311020" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Way Station" Number="3" Volume="2012" Year="2012">
+<Database Name="cv" Series="45071" Issue="315929" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Way Station" Number="4" Volume="2012" Year="2012">
+<Database Name="cv" Series="45071" Issue="325098" />
+</Book>
+<Book Series="Dark Tower: The Gunslinger - The Way Station" Number="5" Volume="2012" Year="2012">
+<Database Name="cv" Series="45071" Issue="333538" />
+</Book>
+<Book Series="Grimm Fairy Tales Angel: One-Shot" Number="1" Volume="2012" Year="2012">
+<Database Name="cv" Series="49878" Issue="342293" />
+</Book>
+<Book Series="Grimm Fairy Tales Angel: One-Shot" Number="2" Volume="2012" Year="2012">
+<Database Name="cv" Series="49878" Issue="346946" />
+</Book>
+<Book Series="Grimm Fairy Tales Angel: One-Shot" Number="3" Volume="2012" Year="2012">
+<Database Name="cv" Series="49878" Issue="351098" />
+</Book>
+<Book Series="Grimm Fairy Tales Angel: One-Shot" Number="4" Volume="2012" Year="2012">
+<Database Name="cv" Series="49878" Issue="357778" />
+</Book>
+<Book Series="Grimm Fairy Tales Angel: One-Shot" Number="5" Volume="2012" Year="2012">
+<Database Name="cv" Series="49878" Issue="362425" />
 </Book>
 <Book Series="Dark Tower: The Gunslinger - Sheemie's Tale" Number="1" Volume="2013" Year="2013">
 <Database Name="cv" Series="57296" Issue="386476" />
@@ -183,14 +258,17 @@
 <Book Series="Dark Tower: The Drawing of the Three - The Sailor" Number="5" Volume="2016" Year="2017">
 <Database Name="cv" Series="94794" Issue="580745" />
 </Book>
+<Book Series="Marvel Spotlight: Dark Tower" Number="1" Volume="2007" Year="2007">
+<Database Name="cv" Series="33968" Issue="221413" />
+</Book>
 <Book Series="Dark Tower: Gunslinger's Guidebook" Number="1" Volume="2007" Year="2007">
 <Database Name="cv" Series="20562" Issue="123149" />
 </Book>
+<Book Series="Dark Tower: End-World Almanac" Number="1" Volume="2008" Year="2008">
+<Database Name="cv" Series="22281" Issue="133983" />
+</Book>
 <Book Series="Dark Tower: Guide To Gilead" Number="1" Volume="2009" Year="2009">
 <Database Name="cv" Series="26246" Issue="155709" />
-</Book>
-<Book Series="Marvel Spotlight: Dark Tower" Number="1" Volume="2007" Year="2007">
-<Database Name="cv" Series="33968" Issue="221413" />
 </Book>
 </Books>
 <Matchers />
