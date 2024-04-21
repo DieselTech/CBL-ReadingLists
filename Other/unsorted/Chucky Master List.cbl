cc6df20ca67f798a537de61805b38ebc5700991d
Cducharme84 <131784515+Cducharme84@users.noreply.github.com>
2024-04-21 09:20:10 -0400

    Chucky Master list added
    Part of the slasher CBL series, these range from 1991's Innovation books to Devil's Due's aborted mini-series in 2009.

---
+++ Other/unsorted/Chucky Master List.cbl
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>Chucky Master List</Name>
+<NumIssues>17</NumIssues>
+<Books>
+<Book Series="Child's Play" Number="1" Volume="1991" Year="1991">
+<Database Name="cv" Series="21726" Issue="131052" />
+</Book>
+<Book Series="Child's Play" Number="2" Volume="1991" Year="1991">
+<Database Name="cv" Series="21726" Issue="131056" />
+</Book>
+<Book Series="Child's Play" Number="3" Volume="1991" Year="1991">
+<Database Name="cv" Series="21726" Issue="131057" />
+</Book>
+<Book Series="Child's Play" Number="4" Volume="1991" Year="1991">
+<Database Name="cv" Series="21726" Issue="131058" />
+</Book>
+<Book Series="Child's Play" Number="5" Volume="1991" Year="1991">
+<Database Name="cv" Series="21726" Issue="131059" />
+</Book>
+<Book Series="Child's Play 2" Number="1" Volume="1992" Year="1992">
+<Database Name="cv" Series="21729" Issue="131061" />
+</Book>
+<Book Series="Child's Play 2" Number="2" Volume="1992" Year="1992">
+<Database Name="cv" Series="21729" Issue="131062" />
+</Book>
+<Book Series="Child's Play 2" Number="3" Volume="1992" Year="1992">
+<Database Name="cv" Series="21729" Issue="131063" />
+</Book>
+<Book Series="Child's Play 3" Number="1" Volume="1992" Year="1993">
+<Database Name="cv" Series="21730" Issue="131070" />
+</Book>
+<Book Series="Child's Play 3" Number="2" Volume="1992" Year="1993">
+<Database Name="cv" Series="21730" Issue="131064" />
+</Book>
+<Book Series="Child's Play 3" Number="3" Volume="1992" Year="1992">
+<Database Name="cv" Series="21730" Issue="131066" />
+</Book>
+<Book Series="Chucky" Number="1" Volume="2007" Year="2007">
+<Database Name="cv" Series="22917" Issue="229061" />
+</Book>
+<Book Series="Chucky" Number="2" Volume="2007" Year="2007">
+<Database Name="cv" Series="22917" Issue="231199" />
+</Book>
+<Book Series="Chucky" Number="3" Volume="2007" Year="2007">
+<Database Name="cv" Series="22917" Issue="231074" />
+</Book>
+<Book Series="Chucky" Number="4" Volume="2007" Year="2007">
+<Database Name="cv" Series="22917" Issue="137771" />
+</Book>
+<Book Series="Hack/Slash vs. Chucky" Number="1" Volume="2007" Year="2007">
+<Database Name="cv" Series="19306" Issue="115847" />
+</Book>
+<Book Series="Chucky" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="25526" Issue="150584" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
