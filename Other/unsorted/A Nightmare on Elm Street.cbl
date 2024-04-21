3a58ca6029aba192f70daa18bf9abee122860b97
Cducharme84 <131784515+Cducharme84@users.noreply.github.com>
2024-04-21 08:29:21 -0400

    A Nightmare on Elm Street Master List
    From Innovation's 3 series (including the special 3-D issue to mirror the final act in Freddy's Dead) to the very last Wildstorm issues with Krueger facing off against Ash and Jason twice. Sadly he's not been brought back in comic form (unless you count his many appearances in MAD since) so Krueger's comic journey ends in 2009.

---
+++ Other/unsorted/A Nightmare on Elm Street.cbl
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>A Nightmare on Elm Street</Name>
+<NumIssues>40</NumIssues>
+<Books>
+<Book Series="Freddy Krueger's A Nightmare on Elm Street" Number="1" Volume="1989" Year="1989">
+<Database Name="cv" Series="25450" Issue="150178" />
+</Book>
+<Book Series="Freddy Krueger's A Nightmare on Elm Street" Number="2" Volume="1989" Year="1989">
+<Database Name="cv" Series="25450" Issue="212776" />
+</Book>
+<Book Series="Nightmares on Elm Street" Number="1" Volume="1991" Year="1991">
+<Database Name="cv" Series="25454" Issue="150189" />
+</Book>
+<Book Series="Nightmares on Elm Street" Number="2" Volume="1991" Year="1991">
+<Database Name="cv" Series="25454" Issue="150190" />
+</Book>
+<Book Series="Nightmares on Elm Street" Number="3" Volume="1991" Year="1991">
+<Database Name="cv" Series="25454" Issue="150191" />
+</Book>
+<Book Series="Nightmares on Elm Street" Number="4" Volume="1991" Year="1992">
+<Database Name="cv" Series="25454" Issue="150192" />
+</Book>
+<Book Series="Nightmares on Elm Street" Number="5" Volume="1991" Year="1992">
+<Database Name="cv" Series="25454" Issue="150193" />
+</Book>
+<Book Series="Nightmares on Elm Street" Number="6" Volume="1991" Year="1992">
+<Database Name="cv" Series="25454" Issue="150194" />
+</Book>
+<Book Series="Freddy's Dead: The Final Nightmare" Number="1" Volume="1991" Year="1991">
+<Database Name="cv" Series="25453" Issue="150182" />
+</Book>
+<Book Series="Freddy's Dead: The Final Nightmare" Number="2" Volume="1991" Year="1991">
+<Database Name="cv" Series="25453" Issue="150186" />
+</Book>
+<Book Series="Freddy's Dead: The Final Nightmare" Number="3" Volume="1991" Year="1991">
+<Database Name="cv" Series="25453" Issue="150188" />
+</Book>
+<Book Series="Freddy's Dead: The Final Nightmare" Number="3-D" Volume="1991" Year="1992">
+<Database Name="cv" Series="25453" Issue="150563" />
+</Book>
+<Book Series="A Nightmare on Elm Street: The Beginning" Number="1" Volume="1992" Year="1992">
+<Database Name="cv" Series="25452" Issue="150180" />
+</Book>
+<Book Series="A Nightmare on Elm Street: The Beginning" Number="2" Volume="1992" Year="1992">
+<Database Name="cv" Series="25452" Issue="150181" />
+</Book>
+<Book Series="A Nightmare on Elm Street: Special" Number="1" Volume="2005" Year="2005">
+<Database Name="cv" Series="21261" Issue="128165" />
+</Book>
+<Book Series="A Nightmare on Elm Street: Paranoid" Number="1" Volume="2005" Year="2005">
+<Database Name="cv" Series="21260" Issue="128164" />
+</Book>
+<Book Series="A Nightmare on Elm Street: Paranoid" Number="2" Volume="2005" Year="2006">
+<Database Name="cv" Series="21260" Issue="128245" />
+</Book>
+<Book Series="A Nightmare on Elm Street: Paranoid" Number="3" Volume="2005" Year="2006">
+<Database Name="cv" Series="21260" Issue="128246" />
+</Book>
+<Book Series="A Nightmare on Elm Street: Fearbook" Number="1" Volume="2006" Year="2006">
+<Database Name="cv" Series="21259" Issue="128163" />
+</Book>
+<Book Series="A Nightmare on Elm Street" Number="1" Volume="2006" Year="2006">
+<Database Name="cv" Series="25449" Issue="150177" />
+</Book>
+<Book Series="A Nightmare on Elm Street" Number="2" Volume="2006" Year="2007">
+<Database Name="cv" Series="25449" Issue="150523" />
+</Book>
+<Book Series="A Nightmare on Elm Street" Number="3" Volume="2006" Year="2007">
+<Database Name="cv" Series="25449" Issue="150527" />
+</Book>
+<Book Series="A Nightmare on Elm Street" Number="4" Volume="2006" Year="2007">
+<Database Name="cv" Series="25449" Issue="150524" />
+</Book>
+<Book Series="A Nightmare on Elm Street" Number="5" Volume="2006" Year="2007">
+<Database Name="cv" Series="25449" Issue="150534" />
+</Book>
+<Book Series="A Nightmare on Elm Street" Number="6" Volume="2006" Year="2007">
+<Database Name="cv" Series="25449" Issue="150535" />
+</Book>
+<Book Series="A Nightmare on Elm Street" Number="7" Volume="2006" Year="2007">
+<Database Name="cv" Series="25449" Issue="150531" />
+</Book>
+<Book Series="A Nightmare on Elm Street" Number="8" Volume="2006" Year="2007">
+<Database Name="cv" Series="25449" Issue="150536" />
+</Book>
+<Book Series="New Line Cinemas Tales Of Horror" Number="1" Volume="2007" Year="2007">
+<Database Name="cv" Series="19121" Issue="114247" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness)" Number="1" Volume="2007" Year="2007">
+<Database Name="cv" Series="19563" Issue="116983" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness)" Number="2" Volume="2007" Year="2007">
+<Database Name="cv" Series="19563" Issue="118673" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness)" Number="3" Volume="2007" Year="2007">
+<Database Name="cv" Series="19563" Issue="121072" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness)" Number="4" Volume="2007" Year="2008">
+<Database Name="cv" Series="19563" Issue="123325" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness)" Number="5" Volume="2007" Year="2008">
+<Database Name="cv" Series="19563" Issue="124441" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness)" Number="6" Volume="2007" Year="2008">
+<Database Name="cv" Series="19563" Issue="126107" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness): The Nightmare Warriors" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26955" Issue="162362" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness): The Nightmare Warriors" Number="2" Volume="2009" Year="2009">
+<Database Name="cv" Series="26955" Issue="164928" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness): The Nightmare Warriors" Number="3" Volume="2009" Year="2009">
+<Database Name="cv" Series="26955" Issue="170609" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness): The Nightmare Warriors" Number="4" Volume="2009" Year="2009">
+<Database Name="cv" Series="26955" Issue="173433" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness): The Nightmare Warriors" Number="5" Volume="2009" Year="2009">
+<Database Name="cv" Series="26955" Issue="179938" />
+</Book>
+<Book Series="Freddy vs Jason vs Ash (of Army of Darkness): The Nightmare Warriors" Number="6" Volume="2009" Year="2010">
+<Database Name="cv" Series="26955" Issue="189590" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
