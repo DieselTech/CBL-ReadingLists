49ae1dd11f7493f3cb8e760a02ec35ac4b29b50d
Nebelherr23 <138651224+Nebelherr23@users.noreply.github.com>
2024-05-01 21:33:17 +0200

    Added Batman Knightfall
    Discord Contribution by The Scummiest of Them All

---
+++ DC/Events/unsorted/[DC Comics] [1992-1995] Batman - Knightfall.cbl
@@ -0,0 +1,331 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>Batman: Knightfall</Name>
+  <Books>
+    <Book Series="Batman: Sword of Azrael" Number="1" Volume="1992" Year="1992">
+      <Id>9d2463dd-f8d3-496d-b1fe-c913962e8b55</Id>
+    </Book>
+    <Book Series="Batman: Sword of Azrael" Number="2" Volume="1992" Year="1992">
+      <Id>429b5eed-72b3-48d1-8808-b3f1b3806348</Id>
+    </Book>
+    <Book Series="Batman: Sword of Azrael" Number="3" Volume="1992" Year="1992">
+      <Id>38c05495-6abb-48e7-a501-9f64faa78653</Id>
+    </Book>
+    <Book Series="Batman: Sword of Azrael" Number="4" Volume="1992" Year="1993">
+      <Id>ab8a9c28-57b0-400f-9241-4c220afd5af2</Id>
+    </Book>
+    <Book Series="Batman: Vengeance of Bane" Number="1" Volume="1993" Year="1993">
+      <Id>b4cbb099-ff64-4bce-9d9f-867032dd024a</Id>
+    </Book>
+    <Book Series="Batman" Number="484" Volume="1940" Year="1992">
+      <Id>dff1849f-1c01-48e5-9e8a-8af4d4010b1d</Id>
+    </Book>
+    <Book Series="Batman" Number="485" Volume="1940" Year="1992">
+      <Id>6e3e296a-6485-40f7-adad-3e31a1ab1683</Id>
+    </Book>
+    <Book Series="Batman" Number="486" Volume="1940" Year="1992">
+      <Id>743edf50-179e-4453-aa09-dabe53a89f65</Id>
+    </Book>
+    <Book Series="Batman" Number="487" Volume="1940" Year="1992">
+      <Id>d982c488-2719-48c8-9ae5-e5008ab0a671</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="654" Volume="1937" Year="1992">
+      <Id>c9fef1ee-978a-4284-92d2-ac221b2f8c13</Id>
+    </Book>
+    <Book Series="Batman" Number="488" Volume="1940" Year="1993">
+      <Id>8779d216-dfd8-466e-939c-513e049db1a5</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="655" Volume="1937" Year="1993">
+      <Id>5b564f14-93a1-4e11-83d9-180cbcb7e97e</Id>
+    </Book>
+    <Book Series="Batman" Number="489" Volume="1940" Year="1993">
+      <Id>60742e72-94dc-420f-8e00-bd7a77259ea9</Id>
+    </Book>
+    <Book Series="Batman" Number="490" Volume="1940" Year="1993">
+      <Id>6db449b4-ec1f-43c0-a01f-816a3e1702e0</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="656" Volume="1937" Year="1993">
+      <Id>de9d363b-a62e-4868-9295-3d842ec63df7</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="657" Volume="1937" Year="1993">
+      <Id>a147491d-a976-4ab0-bf29-f4069acec104</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="658" Volume="1937" Year="1993">
+      <Id>3f969cec-27c5-40c9-b4b7-13d0a65f9ed8</Id>
+    </Book>
+    <Book Series="Batman" Number="491" Volume="1940" Year="1993">
+      <Id>335a2140-c05d-4a6e-8e54-ccbce0f2f3aa</Id>
+    </Book>
+    <Book Series="Batman" Number="492" Volume="1940" Year="1993">
+      <Id>89da1767-45b5-4661-bc94-2b0400b81e47</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="659" Volume="1937" Year="1993">
+      <Id>b9c514ed-5639-41c9-a066-231537d98e35</Id>
+    </Book>
+    <Book Series="Batman" Number="493" Volume="1940" Year="1993">
+      <Id>f256b20a-13ca-4ca6-bbbf-6b02851d6286</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="660" Volume="1937" Year="1993">
+      <Id>6d51d6a7-4a0d-428d-a564-26b37c78cab5</Id>
+    </Book>
+    <Book Series="Batman" Number="494" Volume="1940" Year="1993">
+      <Id>2e3521b3-6a87-46b0-8a15-4968418f1331</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="661" Volume="1937" Year="1993">
+      <Id>b6debfb7-8805-457b-8f79-d84e9eb6dfae</Id>
+    </Book>
+    <Book Series="Batman" Number="495" Volume="1940" Year="1993">
+      <Id>563b9333-572c-444c-921a-8615d3073515</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="662" Volume="1937" Year="1993">
+      <Id>999cd234-01f6-4e3f-bea8-79400ca221a2</Id>
+    </Book>
+    <Book Series="Batman" Number="496" Volume="1940" Year="1993">
+      <Id>c128c9ec-cb06-43c0-a584-f26616226299</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="663" Volume="1937" Year="1993">
+      <Id>71b09d06-d1ee-4dcf-9663-b65541e239c8</Id>
+    </Book>
+    <Book Series="Batman" Number="497" Volume="1940" Year="1993">
+      <Id>9d29b007-09ac-4e95-855e-84b174e525a3</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="664" Volume="1937" Year="1993">
+      <Id>c93ef756-4f8e-4bd8-9fb0-fedce2051466</Id>
+    </Book>
+    <Book Series="Showcase '93" Number="7" Volume="1993" Year="1993">
+      <Id>7a18ec0c-ce8f-44d8-98a8-c4e69ac14d54</Id>
+    </Book>
+    <Book Series="Showcase '93" Number="8" Volume="1993" Year="1993">
+      <Id>97cffb25-3aa2-4cb2-83bc-bc1e3a084b5a</Id>
+    </Book>
+    <Book Series="Batman" Number="498" Volume="1940" Year="1993">
+      <Id>292c980e-6702-4964-8cf8-5f614d44ddc8</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="16" Volume="1992" Year="1993">
+      <Id>0c8c38b4-ee44-471c-805a-c79fecee0a16</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="17" Volume="1992" Year="1993">
+      <Id>5596d345-b29e-4b93-9e1d-821d4630adbc</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="18" Volume="1992" Year="1993">
+      <Id>ffbfc1ca-8c70-4598-b5bd-7d898b140d38</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="665" Volume="1937" Year="1993">
+      <Id>c777c9c9-80fb-4751-a7ec-12ab10b4931e</Id>
+    </Book>
+    <Book Series="Batman" Number="499" Volume="1940" Year="1993">
+      <Id>d43f9848-7888-4af0-8cab-643feef088f9</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="666" Volume="1937" Year="1993">
+      <Id>859efeef-cafc-48b8-a7e0-660e46b5aaf7</Id>
+    </Book>
+    <Book Series="Batman" Number="500" Volume="1940" Year="1993">
+      <Id>8abfdd25-841f-49fc-a532-69449eec6c57</Id>
+    </Book>
+    <Book Series="Robin" Number="1" Volume="1993" Year="1993">
+      <Id>297970b4-df7f-444a-8fa1-60e3179d0489</Id>
+    </Book>
+    <Book Series="Robin" Number="2" Volume="1993" Year="1993">
+      <Id>969e3d1b-8731-426b-90dc-0e0b16614771</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="667" Volume="1937" Year="1993">
+      <Id>7f040c4b-e580-4f1e-9da1-bb04b3145673</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="668" Volume="1937" Year="1993">
+      <Id>b05aabe1-64de-4dfb-9a3b-96282b8cf95e</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="19" Volume="1992" Year="1993">
+      <Id>da36c9e3-81c3-46c8-85ce-86a5542a1886</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="20" Volume="1992" Year="1993">
+      <Id>016660b8-97f1-4175-9b78-529e34ac12af</Id>
+    </Book>
+    <Book Series="Batman" Number="501" Volume="1940" Year="1993">
+      <Id>6634bc76-64d6-4c81-9d9b-e0c03fefc516</Id>
+    </Book>
+    <Book Series="Batman" Number="502" Volume="1940" Year="1993">
+      <Id>c76f3efe-b2bc-48fd-8a07-0a5fd9cb80f8</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="669" Volume="1937" Year="1993">
+      <Id>04402217-6bd4-42aa-bc9f-622053d04662</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="670" Volume="1937" Year="1994">
+      <Id>b6c1f3f5-3303-4355-b3ba-e3bd03d2466c</Id>
+    </Book>
+    <Book Series="Catwoman" Number="6" Volume="1993" Year="1994">
+      <Id>1591b07c-12b8-4def-ae0a-ce604c70a50e</Id>
+    </Book>
+    <Book Series="Batman" Number="503" Volume="1940" Year="1994">
+      <Id>00b10d6a-1a5f-4b47-abd2-ac7e6471eda4</Id>
+    </Book>
+    <Book Series="Batman" Number="504" Volume="1940" Year="1994">
+      <Id>c6516850-a894-46dd-bb25-4832bf3d27f9</Id>
+    </Book>
+    <Book Series="Catwoman" Number="7" Volume="1993" Year="1994">
+      <Id>06cf4225-ed40-4a95-b78e-0824e22df7c7</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="671" Volume="1937" Year="1994">
+      <Id>c67e93c3-b35b-4958-9161-7973599df9da</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="672" Volume="1937" Year="1994">
+      <Id>01f32b16-f6d0-4c0a-b86d-449664473a27</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="673" Volume="1937" Year="1994">
+      <Id>bdd7923e-1146-4c8b-ab04-5f95f39a84bc</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="24" Volume="1992" Year="1994">
+      <Id>f82a04fc-2278-4a4d-bcf9-eaaae8795613</Id>
+    </Book>
+    <Book Series="Batman" Number="505" Volume="1940" Year="1994">
+      <Id>2381f5f6-e1be-4105-882e-2ca7c95c69f2</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="25" Volume="1992" Year="1994">
+      <Id>3121904e-9982-4b36-a1f6-73ebb31eb98d</Id>
+    </Book>
+    <Book Series="Batman" Number="506" Volume="1940" Year="1994">
+      <Id>35ec5c2e-c703-422b-9b29-30303823c374</Id>
+    </Book>
+    <Book Series="Batman" Number="507" Volume="1940" Year="1994">
+      <Id>ee8ad8b9-9c5f-4afe-afa2-b1c9a646129f</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="26" Volume="1992" Year="1994">
+      <Id>6637276a-8b05-41ea-b585-422fb240c290</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="27" Volume="1992" Year="1994">
+      <Id>d8af51d4-11e2-479c-a706-5ae0696b262a</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="674" Volume="1937" Year="1994">
+      <Id>66cd37de-8c72-4488-80cb-80f16af5f57b</Id>
+    </Book>
+    <Book Series="Batman" Number="508" Volume="1940" Year="1994">
+      <Id>5e3994f5-6d6f-4c6c-a304-114760225c38</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="28" Volume="1992" Year="1994">
+      <Id>f91310d6-89fe-44d2-95c7-682b39266abe</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="675" Volume="1937" Year="1994">
+      <Id>0ff787d9-93a7-4162-9d3a-fe945a80996f</Id>
+    </Book>
+    <Book Series="Showcase '94" Number="7" Volume="1994" Year="1994">
+      <Id>36bcab01-0b41-4913-85be-c2895cee5ebd</Id>
+    </Book>
+    <Book Series="Justice League Task Force" Number="5" Volume="1993" Year="1993">
+      <Id>1284b3a6-9e6b-4eca-bfaa-7708e7581f7d</Id>
+    </Book>
+    <Book Series="Justice League Task Force" Number="6" Volume="1993" Year="1993">
+      <Id>0cf17059-ca14-48c2-81f0-9bf50050e188</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="21" Volume="1992" Year="1993">
+      <Id>92ba8d00-cc4a-48e3-b0e9-03116f0be2e9</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="22" Volume="1992" Year="1993">
+      <Id>e35758c7-c7fe-4982-9e18-07078e692bb1</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="23" Volume="1992" Year="1994">
+      <Id>e0682ff5-b034-41bb-927c-3519847b452e</Id>
+    </Book>
+    <Book Series="Batman: Legends of the Dark Knight" Number="59" Volume="1989" Year="1994">
+      <Id>87582d88-bd9a-49c9-a9fa-a6cf9b13ca0d</Id>
+    </Book>
+    <Book Series="Batman: Legends of the Dark Knight" Number="60" Volume="1989" Year="1994">
+      <Id>fc51e1b6-9293-4c57-acb2-9152fc6955e5</Id>
+    </Book>
+    <Book Series="Batman: Legends of the Dark Knight" Number="61" Volume="1989" Year="1994">
+      <Id>76e4ef0a-d398-4e77-9a9e-aa43e46d44eb</Id>
+    </Book>
+    <Book Series="Robin" Number="7" Volume="1993" Year="1994">
+      <Id>f3aacd6c-0e41-48c2-af4b-cf49404e6a3c</Id>
+    </Book>
+    <Book Series="Batman" Number="509" Volume="1940" Year="1994">
+      <Id>7c131c05-71f8-4a42-8abc-f0eedcb903dd</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="29" Volume="1992" Year="1994">
+      <Id>e2366ac3-8233-4766-9521-549467c9d64c</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="676" Volume="1937" Year="1994">
+      <Id>0d4684f8-94c4-4a83-b500-8a27c57142ca</Id>
+    </Book>
+    <Book Series="Batman: Legends of the Dark Knight" Number="62" Volume="1989" Year="1994">
+      <Id>f9a7d2bc-6984-40cf-821e-be36fb57d09e</Id>
+    </Book>
+    <Book Series="Robin" Number="8" Volume="1993" Year="1994">
+      <Id>6def8c31-bf0c-4741-9c50-cc9ad0891d9b</Id>
+    </Book>
+    <Book Series="Catwoman" Number="12" Volume="1993" Year="1994">
+      <Id>fa31beb4-002e-4e09-857c-75f54880bdf7</Id>
+    </Book>
+    <Book Series="Batman" Number="510" Volume="1940" Year="1994">
+      <Id>e5e4bc9d-43b2-4f31-ab61-1d2e73a67f1c</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="30" Volume="1992" Year="1994">
+      <Id>8bc811b9-97d9-4044-ba64-7b145c64d89c</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="677" Volume="1937" Year="1994">
+      <Id>403c5c2d-36ff-4dbc-be0f-d3e565e4c1bc</Id>
+    </Book>
+    <Book Series="Batman: Legends of the Dark Knight" Number="63" Volume="1989" Year="1994">
+      <Id>b4a8a9b8-73f7-41da-a18f-27586966ca5c</Id>
+    </Book>
+    <Book Series="Robin" Number="9" Volume="1993" Year="1994">
+      <Id>ed60b9e9-40b6-46bc-957b-0dca8f48e55e</Id>
+    </Book>
+    <Book Series="Catwoman" Number="13" Volume="1993" Year="1994">
+      <Id>cfa57d90-3def-486f-b690-8c1b60d48040</Id>
+    </Book>
+    <Book Series="Showcase '94" Number="10" Volume="1994" Year="1994">
+      <Id>1bc7d412-c564-48a7-b2af-18c92ecccb3d</Id>
+    </Book>
+    <Book Series="Robin" Number="0" Volume="1993" Year="1994">
+      <Id>8264a9ba-b33d-419b-abdb-15c839aae50b</Id>
+    </Book>
+    <Book Series="Batman" Number="512" Volume="1940" Year="1994">
+      <Id>7ade6ad8-1c90-46b8-9d8c-fde1d05c6431</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="32" Volume="1992" Year="1994">
+      <Id>b7465472-bf9d-476a-9175-60e41a8e3426</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="679" Volume="1937" Year="1994">
+      <Id>68fdc2bb-b4c1-4af6-8fcd-5b5acadd77fe</Id>
+    </Book>
+    <Book Series="Robin" Number="11" Volume="1993" Year="1994">
+      <Id>b6bf5699-bca4-4211-86cb-e2823be79263</Id>
+    </Book>
+    <Book Series="Batman" Number="513" Volume="1940" Year="1994">
+      <Id>a590a958-7cc3-4a43-9c99-2eee9fb1d31e</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="33" Volume="1992" Year="1994">
+      <Id>75238b5b-bcd3-4b2d-b590-48b7416e7056</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="680" Volume="1937" Year="1994">
+      <Id>04b0ad9c-384e-4084-9c28-0b146d6d8366</Id>
+    </Book>
+    <Book Series="Robin" Number="12" Volume="1993" Year="1994">
+      <Id>4cfda2ce-2c19-430e-9195-1bf4b6cf92b3</Id>
+    </Book>
+    <Book Series="Batman" Number="514" Volume="1940" Year="1995">
+      <Id>33048b62-a869-4df7-857c-e35f95776846</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="34" Volume="1992" Year="1995">
+      <Id>dbea6367-0af8-4348-913f-d73ca5b839f7</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="681" Volume="1937" Year="1995">
+      <Id>e99a4d7a-0093-48ec-83bd-352168493c07</Id>
+    </Book>
+    <Book Series="Robin" Number="13" Volume="1993" Year="1995">
+      <Id>035a871e-10f8-4d13-9063-7922512a7436</Id>
+    </Book>
+    <Book Series="Batman" Number="515" Volume="1940" Year="1995">
+      <Id>d0b32829-317a-46b7-b06e-5922cb730965</Id>
+    </Book>
+    <Book Series="Batman: Shadow of the Bat" Number="35" Volume="1992" Year="1995">
+      <Id>12b154a5-35dd-4080-b105-f9afe1fdd8b2</Id>
+    </Book>
+    <Book Series="Detective Comics" Number="682" Volume="1937" Year="1995">
+      <Id>82696d9b-6073-4876-b882-1668dd42d82e</Id>
+    </Book>
+    <Book Series="Robin" Number="14" Volume="1993" Year="1995">
+      <Id>7691d93c-92a8-4b06-95ef-a79554159ad4</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
