a603845b28ca6b3c2505f3bff95feeb96d62d716
flips22 <1898114+flips22@users.noreply.github.com>
2024-05-01 22:35:48 -0500

    Moved Knightfall and added beast world from The Scummiest of Them All (who doesn't seem to be that scummy tbh)

---
+++ DC/Events/[DC Comics] [2023-2024] Titans - Beast World.cbl
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[DC Comics] [2023-2024] Titans - Beast World</Name>
+<NumIssues>22</NumIssues>
+<Books>
+<Book Series="Tales of the Titans" Number="4" Volume="2023" Year="2023">
+<Database Name="cv" Series="152251" Issue="1026525" />
+</Book>
+<Book Series="Titans" Number="5" Volume="2023" Year="2024">
+<Database Name="cv" Series="150428" Issue="1030779" />
+</Book>
+<Book Series="Titans: Beast World Evolution" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="154830" Issue="1030781" />
+</Book>
+<Book Series="Nightwing" Number="109" Volume="2016" Year="2024">
+<Database Name="cv" Series="92569" Issue="1035447" />
+</Book>
+<Book Series="Titans: Beast World" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="154912" Issue="1031594" />
+</Book>
+<Book Series="Titans: Beast World: Waller Rising" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="155124" Issue="1033111" />
+</Book>
+<Book Series="Titans: Beast World Tour: Metropolis" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="155123" Issue="1033110" />
+</Book>
+<Book Series="Titans: Beast World" Number="2" Volume="2024" Year="2024">
+<Database Name="cv" Series="154912" Issue="1034146" />
+</Book>
+<Book Series="Titans: Beast World Tour: Gotham" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="155310" Issue="1034145" />
+</Book>
+<Book Series="Action Comics" Number="1060" Volume="2016" Year="2024">
+<Database Name="cv" Series="91078" Issue="1034139" />
+</Book>
+<Book Series="Titans: Beast World Tour: Central City" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="155469" Issue="1035450" />
+</Book>
+<Book Series="Titans: Beast World" Number="3" Volume="2024" Year="2024">
+<Database Name="cv" Series="154912" Issue="1038116" />
+</Book>
+<Book Series="Titans" Number="6" Volume="2023" Year="2024">
+<Database Name="cv" Series="150428" Issue="1038117" />
+</Book>
+<Book Series="Titans: Beast World Tour: Atlantis" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="155831" Issue="1038823" />
+</Book>
+<Book Series="Titans: Beast World" Number="4" Volume="2024" Year="2024">
+<Database Name="cv" Series="154912" Issue="1039466" />
+</Book>
+<Book Series="Titans" Number="7" Volume="2023" Year="2024">
+<Database Name="cv" Series="150428" Issue="1040771" />
+</Book>
+<Book Series="Nightwing" Number="110" Volume="2016" Year="2024">
+<Database Name="cv" Series="92569" Issue="1040768" />
+</Book>
+<Book Series="Titans: Beast World" Number="5" Volume="2024" Year="2024">
+<Database Name="cv" Series="154912" Issue="1042195" />
+</Book>
+<Book Series="Titans: Beast World Tour: Star City" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="156358" Issue="1042196" />
+</Book>
+<Book Series="Titans: Beast World" Number="6" Volume="2024" Year="2024">
+<Database Name="cv" Series="154912" Issue="1042820" />
+</Book>
+<Book Series="Titans" Number="8" Volume="2023" Year="2024">
+<Database Name="cv" Series="150428" Issue="1045414" />
+</Book>
+<Book Series="Green Arrow" Number="9" Volume="2023" Year="2024">
+<Database Name="cv" Series="149738" Issue="1046256" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
