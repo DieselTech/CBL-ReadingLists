93d89117fce9dd988c9204a2f951a4f27b5a62d2
Oli Wright <1392563+falo2k@users.noreply.github.com>
2024-06-03 20:51:28 +0100

    Part 17 - Flashpoint Interlude

---
+++ DC/Characters/Batman/Batman - Modern Age/[DC] Batman Modern Age - Part 17 INTERLUDE Flashpoint [2009-2011].cbl
@@ -0,0 +1,209 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[DC] Batman Modern Age - Part 17 INTERLUDE Flashpoint [2009-2011]</Name>
+<NumIssues>67</NumIssues>
+<Books>
+<Book Series="The Flash: Rebirth" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26076" Issue="154447" />
+</Book>
+<Book Series="The Flash: Rebirth" Number="2" Volume="2009" Year="2009">
+<Database Name="cv" Series="26076" Issue="156621" />
+</Book>
+<Book Series="The Flash: Rebirth" Number="3" Volume="2009" Year="2009">
+<Database Name="cv" Series="26076" Issue="159734" />
+</Book>
+<Book Series="The Flash: Rebirth" Number="4" Volume="2009" Year="2009">
+<Database Name="cv" Series="26076" Issue="168289" />
+</Book>
+<Book Series="The Flash: Rebirth" Number="5" Volume="2009" Year="2010">
+<Database Name="cv" Series="26076" Issue="183719" />
+</Book>
+<Book Series="The Flash: Rebirth" Number="6" Volume="2009" Year="2010">
+<Database Name="cv" Series="26076" Issue="198264" />
+</Book>
+<Book Series="The Flash" Number="8" Volume="2010" Year="2011">
+<Database Name="cv" Series="32562" Issue="252622" />
+</Book>
+<Book Series="The Flash" Number="9" Volume="2010" Year="2011">
+<Database Name="cv" Series="32562" Issue="261124" />
+</Book>
+<Book Series="The Flash" Number="10" Volume="2010" Year="2011">
+<Database Name="cv" Series="32562" Issue="268209" />
+</Book>
+<Book Series="The Flash" Number="11" Volume="2010" Year="2011">
+<Database Name="cv" Series="32562" Issue="268970" />
+</Book>
+<Book Series="The Flash" Number="12" Volume="2010" Year="2011">
+<Database Name="cv" Series="32562" Issue="269821" />
+</Book>
+<Book Series="Time Masters: Vanishing Point" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="34584" Issue="225826" />
+</Book>
+<Book Series="Time Masters: Vanishing Point" Number="2" Volume="2010" Year="2010">
+<Database Name="cv" Series="34584" Issue="231662" />
+</Book>
+<Book Series="Time Masters: Vanishing Point" Number="3" Volume="2010" Year="2010">
+<Database Name="cv" Series="34584" Issue="236260" />
+</Book>
+<Book Series="Time Masters: Vanishing Point" Number="4" Volume="2010" Year="2010">
+<Database Name="cv" Series="34584" Issue="239847" />
+</Book>
+<Book Series="Time Masters: Vanishing Point" Number="5" Volume="2010" Year="2011">
+<Database Name="cv" Series="34584" Issue="249301" />
+</Book>
+<Book Series="Time Masters: Vanishing Point" Number="6" Volume="2010" Year="2011">
+<Database Name="cv" Series="34584" Issue="260029" />
+</Book>
+<Book Series="Flashpoint" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="39997" Issue="269827" />
+</Book>
+<Book Series="Flashpoint" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="39997" Issue="272411" />
+</Book>
+<Book Series="Flashpoint" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="39997" Issue="277377" />
+</Book>
+<Book Series="Flashpoint" Number="4" Volume="2011" Year="2011">
+<Database Name="cv" Series="39997" Issue="282568" />
+</Book>
+<Book Series="Flashpoint" Number="5" Volume="2011" Year="2011">
+<Database Name="cv" Series="39997" Issue="290102" />
+</Book>
+<Book Series="Flashpoint: Batman Knight of Vengeance" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40364" Issue="272346" />
+</Book>
+<Book Series="Flashpoint: Batman Knight of Vengeance" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40364" Issue="277419" />
+</Book>
+<Book Series="Flashpoint: Batman Knight of Vengeance" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40364" Issue="282900" />
+</Book>
+<Book Series="Flashpoint: Deadman and the Flying Graysons" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40683" Issue="274394" />
+</Book>
+<Book Series="Flashpoint: Deadman and the Flying Graysons" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40683" Issue="279957" />
+</Book>
+<Book Series="Flashpoint: Deadman and the Flying Graysons" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40683" Issue="285171" />
+</Book>
+<Book Series="Flashpoint: Deathstroke and the Curse of the Ravager" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40523" Issue="273081" />
+</Book>
+<Book Series="Flashpoint: Deathstroke and the Curse of the Ravager" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40523" Issue="278558" />
+</Book>
+<Book Series="Flashpoint: Deathstroke and the Curse of the Ravager" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40523" Issue="282906" />
+</Book>
+<Book Series="Flashpoint: Secret Seven" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40365" Issue="272348" />
+</Book>
+<Book Series="Flashpoint: Secret Seven" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40365" Issue="277433" />
+</Book>
+<Book Series="Flashpoint: Secret Seven" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40365" Issue="282910" />
+</Book>
+<Book Series="Flashpoint: Wonder Woman and the Furies" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40688" Issue="274393" />
+</Book>
+<Book Series="Flashpoint: Wonder Woman and the Furies" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40688" Issue="279958" />
+</Book>
+<Book Series="Flashpoint: Wonder Woman and the Furies" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40688" Issue="286681" />
+</Book>
+<Book Series="Flashpoint: Emperor Aquaman" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40524" Issue="273083" />
+</Book>
+<Book Series="Flashpoint: Emperor Aquaman" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40524" Issue="278556" />
+</Book>
+<Book Series="Flashpoint: Emperor Aquaman" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40524" Issue="285172" />
+</Book>
+<Book Series="Flashpoint: Lois Lane and the Resistance" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40762" Issue="275587" />
+</Book>
+<Book Series="Flashpoint: Lois Lane and the Resistance" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40762" Issue="281525" />
+</Book>
+<Book Series="Flashpoint: Lois Lane and the Resistance" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40762" Issue="288131" />
+</Book>
+<Book Series="Flashpoint: The Outsider" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40766" Issue="275585" />
+</Book>
+<Book Series="Flashpoint: The Outsider" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40766" Issue="279960" />
+</Book>
+<Book Series="Flashpoint: The Outsider" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40766" Issue="286679" />
+</Book>
+<Book Series="Flashpoint: Abin Sur - The Green Lantern" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40367" Issue="272344" />
+</Book>
+<Book Series="Flashpoint: Abin Sur - The Green Lantern" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40367" Issue="277427" />
+</Book>
+<Book Series="Flashpoint: Abin Sur - The Green Lantern" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40367" Issue="286680" />
+</Book>
+<Book Series="Flashpoint: Frankenstein and the Creatures of the Unknown" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40459" Issue="273080" />
+</Book>
+<Book Series="Flashpoint: Frankenstein and the Creatures of the Unknown" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40459" Issue="278557" />
+</Book>
+<Book Series="Flashpoint: Frankenstein and the Creatures of the Unknown" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40459" Issue="285174" />
+</Book>
+<Book Series="Flashpoint: Green Arrow Industries" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40924" Issue="276567" />
+</Book>
+<Book Series="Flashpoint: Hal Jordan" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40928" Issue="276570" />
+</Book>
+<Book Series="Flashpoint: Hal Jordan" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40928" Issue="281526" />
+</Book>
+<Book Series="Flashpoint: Hal Jordan" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40928" Issue="288130" />
+</Book>
+<Book Series="Flashpoint: Reverse Flash" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40779" Issue="275584" />
+</Book>
+<Book Series="Flashpoint: Citizen Cold" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40522" Issue="273082" />
+</Book>
+<Book Series="Flashpoint: Citizen Cold" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40522" Issue="278555" />
+</Book>
+<Book Series="Flashpoint: Citizen Cold" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40522" Issue="285179" />
+</Book>
+<Book Series="Flashpoint: Legion of Doom" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40689" Issue="274391" />
+</Book>
+<Book Series="Flashpoint: Legion of Doom" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40689" Issue="279962" />
+</Book>
+<Book Series="Flashpoint: Legion of Doom" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40689" Issue="286682" />
+</Book>
+<Book Series="Flashpoint: Grodd of War" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40691" Issue="274392" />
+</Book>
+<Book Series="Flashpoint: Kid Flash Lost" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="40769" Issue="275586" />
+</Book>
+<Book Series="Flashpoint: Kid Flash Lost" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="40769" Issue="281527" />
+</Book>
+<Book Series="Flashpoint: Kid Flash Lost" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="40769" Issue="288132" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
