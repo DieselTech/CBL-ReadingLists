0a8ab1b09458f3b628e546899d6f3406def37cff
Oli Wright <1392563+falo2k@users.noreply.github.com>
2024-06-03 16:26:30 +0100

    Parts 13,14,15

--- DC/Characters/Batman/Batman - Modern Age/[DC] Batman Modern Age - Part 15 Batman Reborn [1989-2013].cbl
+++ DC/Characters/Batman/Batman - Modern Age/[DC] Batman Modern Age - Part 15 Batman Reborn [1989-2013].cbl
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 <Name>[DC] Batman Modern Age - Part 15 Batman Reborn [1989-2013]</Name>
-<NumIssues>147</NumIssues>
+<NumIssues>154</NumIssues>
 <Books>
 <Book Series="Batman: Battle for the Cowl" Number="1" Volume="2009" Year="2009">
 <Database Name="cv" Series="25959" Issue="153443" />
@@ -12,6 +12,12 @@
 <Book Series="Batman: Battle for the Cowl" Number="3" Volume="2009" Year="2009">
 <Database Name="cv" Series="25959" Issue="157716" />
 </Book>
+<Book Series="Gotham Gazette" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="25903" Issue="153071" />
+</Book>
+<Book Series="Gotham Gazette: Batman Alive?" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26539" Issue="158686" />
+</Book>
 <Book Series="Batman" Number="684" Volume="1940" Year="2009">
 <Database Name="cv" Series="796" Issue="149279" />
 </Book>
@@ -20,6 +26,21 @@
 </Book>
 <Book Series="Detective Comics" Number="851" Volume="1937" Year="2009">
 <Database Name="cv" Series="18058" Issue="145530" />
+</Book>
+<Book Series="Batman: Battle for the Cowl: Commissioner Gordon" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26052" Issue="153993" />
+</Book>
+<Book Series="Batman: Battle for the Cowl: Man-Bat" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26098" Issue="154627" />
+</Book>
+<Book Series="Batman: Battle for the Cowl: Arkham Asylum" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26256" Issue="155760" />
+</Book>
+<Book Series="Batman: Battle for the Cowl: The Underground" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26294" Issue="156117" />
+</Book>
+<Book Series="Batman: Battle for the Cowl: The Network" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26323" Issue="156624" />
 </Book>
 <Book Series="Birds of Prey" Number="126" Volume="1999" Year="2009">
 <Database Name="cv" Series="6498" Issue="150509" />
@@ -69,26 +90,26 @@
 <Book Series="Batman" Number="691" Volume="1940" Year="2009">
 <Database Name="cv" Series="796" Issue="176039" />
 </Book>
-<Book Series="Batgirl" Number="1" Volume="2011" Year="2011">
-<Database Name="cv" Series="42604" Issue="291171" />
+<Book Series="Batgirl" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="27457" Issue="167360" />
 </Book>
-<Book Series="Batgirl" Number="2" Volume="2011" Year="2011">
-<Database Name="cv" Series="42604" Issue="294820" />
+<Book Series="Batgirl" Number="2" Volume="2009" Year="2009">
+<Database Name="cv" Series="27457" Issue="171414" />
 </Book>
-<Book Series="Batgirl" Number="3" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="301534" />
+<Book Series="Batgirl" Number="3" Volume="2009" Year="2009">
+<Database Name="cv" Series="27457" Issue="176018" />
 </Book>
-<Book Series="Batgirl" Number="4" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="306479" />
+<Book Series="Batgirl" Number="4" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="182564" />
 </Book>
-<Book Series="Batgirl" Number="5" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="310552" />
+<Book Series="Batgirl" Number="5" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="186732" />
 </Book>
-<Book Series="Batgirl" Number="6" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="314975" />
+<Book Series="Batgirl" Number="6" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="192403" />
 </Book>
-<Book Series="Batgirl" Number="7" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="321187" />
+<Book Series="Batgirl" Number="7" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="196464" />
 </Book>
 <Book Series="Detective Comics" Number="852" Volume="1937" Year="2009">
 <Database Name="cv" Series="18058" Issue="149814" />
@@ -192,8 +213,8 @@
 <Book Series="Detective Comics" Number="853" Volume="1937" Year="2009">
 <Database Name="cv" Series="18058" Issue="155784" />
 </Book>
-<Book Series="Batman: Black and White" Number="2" Volume="1997" Year="2003">
-<Database Name="cv" Series="34627" Issue="230320" />
+<Book Series="Batman: Black and White" Number="2" Volume="1996" Year="1996">
+<Database Name="cv" Series="5729" Issue="42570" />
 </Book>
 <Book Series="Secret Origins" Number="36" Volume="1986" Year="1989">
 <Database Name="cv" Series="3616" Issue="30746" />
@@ -240,6 +261,9 @@
 <Book Series="Detective Comics" Number="865" Volume="1937" Year="2010">
 <Database Name="cv" Series="18058" Issue="216196" />
 </Book>
+<Book Series="DCU Halloween Special '09" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="28830" Issue="177713" />
+</Book>
 <Book Series="Batman" Number="692" Volume="1940" Year="2009">
 <Database Name="cv" Series="796" Issue="178923" />
 </Book>
@@ -285,6 +309,9 @@
 <Book Series="Batman: Streets of Gotham" Number="11" Volume="2009" Year="2010">
 <Database Name="cv" Series="26754" Issue="208690" />
 </Book>
+<Book Series="Batman 80-Page Giant 2010" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="37100" Issue="247472" />
+</Book>
 <Book Series="Gotham City Sirens" Number="8" Volume="2014" Year="2010">
 <Database Name="cv" Series="26891" Issue="194261" />
 </Book>
@@ -302,6 +329,9 @@
 </Book>
 <Book Series="Gotham City Sirens" Number="13" Volume="2014" Year="2010">
 <Database Name="cv" Series="26891" Issue="221895" />
+</Book>
+<Book Series="DC Holiday Special '09" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="30387" Issue="187207" />
 </Book>
 <Book Series="Detective Comics" Number="867" Volume="1937" Year="2010">
 <Database Name="cv" Series="18058" Issue="226884" />
@@ -330,23 +360,23 @@
 <Book Series="Red Robin" Number="17" Volume="2009" Year="2011">
 <Database Name="cv" Series="26651" Issue="241934" />
 </Book>
-<Book Series="Batgirl" Number="9" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="335019" />
+<Book Series="Batgirl" Number="9" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="206639" />
 </Book>
-<Book Series="Batgirl" Number="10" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="340428" />
+<Book Series="Batgirl" Number="10" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="213450" />
 </Book>
-<Book Series="Batgirl" Number="11" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="345344" />
+<Book Series="Batgirl" Number="11" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="218282" />
 </Book>
-<Book Series="Batgirl" Number="12" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="349623" />
+<Book Series="Batgirl" Number="12" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="224579" />
 </Book>
-<Book Series="Batgirl" Number="13" Volume="2011" Year="2012">
-<Database Name="cv" Series="42604" Issue="360791" />
+<Book Series="Batgirl" Number="13" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="228846" />
 </Book>
-<Book Series="Batgirl" Number="14" Volume="2011" Year="2013">
-<Database Name="cv" Series="42604" Issue="367688" />
+<Book Series="Batgirl" Number="14" Volume="2009" Year="2010">
+<Database Name="cv" Series="27457" Issue="233657" />
 </Book>
 <Book Series="Batman and Robin" Number="7" Volume="2009" Year="2010">
 <Database Name="cv" Series="26584" Issue="194484" />
@@ -395,6 +425,30 @@
 </Book>
 <Book Series="Batman: The Return of Bruce Wayne" Number="6" Volume="2010" Year="2011">
 <Database Name="cv" Series="33073" Issue="241775" />
+</Book>
+<Book Series="Bruce Wayne: The Road Home: Batman and Robin" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="36061" Issue="238143" />
+</Book>
+<Book Series="Bruce Wayne: The Road Home: Red Robin" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="36063" Issue="238147" />
+</Book>
+<Book Series="Bruce Wayne: The Road Home: Outsiders" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="36064" Issue="238153" />
+</Book>
+<Book Series="Bruce Wayne: The Road Home: Batgirl" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="36065" Issue="238155" />
+</Book>
+<Book Series="Bruce Wayne: The Road Home: Catwoman" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="36233" Issue="239024" />
+</Book>
+<Book Series="Bruce Wayne: The Road Home: Commissioner Gordon" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="36226" Issue="238974" />
+</Book>
+<Book Series="Bruce Wayne: The Road Home: Oracle" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="36338" Issue="239802" />
+</Book>
+<Book Series="Bruce Wayne: The Road Home: Ra's al Ghul" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="36351" Issue="239843" />
 </Book>
 <Book Series="Batman and Robin" Number="13" Volume="2009" Year="2010">
 <Database Name="cv" Series="26584" Issue="223270" />
