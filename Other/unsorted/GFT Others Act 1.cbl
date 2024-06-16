683cd3305e6770cacde9c418acb9b9d917b60d86
Cducharme84 <131784515+Cducharme84@users.noreply.github.com>
2024-06-15 22:10:09 -0400

    Grimm Fairy Tales Other Stories Act 1
    These are non-main stories but still firmly part of the GFT series, stuff like april fools or series that spun off from the main one but didn't sprawl as much as Wonderland did. These will be concurrent with the Main and Wonderland lists in chronology but this set will be fairly optional.

---
+++ Other/unsorted/GFT Others Act 1.cbl
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>GFT Others Act 1</Name>
+<NumIssues>24</NumIssues>
+<Books>
+<Book Series="Grimm Fairy Tales April Fools'" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26240" Issue="155632" />
+</Book>
+<Book Series="Grimm Fairy Tales April Fools'" Number="2" Volume="2009" Year="2010">
+<Database Name="cv" Series="26240" Issue="205175" />
+</Book>
+<Book Series="Salem's Daughter" Number="0" Volume="2009" Year="2009">
+<Database Name="cv" Series="7451" Issue="155174" />
+</Book>
+<Book Series="Salem's Daughter" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="7451" Issue="160427" />
+</Book>
+<Book Series="Salem's Daughter" Number="2" Volume="2009" Year="2009">
+<Database Name="cv" Series="7451" Issue="177663" />
+</Book>
+<Book Series="Salem's Daughter" Number="3" Volume="2009" Year="2010">
+<Database Name="cv" Series="7451" Issue="197050" />
+</Book>
+<Book Series="Salem's Daughter" Number="4" Volume="2009" Year="2010">
+<Database Name="cv" Series="7451" Issue="209457" />
+</Book>
+<Book Series="Salem's Daughter" Number="5" Volume="2009" Year="2010">
+<Database Name="cv" Series="7451" Issue="232024" />
+</Book>
+<Book Series="Neverland" Number="0" Volume="2010" Year="2010">
+<Database Name="cv" Series="31251" Issue="194435" />
+</Book>
+<Book Series="Neverland" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="31251" Issue="201384" />
+</Book>
+<Book Series="Neverland" Number="2" Volume="2010" Year="2010">
+<Database Name="cv" Series="31251" Issue="210436" />
+</Book>
+<Book Series="Neverland" Number="3" Volume="2010" Year="2010">
+<Database Name="cv" Series="31251" Issue="219611" />
+</Book>
+<Book Series="Neverland" Number="4" Volume="2010" Year="2010">
+<Database Name="cv" Series="31251" Issue="225681" />
+</Book>
+<Book Series="Neverland" Number="5" Volume="2010" Year="2010">
+<Database Name="cv" Series="31251" Issue="236488" />
+</Book>
+<Book Series="Neverland" Number="6" Volume="2010" Year="2010">
+<Database Name="cv" Series="31251" Issue="249404" />
+</Book>
+<Book Series="Neverland" Number="7" Volume="2010" Year="2011">
+<Database Name="cv" Series="31251" Issue="263474" />
+</Book>
+<Book Series="Grimm Fairy Tales: Tales From Neverland" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="41931" Issue="285353" />
+</Book>
+<Book Series="Grimm Fairy Tales: Tales From Neverland" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="41931" Issue="285354" />
+</Book>
+<Book Series="Grimm Fairy Tales: Tales From Neverland" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="41931" Issue="311057" />
+</Book>
+<Book Series="Grimm Fairy Tales presents The Library" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="42984" Issue="293447" />
+</Book>
+<Book Series="Grimm Fairy Tales presents The Library" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="42984" Issue="294610" />
+</Book>
+<Book Series="Grimm Fairy Tales presents The Library" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="42984" Issue="306794" />
+</Book>
+<Book Series="Grimm Fairy Tales presents The Library" Number="4" Volume="2011" Year="2012">
+<Database Name="cv" Series="42984" Issue="315871" />
+</Book>
+<Book Series="Grimm Fairy Tales presents The Library" Number="5" Volume="2011" Year="2012">
+<Database Name="cv" Series="42984" Issue="327730" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
