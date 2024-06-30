5ac92c0a9d1a615e5ca08760e5e9015684a1deff
grosejon <36434630+grosejon@users.noreply.github.com>
2024-06-30 12:09:49 -0500

    Add files via upload

---
+++ Marvel/Teams/X-Men/CBH X-Men Reading Order/II. Days of Future Past _ Brood Saga.cbl
@@ -0,0 +1,109 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>II. Days of Future Past / Brood Saga</Name>
+  <Books>
+    <Book Series="X-Men Annual" Number="4" Volume="1970" Year="1980" Format="Annual">
+      <Id>2b892637-7e2d-4ff8-b5d5-99619e435c84</Id>
+    </Book>
+    <Book Series="The X-Men" Number="139" Volume="1963" Year="1980">
+      <Id>9cbeae76-23f4-4de6-af46-2cd4f1a516b5</Id>
+    </Book>
+    <Book Series="The X-Men" Number="140" Volume="1963" Year="1980">
+      <Id>ccbfd100-aae3-40d2-91cd-0215eb70610d</Id>
+    </Book>
+    <Book Series="The X-Men" Number="141" Volume="1963" Year="1981">
+      <Id>925fdf33-bd94-4436-b170-634ce257756b</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="142" Volume="1981" Year="1981">
+      <Id>ea4b20b7-8d59-4cdc-9a4e-50d9e31c2ab9</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="143" Volume="1981" Year="1981">
+      <Id>d0d4facd-3af8-47c2-85b6-b749c1ab389e</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="144" Volume="1981" Year="1981">
+      <Id>a34b69a5-f787-43d5-ba2c-aea2cf5c94e9</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="145" Volume="1981" Year="1981">
+      <Id>6b18270e-b553-40cd-a917-106ae16a1495</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="146" Volume="1981" Year="1981">
+      <Id>364deb2e-6bac-4f11-8e7d-2ecbe5fc36f9</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="147" Volume="1981" Year="1981">
+      <Id>5a1b2b6f-3469-476e-a0d9-83dc03dfd448</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="148" Volume="1981" Year="1981">
+      <Id>d123435d-fede-4fbd-b1aa-a2dbc72c7db8</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="149" Volume="1981" Year="1981">
+      <Id>9de75b6c-c6de-437b-a165-95ca4aa93d0f</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="150" Volume="1981" Year="1981">
+      <Id>8be161bc-9c9b-445a-b0fe-d47994d98cf5</Id>
+    </Book>
+    <Book Series="The Avengers Annual" Number="8" Volume="1967" Year="1978" Format="Annual">
+      <Id>d0e138a6-cde0-4255-b983-8d46d1d55a46</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="151" Volume="1981" Year="1981">
+      <Id>c91b4e9a-d939-4d0e-b0c1-6cbd7eebe57c</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="152" Volume="1981" Year="1981">
+      <Id>3c5f1c33-f1df-45b7-9595-05006d500a52</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="153" Volume="1981" Year="1982">
+      <Id>2779939f-4ed4-4e7f-8e24-4c2499828bf3</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="154" Volume="1981" Year="1982">
+      <Id>2591d0e4-06f1-4396-8e29-134af68478b8</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="155" Volume="1981" Year="1982">
+      <Id>7f81e251-4177-45dd-99b2-63cae3e6220b</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="156" Volume="1981" Year="1982">
+      <Id>fc0b8e00-9819-4503-a581-0ea09d3aca8c</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="157" Volume="1981" Year="1982">
+      <Id>2f79d88e-8b20-46d6-8cf6-af06f024bb6d</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="158" Volume="1981" Year="1982">
+      <Id>5772d063-46fe-45a4-a64a-216cbac23215</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="159" Volume="1981" Year="1982">
+      <Id>f190cce2-2369-4fb7-9605-36a54397bbdc</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="160" Volume="1981" Year="1982">
+      <Id>c8f83cdd-1b9d-4631-ab26-917ed7685df2</Id>
+    </Book>
+    <Book Series="Magik" Number="1" Volume="1983" Year="1983">
+      <Id>3de0c52e-0b15-480a-b9d3-0f26c9170d30</Id>
+    </Book>
+    <Book Series="Magik" Number="2" Volume="1983" Year="1984">
+      <Id>5e3e78c6-e84e-4a05-9497-2169ffc6a06d</Id>
+    </Book>
+    <Book Series="Magik" Number="3" Volume="1983" Year="1984">
+      <Id>2abc263f-fe4e-4bcb-9aa2-70c991d5c25b</Id>
+    </Book>
+    <Book Series="Magik" Number="4" Volume="1983" Year="1984">
+      <Id>eb935e34-e70b-4970-baa7-c1a2753e2116</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="161" Volume="1981" Year="1982">
+      <Id>d06f8780-ffe2-4604-8c77-42e660350620</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="162" Volume="1981" Year="1982">
+      <Id>ac5dd487-22de-4662-8979-aeffe94b4f26</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="163" Volume="1981" Year="1982">
+      <Id>2cd8bb51-6233-4581-8ac4-b4293cc04709</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="164" Volume="1981" Year="1982">
+      <Id>b8a654dc-269e-4dca-aa13-cb4eeb9711fa</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="166" Volume="1981" Year="1983">
+      <Id>9ab84441-b4f3-4c03-9309-984d74dae0c8</Id>
+    </Book>
+    <Book Series="X-Men Annual" Number="6" Volume="1970" Year="1982" Format="Annual">
+      <Id>ec4e8072-b52b-4daa-aabc-f8f0e41fd3e1</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
---
+++ Marvel/Teams/X-Men/CBH X-Men Reading Order/VIII. Xtinction Agenda.cbl
@@ -0,0 +1,76 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>VIII. Xtinction Agenda</Name>
+  <Books>
+    <Book Series="The Uncanny X-Men" Number="270" Volume="1981" Year="1990">
+      <Id>15a86593-8a04-4108-a7bb-9e30807057e7</Id>
+    </Book>
+    <Book Series="X-Factor" Number="60" Volume="1986" Year="1990">
+      <Id>88d18758-1cef-4890-8bce-5bf36bd012aa</Id>
+    </Book>
+    <Book Series="The New Mutants" Number="95" Volume="1983" Year="1990">
+      <Id>0060f349-8524-4396-a0c4-6835126a5f35</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="271" Volume="1981" Year="1990">
+      <Id>cac09854-5899-4dc1-83a4-c7c5a02691a9</Id>
+    </Book>
+    <Book Series="X-Factor" Number="61" Volume="1986" Year="1990">
+      <Id>5958591f-ac44-4d45-ba88-dada2e4bc546</Id>
+    </Book>
+    <Book Series="The New Mutants" Number="96" Volume="1983" Year="1990">
+      <Id>5c1afd8e-1b04-4edf-9593-bf4981380e8d</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="272" Volume="1981" Year="1991">
+      <Id>935748fb-cb1f-4302-904f-b20bafec623f</Id>
+    </Book>
+    <Book Series="X-Factor" Number="62" Volume="1986" Year="1991">
+      <Id>3c9ae0b9-0932-4809-b8c3-c878c8471e27</Id>
+    </Book>
+    <Book Series="The New Mutants" Number="97" Volume="1983" Year="1991">
+      <Id>90b8d64d-d994-4f0a-b8d7-917f75d484c9</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="273" Volume="1981" Year="1991">
+      <Id>e5403ff8-4283-436c-8f43-49e7454946fb</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="274" Volume="1981" Year="1991">
+      <Id>39db4438-2092-447e-b7e9-e99045f9fb6c</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="275" Volume="1981" Year="1991">
+      <Id>2846774b-fabe-4f39-bdf6-7943f2d27f5e</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="276" Volume="1981" Year="1991">
+      <Id>edc716d2-8a47-4c6b-aa6a-ee90330a3461</Id>
+    </Book>
+    <Book Series="The Uncanny X-Men" Number="277" Volume="1981" Year="1991">
+      <Id>ae205394-ddf1-4033-9f4e-54e74e95173e</Id>
+    </Book>
+    <Book Series="X-Factor" Number="63" Volume="1986" Year="1991">
+      <Id>82ef6d98-85c8-4ef0-ad30-b204f7b01df8</Id>
+    </Book>
+    <Book Series="X-Factor" Number="64" Volume="1986" Year="1991">
+      <Id>a1b0fe40-5ce3-4619-a63a-afd9ada63efc</Id>
+    </Book>
+    <Book Series="X-Factor" Number="65" Volume="1986" Year="1991">
+      <Id>9f43b8ab-a4b8-4390-8aec-9e8aef36bb1b</Id>
+    </Book>
+    <Book Series="X-Factor" Number="66" Volume="1986" Year="1991">
+      <Id>201157b7-26cf-4bc1-8247-1287a7c4e0b0</Id>
+    </Book>
+    <Book Series="X-Factor" Number="67" Volume="1986" Year="1991">
+      <Id>faaf9f86-97b6-4775-8945-cf470090f932</Id>
+    </Book>
+    <Book Series="X-Factor" Number="68" Volume="1986" Year="1991">
+      <Id>a72f395e-fe93-46dd-a9c6-e1136a2a73f0</Id>
+    </Book>
+    <Book Series="The New Mutants" Number="98" Volume="1983" Year="1991">
+      <Id>c4a1352c-c54e-4468-ae5d-c9ad60209221</Id>
+    </Book>
+    <Book Series="The New Mutants" Number="99" Volume="1983" Year="1991">
+      <Id>28b0f502-60c8-472d-a285-608808ef0afe</Id>
+    </Book>
+    <Book Series="The New Mutants" Number="100" Volume="1983" Year="1991">
+      <Id>abb6a2db-80de-4220-a4d4-513ee68797c1</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
