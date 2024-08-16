52edea5136c42aa9236a9f2d2bd60a9175c54354
Ulisses Capato <125318439+ucapato@users.noreply.github.com>
2024-08-16 15:28:58 +0200

    Latest update on Series name, Issue and Series IDs. All data provided from ComicVine Database

--- Marvel/Events/LoCG/[2016-2017] Civil War II (Marvel Comics)(LoCG).cbl
+++ Marvel/Events/LoCG/[2016-2017] Civil War II (Marvel Comics)(LoCG).cbl
@@ -43,7 +43,7 @@
 <Database Name="cv" Series="86114" Issue="531894" />
 </Book>
 <Book Series="Civil War II: Ulysses Infinite Comic" Number="1" Volume="2016" Year="2016">
-<Database Name="cv" Series="91316" Issue="535948" />
+<Database Name="cv" Series="91316" Issue="535498" />
 </Book>
 <Book Series="Civil War II: Ulysses Infinite Comic" Number="2" Volume="2016" Year="2016">
 <Database Name="cv" Series="91316" Issue="538664" />
