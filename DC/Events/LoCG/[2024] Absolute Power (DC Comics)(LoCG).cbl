cb041030bb39bd87fb6f6394d97f059429c2aec0
TheScummiestofThemAll <63425978+TheScummiestofThemAll@users.noreply.github.com>
2024-10-02 18:45:51 -0400

    Add files via upload
    League of Comic Geeks Events

---
+++ DC/Events/LoCG/[2024] Absolute Power (DC Comics)(LoCG).cbl
@@ -0,0 +1,124 @@
+<?xml version="1.0"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>Absolute Power</Name>
+  <Books>
+    <Book Series="Suicide Squad: Dream Team" Number="1" Volume="2024" Year="2024">
+      <Id>de15bd90-3d43-4cb7-af21-510e8c4f8886</Id>
+    </Book>
+    <Book Series="Suicide Squad: Dream Team" Number="2" Volume="2024" Year="2024">
+      <Id>8fa9c5e0-e206-4198-a186-17feecd18187</Id>
+    </Book>
+    <Book Series="Suicide Squad: Dream Team" Number="3" Volume="2024" Year="2024">
+      <Id>b8759d54-58da-47f9-b085-13f453e922f4</Id>
+    </Book>
+    <Book Series="Absolute Power 2024 FCBD Special Edition" Number="1" Volume="2024" Year="2024">
+      <Id>707560af-65f5-4011-a15b-1e2d2a7371aa</Id>
+    </Book>
+    <Book Series="Suicide Squad: Dream Team" Number="4" Volume="2024" Year="2024">
+      <Id>fe030b27-6ffd-48af-a633-0d8466ed550d</Id>
+    </Book>
+    <Book Series="Superman" Number="15" Volume="2023" Year="2024">
+      <Id>b0267d35-463c-4ffc-833a-bd3934fa2bdf</Id>
+    </Book>
+    <Book Series="Green Arrow" Number="13" Volume="2023" Year="2024">
+      <Id>d3c5a74c-1092-42fa-b9b0-9b83a60a36da</Id>
+    </Book>
+    <Book Series="The Flash" Number="10" Volume="2023" Year="2024">
+      <Id>b74e340f-8e0c-4abf-8075-68fe5bbe4224</Id>
+    </Book>
+    <Book Series="Absolute Power: Ground Zero" Number="1" Volume="2024" Year="2024">
+      <Id>dd483a6d-2ffc-40d3-b1f3-e72b24315c77</Id>
+    </Book>
+    <Book Series="Absolute Power" Number="1" Volume="2024" Year="2024">
+      <Id>3faeab86-27eb-4a92-b00f-fc7ee01e412a</Id>
+    </Book>
+    <Book Series="Batman" Number="150" Volume="2016" Year="2024">
+      <Id>bbea41ab-89ba-493d-9c07-ecc02ae500bd</Id>
+    </Book>
+    <Book Series="Wonder Woman" Number="11" Volume="2023" Year="2024">
+      <Id>74484cb4-38cb-4565-849d-beb69793ebdb</Id>
+    </Book>
+    <Book Series="Absolute Power: Task Force VII" Number="1" Volume="2024" Year="2024">
+      <Id>c93cfeac-c992-4bde-876a-a3adad069bd4</Id>
+    </Book>
+    <Book Series="Green Lantern" Number="13" Volume="2023" Year="2024">
+      <Id>c87e9dbe-ec00-4246-a53b-ff04ddb46389</Id>
+    </Book>
+    <Book Series="Superman" Number="16" Volume="2023" Year="2024">
+      <Id>1ea7e295-461c-4620-b1d9-225141e03c5f</Id>
+    </Book>
+    <Book Series="Absolute Power: Task Force VII" Number="2" Volume="2024" Year="2024">
+      <Id>4077baba-855e-4958-a0a1-f9c346226dcd</Id>
+    </Book>
+    <Book Series="Absolute Power: Origins" Number="1" Volume="2024" Year="2024">
+      <Id>538b61f1-1153-4202-b696-cc556f8ee494</Id>
+    </Book>
+    <Book Series="Green Arrow" Number="14" Volume="2023" Year="2024">
+      <Id>991faeb8-4b8e-4f1c-a3a6-8000c483bfc9</Id>
+    </Book>
+    <Book Series="Absolute Power: Task Force VII" Number="3" Volume="2024" Year="2024">
+      <Id>82efb370-8144-4528-844a-3056697e0bdf</Id>
+    </Book>
+    <Book Series="Absolute Power" Number="2" Volume="2024" Year="2024">
+      <Id>6831c6fb-376f-42a4-9fba-c34ce94e440b</Id>
+    </Book>
+    <Book Series="Batman" Number="151" Volume="2016" Year="2024">
+      <Id>c7ed41f4-10f5-424a-b15b-d99e1b54cf6e</Id>
+    </Book>
+    <Book Series="Absolute Power: Task Force VII" Number="4" Volume="2024" Year="2024">
+      <Id>acdb7821-6cda-486c-9af4-a490e6088439</Id>
+    </Book>
+    <Book Series="Green Lantern" Number="14" Volume="2023" Year="2024">
+      <Id>0f1ce115-c256-4e9a-8309-07bb1d9ad044</Id>
+    </Book>
+    <Book Series="Wonder Woman" Number="12" Volume="2023" Year="2024">
+      <Id>ad6cedc0-f9b0-4ac3-bede-2999cadd8c5b</Id>
+    </Book>
+    <Book Series="Superman" Number="17" Volume="2023" Year="2024">
+      <Id>9599683d-41d8-4a89-83fd-ad680c6225b6</Id>
+    </Book>
+    <Book Series="Absolute Power: Origins" Number="2" Volume="2024" Year="2024">
+      <Id>0d6811ef-852a-4463-9900-c2562a00438d</Id>
+    </Book>
+    <Book Series="Absolute Power: Task Force VII" Number="5" Volume="2024" Year="2024">
+      <Id>fd54099f-da21-4260-b6be-c9a0c0d7fabf</Id>
+    </Book>
+    <Book Series="Green Arrow" Number="15" Volume="2023" Year="2024">
+      <Id>448a5873-70c0-4705-a8c0-cd0394a21f8b</Id>
+    </Book>
+    <Book Series="Batman" Number="152" Volume="2016" Year="2024">
+      <Id>592028d5-55af-40e1-a327-5937a251b60a</Id>
+    </Book>
+    <Book Series="Absolute Power" Number="3" Volume="2024" Year="2024">
+      <Id>c49d4f4e-118b-4da7-83b0-c1de63c38323</Id>
+    </Book>
+    <Book Series="Absolute Power: Super Son" Number="1" Volume="2024" Year="2024">
+      <Id>1d6fb648-4e88-4e44-8077-dd6640005ed3</Id>
+    </Book>
+    <Book Series="Absolute Power: Task Force VII" Number="6" Volume="2024" Year="2024">
+      <Id>e184388d-faf1-4cae-8240-6634e7c0b1ec</Id>
+    </Book>
+    <Book Series="Green Lantern" Number="15" Volume="2023" Year="2024">
+      <Id>2d4927bc-48e2-4f49-ae22-93cdaf29a2b3</Id>
+    </Book>
+    <Book Series="Superman" Number="18" Volume="2023" Year="2024">
+      <Id>b281d4f1-0127-4d0a-be51-c6f997ec445b</Id>
+    </Book>
+    <Book Series="Wonder Woman" Number="13" Volume="2023" Year="2024">
+      <Id>7acbbfa8-dbef-4900-9189-3c8b1b8bd9b5</Id>
+    </Book>
+    <Book Series="Absolute Power: Origins" Number="3" Volume="2024" Year="2024">
+      <Id>e2aa7833-4688-4fb4-adfc-3884f205f9ea</Id>
+    </Book>
+    <Book Series="Absolute Power: Task Force VII" Number="7" Volume="2024" Year="2024">
+      <Id>1e2aa1c3-4075-4191-afe1-81f65810bcbc</Id>
+    </Book>
+    <Book Series="Green Arrow" Number="16" Volume="2023" Year="2024">
+      <Id>002b84ef-ab7e-4f54-a7c7-65fc01b7ecb1</Id>
+    </Book>
+    <Book Series="Absolute Power" Number="4" Volume="2024" Year="2024">
+      <Id>3b55bf3a-b13c-44ba-b505-5869e99c1dae</Id>
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
