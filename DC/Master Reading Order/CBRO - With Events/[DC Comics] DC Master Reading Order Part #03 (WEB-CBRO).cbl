f8893e8b58b32e2220585934c0fc3b3c07e42c35
Artem Ryabkov <rad964@pm.me>
2024-11-03 18:14:53 +0400

    fixed Adventures of Superman 530 duplicate
    (cherry picked from commit a5ae0adb0cc0494a725eb4625c067bf711536b62)

--- DC/Master Reading Order/CBRO - With Events/[DC Comics] DC Master Reading Order Part #03 (WEB-CBRO).cbl
+++ DC/Master Reading Order/CBRO - With Events/[DC Comics] DC Master Reading Order Part #03 (WEB-CBRO).cbl
@@ -2436,9 +2436,6 @@
 <Book Series="Underworld Unleashed" Number="2" Volume="1995" Year="1995">
 <Database Name="cv" Series="5526" Issue="41753" />
 </Book>
-<Book Series="Adventures of Superman" Number="530" Volume="1987" Year="1995">
-<Database Name="cv" Series="3778" Issue="41723" />
-</Book>
 <Book Series="Superman: The Man of Tomorrow" Number="3" Volume="1995" Year="1995">
 <Database Name="cv" Series="5523" Issue="41793" />
 </Book>
