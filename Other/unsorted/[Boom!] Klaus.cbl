e6fb25f0df453d355d186d7b65f6c52634be6917
falo <1392563+falo2k@users.noreply.github.com>
2025-01-06 14:20:13 +0000

    New list.  Fairly simple series, but there for discovery and ordering the one-shots.

---
+++ Other/unsorted/[Boom!] Klaus.cbl
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[Boom!] Klaus</Name>
+<NumIssues>11</NumIssues>
+<Books>
+<Book Series="Klaus" Number="1" Volume="2015" Year="2015">
+<Database Name="cv" Series="85771" Issue="505000" />
+</Book>
+<Book Series="Klaus" Number="2" Volume="2015" Year="2015">
+<Database Name="cv" Series="85771" Issue="508492" />
+</Book>
+<Book Series="Klaus" Number="3" Volume="2015" Year="2016">
+<Database Name="cv" Series="85771" Issue="513713" />
+</Book>
+<Book Series="Klaus" Number="4" Volume="2015" Year="2016">
+<Database Name="cv" Series="85771" Issue="521371" />
+</Book>
+<Book Series="Klaus" Number="5" Volume="2015" Year="2016">
+<Database Name="cv" Series="85771" Issue="528567" />
+</Book>
+<Book Series="Klaus" Number="6" Volume="2015" Year="2016">
+<Database Name="cv" Series="85771" Issue="535395" />
+</Book>
+<Book Series="Klaus" Number="7" Volume="2015" Year="2016">
+<Database Name="cv" Series="85771" Issue="544835" />
+</Book>
+<Book Series="Klaus and the Witch of Winter" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="97056" Issue="569402" />
+</Book>
+<Book Series="Klaus and the Crisis In Xmasville" Number="1" Volume="2017" Year="2017">
+<Database Name="cv" Series="106683" Issue="644552" />
+</Book>
+<Book Series="Klaus and the Crying Snowman" Number="1" Volume="2018" Year="2018">
+<Database Name="cv" Series="116038" Issue="695563" />
+</Book>
+<Book Series="Klaus and the Life &#38; Times of Joe Christmas" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="123688" Issue="731409" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
