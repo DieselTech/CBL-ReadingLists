dc2d8f405e443dd37fbce890b847923cdd157f84
falo <1392563+falo2k@users.noreply.github.com>
2025-03-20 15:06:45 +0000

    CV ID Mapping - Other

--- Other/unsorted/Dark Horse/Concrete.cbl
+++ Other/unsorted/Dark Horse/Concrete.cbl
@@ -1,30 +1,31 @@
-<?xml version="1.0"?>
+<?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <Name>Concrete</Name>
+  <NumIssues>8</NumIssues>
   <Books>
     <Book Series="Concrete: Three Uneasy Pieces" Number="1" Volume="2012" Year="2012">
-      <Id>0ea3f739-892f-4074-9788-72567774a2aa</Id>
+      <Database Name="cv" Series="50580" Issue="346287" />
     </Book>
     <Book Series="Concrete" Number="1" Volume="2005" Year="2005">
-      <Id>c413d35c-f425-46de-8ada-ee0916755567</Id>
+      <Database Name="cv" Series="19457" Issue="116476" />
     </Book>
     <Book Series="Concrete" Number="2" Volume="2005" Year="2005">
-      <Id>d6d866d9-de8d-428c-920a-9b025fb97ce3</Id>
+      <Database Name="cv" Series="19457" Issue="116496" />
     </Book>
     <Book Series="Concrete" Number="3" Volume="2005" Year="2006">
-      <Id>677f5503-c22e-4646-9e57-a05570e0812e</Id>
+      <Database Name="cv" Series="19457" Issue="116497" />
     </Book>
     <Book Series="Concrete" Number="4" Volume="2005" Year="2006">
-      <Id>e9b71b18-6fda-4b24-b799-3ed1b1c5a825</Id>
+      <Database Name="cv" Series="19457" Issue="116498" />
     </Book>
     <Book Series="Concrete" Number="5" Volume="2005" Year="2006">
-      <Id>ef8cb8eb-86f8-4ac0-9ea1-c8978eb67c2e</Id>
+      <Database Name="cv" Series="19457" Issue="116499" />
     </Book>
     <Book Series="Concrete" Number="6" Volume="2005" Year="2006">
-      <Id>dd0926a8-7817-40a4-8181-d09a008062cc</Id>
+      <Database Name="cv" Series="19457" Issue="116500" />
     </Book>
     <Book Series="Concrete" Number="7" Volume="2005" Year="2006">
-      <Id>bcaad778-1012-417f-8605-1d7bee798f39</Id>
+      <Database Name="cv" Series="19457" Issue="116501" />
     </Book>
   </Books>
   <Matchers />
--- Other/unsorted/Dynamite/Vampirella.cbl
+++ Other/unsorted/Dynamite/Vampirella.cbl
@@ -1,93 +1,94 @@
-<?xml version="1.0"?>
+<?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <Name>Vampirella</Name>
+  <NumIssues>29</NumIssues>
   <Books>
     <Book Series="Vampirella: The Essential Warren Years" Number="1" Volume="2014" Year="2014">
-      <Id>3e0ce081-35a8-425f-9928-3e3cf9a69703</Id>
+      <Database Name="cv" Series="71805" Issue="445832" />
     </Book>
     <Book Series="Vampirella: Masters Series" Number="1" Volume="2010" Year="2010">
-      <Id>606a3477-208c-43e5-b0f8-e9b51ea648ed</Id>
+      <Database Name="cv" Series="36598" Issue="242653" />
     </Book>
     <Book Series="Vampirella: Masters Series" Number="2" Volume="2010" Year="2010">
-      <Id>351ed449-3925-4c79-adc8-4bd915748dea</Id>
+      <Database Name="cv" Series="36598" Issue="247365" />
     </Book>
     <Book Series="Vampirella: Masters Series" Number="3" Volume="2010" Year="2011">
-      <Id>74c02cf7-cd6d-40b5-8dc8-8af6bbecd6c0</Id>
+      <Database Name="cv" Series="36598" Issue="293529" />
     </Book>
     <Book Series="Vampirella: Masters Series" Number="4" Volume="2010" Year="2011">
-      <Id>29c4910e-94e9-4d1d-9d71-17927a4f8f4f</Id>
+      <Database Name="cv" Series="36598" Issue="293530" />
     </Book>
     <Book Series="Vampirella: Masters Series" Number="5" Volume="2010" Year="2011">
-      <Id>35cdff97-22de-499c-9ad5-29c8764d260b</Id>
+      <Database Name="cv" Series="36598" Issue="293531" />
     </Book>
     <Book Series="Vampirella: Masters Series" Number="6" Volume="2010" Year="2011">
-      <Id>1f85bbfb-365f-4315-b160-02d08648cde8</Id>
+      <Database Name="cv" Series="36598" Issue="299646" />
     </Book>
     <Book Series="Vampirella: Masters Series" Number="7" Volume="2010" Year="2012">
-      <Id>c3daa252-0939-4bdc-9395-22669b25ead1</Id>
+      <Database Name="cv" Series="36598" Issue="329505" />
     </Book>
     <Book Series="Vampirella: Masters Series" Number="8" Volume="2010" Year="2013">
-      <Id>15f03b54-7f53-43e6-9f0c-c0abb9d78afd</Id>
+      <Database Name="cv" Series="36598" Issue="431454" />
     </Book>
     <Book Series="Vampirella: The Dynamite Years Omnibus" Number="1" Volume="2017" Year="2017">
-      <Id>08d63e16-4101-473f-b9d1-7620b5dd5e64</Id>
+      <Database Name="cv" Series="105778" Issue="634552" />
     </Book>
     <Book Series="Vampirella: The Dynamite Years Omnibus" Number="2" Volume="2017" Year="2018">
-      <Id>274c23a6-46f2-45f9-a438-f8f29fa5e826</Id>
+      <Database Name="cv" Series="105778" Issue="656723" />
     </Book>
     <Book Series="Vampirella: The Dynamite Years Omnibus" Number="3" Volume="2017" Year="2018">
-      <Id>40190c36-d783-4dc6-81ca-179763775480</Id>
+      <Database Name="cv" Series="105778" Issue="673153" />
     </Book>
     <Book Series="Vampirella: Feary Tales" Number="1" Volume="2015" Year="2015">
-      <Id>674c7db9-6eb9-49d3-a8e6-ba3811e23651</Id>
+      <Database Name="cv" Series="83984" Issue="497977" />
     </Book>
     <Book Series="Vampirella Strikes" Number="1" Volume="2013" Year="2013">
-      <Id>c543efe6-1d5e-45b1-8d50-354cb693854d</Id>
+      <Database Name="cv" Series="55333" Issue="376679" />
     </Book>
-    <Book Series="Legenderry: Vampirella" Number="1" Volume="2015" Year="2015" Format="Trade Paper Back">
-      <Id>f242ee46-4037-4c42-8dd7-4b0de7e3e3ad</Id>
+    <Book Series="Legenderry: Vampirella" Number="1" Volume="2015" Year="2015">
+      <Database Name="cv" Series="86701" Issue="508449" />
     </Book>
     <Book Series="Dawn/Vampirella" Number="1" Volume="2014" Year="2014">
-      <Id>3402b36f-261f-414b-a758-d54a720e896b</Id>
+      <Database Name="cv" Series="76865" Issue="464995" />
     </Book>
     <Book Series="Dawn/Vampirella" Number="2" Volume="2014" Year="2014">
-      <Id>7f44f78b-e60b-46bc-99a8-16bf2484bd96</Id>
+      <Database Name="cv" Series="76865" Issue="470440" />
     </Book>
     <Book Series="Dawn/Vampirella" Number="3" Volume="2014" Year="2015">
-      <Id>409faefa-5cdb-45de-8322-6afec3cbd8d3</Id>
+      <Database Name="cv" Series="76865" Issue="479989" />
     </Book>
     <Book Series="Dawn/Vampirella" Number="4" Volume="2014" Year="2015">
-      <Id>60dbd683-a657-41e3-9f90-a87e11b8cbc8</Id>
+      <Database Name="cv" Series="76865" Issue="493056" />
     </Book>
     <Book Series="Dawn/Vampirella" Number="5" Volume="2014" Year="2015">
-      <Id>22130fc9-0f74-4883-a1d2-6e17b2e81f07</Id>
+      <Database Name="cv" Series="76865" Issue="503569" />
     </Book>
     <Book Series="Vampirella: Hollywood Horror" Number="1" Volume="2016" Year="2016">
-      <Id>57e7d210-a3e8-4b67-ad29-9c785d434df7</Id>
+      <Database Name="cv" Series="97083" Issue="569364" />
     </Book>
     <Book Series="Vampirella: Forbidden Fruit" Number="1" Volume="2017" Year="2017">
-      <Id>41c038e0-56cb-4e68-b2a0-ef5070ad3530</Id>
+      <Database Name="cv" Series="107376" Issue="649758" />
     </Book>
     <Book Series="Vampirella: The God You Know" Number="1" Volume="2018" Year="2018">
-      <Id>95829a71-fdbd-43bf-aacd-5d664aebd943</Id>
+      <Database Name="cv" Series="114275" Issue="688173" />
     </Book>
     <Book Series="Aliens/Vampirella" Number="1" Volume="2016" Year="2016">
-      <Id>320f36ed-d34b-4fa0-b1f9-f9cfeaa53095</Id>
+      <Database Name="cv" Series="92012" Issue="538527" />
     </Book>
     <Book Series="Hack/Slash vs. Vampirella" Number="1" Volume="2017" Year="2017">
-      <Id>8014cc84-3240-49e2-b827-58e5d59b4b8d</Id>
+      <Database Name="cv" Series="104788" Issue="626309" />
     </Book>
     <Book Series="Hack/Slash vs. Vampirella" Number="2" Volume="2017" Year="2017">
-      <Id>c8b8b075-7bc0-478b-9f5c-f74e778dac7c</Id>
+      <Database Name="cv" Series="104788" Issue="636405" />
     </Book>
     <Book Series="Hack/Slash vs. Vampirella" Number="3" Volume="2017" Year="2017">
-      <Id>8c8db2c0-3e17-43ff-b667-31c46edcafa4</Id>
+      <Database Name="cv" Series="104788" Issue="646208" />
     </Book>
     <Book Series="Hack/Slash vs. Vampirella" Number="4" Volume="2017" Year="2018">
-      <Id>350c0886-edc9-49a7-a8b3-a7743ea5083c</Id>
+      <Database Name="cv" Series="104788" Issue="652652" />
     </Book>
     <Book Series="Hack/Slash vs. Vampirella" Number="5" Volume="2017" Year="2018">
-      <Id>3cb35d1f-eca0-4027-a6b9-2e2faddad459</Id>
+      <Database Name="cv" Series="104788" Issue="660030" />
     </Book>
   </Books>
   <Matchers />
