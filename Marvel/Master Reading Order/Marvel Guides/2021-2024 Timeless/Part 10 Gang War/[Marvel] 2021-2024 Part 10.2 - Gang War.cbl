7d0dfb0cdef7fd655009985c5df9070c154b6912
jakem742 <jakemad@gmail.com>
2025-04-10 19:53:02 +1000

    Fixed formatting for Gang Wars

--- Marvel/Master Reading Order/Marvel Guides/2021-2024 Timeless/Part 10 Gang War/[Marvel] 2021-2024 Part 10.2 - Gang War.cbl
+++ Marvel/Master Reading Order/Marvel Guides/2021-2024 Timeless/Part 10 Gang War/[Marvel] 2021-2024 Part 10.2 - Gang War.cbl
@@ -1,1 +1,114 @@
-<?xml version="1.0" encoding="utf-8"?><ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Name>Part 2 - Gang War</Name><NumIssues>32</NumIssues><Source>Part 2 - Gang War.txt</Source><StartYear>2024</StartYear><Books><Book Series="Luke Cage: Gang War" Number="1" Volume="2024" Year="2024"><Database Name="comicvine" Series="154931" Issue="1031691" /></Book><Book Series="Spider-Woman" Number="1" Volume="2024" Year="2024"><Database Name="comicvine" Series="154933" Issue="1031698" /></Book><Book Series="Amazing Spider-Man: Gang War - First Strike" Number="1" Volume="2024" Year="2024"><Database Name="comicvine" Series="154930" Issue="1031689" /></Book><Book Series="Luke Cage: Gang War" Number="2" Volume="2024" Year="2024"><Database Name="comicvine" Series="154931" Issue="1034340" /></Book><Book Series="Luke Cage: Gang War" Number="3" Volume="2024" Year="2024"><Database Name="comicvine" Series="154931" Issue="1039580" /></Book><Book Series="Luke Cage: Gang War" Number="4" Volume="2024" Year="2024"><Database Name="comicvine" Series="154931" Issue="1045763" /></Book><Book Series="Amazing Spider-Man" Number="39" Volume="2022" Year="2024"><Database Name="comicvine" Series="142577" Issue="1033199" /><Database Name="metron" Series="3249" Issue="88807" /></Book><Book Series="Daredevil: Gang War" Number="1" Volume="2024" Year="2024"><Database Name="comicvine" Series="155340" Issue="1034336" /></Book><Book Series="Daredevil: Gang War" Number="2" Volume="2024" Year="2024"><Database Name="comicvine" Series="155340" Issue="1039578" /></Book><Book Series="Deadly Hands of Kung Fu: Gang War" Number="1" Volume="2024" Year="2024"><Database Name="comicvine" Series="155694" Issue="1038293" /></Book><Book Series="Deadly Hands of Kung Fu: Gang War" Number="2" Volume="2024" Year="2024"><Database Name="comicvine" Series="155694" Issue="1042827" /></Book><Book Series="Deadly Hands of Kung Fu: Gang War" Number="3" Volume="2024" Year="2024"><Database Name="comicvine" Series="155694" Issue="1044778" /></Book><Book Series="Miles Morales: Spider-Man" Number="13" Volume="2022" Year="2024"><Database Name="comicvine" Series="146734" Issue="1034570" /></Book><Book Series="Miles Morales: Spider-Man" Number="14" Volume="2022" Year="2024"><Database Name="comicvine" Series="146734" Issue="1038292" /></Book><Book Series="Amazing Spider-Man" Number="40" Volume="2022" Year="2024"><Database Name="comicvine" Series="142577" Issue="1035952" /><Database Name="metron" Series="3249" Issue="93118" /></Book><Book Series="Amazing Spider-Man" Number="41" Volume="2022" Year="2024"><Database Name="comicvine" Series="142577" Issue="1038980" /><Database Name="metron" Series="3249" Issue="99399" /></Book><Book Series="Miles Morales: Spider-Man" Number="15" Volume="2022" Year="2024"><Database Name="comicvine" Series="146734" Issue="1039581" /></Book><Book Series="Miles Morales: Spider-Man" Number="16" Volume="2022" Year="2024"><Database Name="comicvine" Series="146734" Issue="1044780" /></Book><Book Series="Spider-Woman" Number="2" Volume="2024" Year="2024"><Database Name="comicvine" Series="154933" Issue="1038391" /></Book><Book Series="Punisher" Number="3" Volume="2024" Year="2024"><Database Name="comicvine" Series="154552" Issue="1042225" /></Book><Book Series="Punisher" Number="4" Volume="2024" Year="2024"><Database Name="comicvine" Series="154552" Issue="1046271" /></Book><Book Series="Amazing Spider-Man" Number="42" Volume="2022" Year="2024"><Database Name="comicvine" Series="142577" Issue="1040933" /><Database Name="metron" Series="3249" Issue="104089" /></Book><Book Series="Spider-Woman" Number="3" Volume="2024" Year="2024"><Database Name="comicvine" Series="154933" Issue="1042224" /></Book><Book Series="Spider-Woman" Number="4" Volume="2024" Year="2024"><Database Name="comicvine" Series="154933" Issue="1045767" /></Book><Book Series="Jackpot" Number="1" Volume="2024" Year="2024"><Database Name="comicvine" Series="156140" Issue="1040942" /></Book><Book Series="Daredevil: Gang War" Number="3" Volume="2024" Year="2024"><Database Name="comicvine" Series="155340" Issue="1043834" /></Book><Book Series="Amazing Spider-Man" Number="43" Volume="2022" Year="2024"><Database Name="comicvine" Series="142577" Issue="1044773" /><Database Name="metron" Series="3249" Issue="107637" /></Book><Book Series="Amazing Spider-Man" Number="44" Volume="2022" Year="2024"><Database Name="comicvine" Series="142577" Issue="1046261" /><Database Name="metron" Series="3249" Issue="110046" /></Book><Book Series="Daredevil: Gang War" Number="4" Volume="2024" Year="2024"><Database Name="comicvine" Series="155340" Issue="1047082" /></Book><Book Series="The Sentry" Number="2" Volume="2024" Year="2024"><Database Name="comicvine" Series="155137" Issue="1039584" /></Book><Book Series="The Sentry" Number="3" Volume="2024" Year="2024"><Database Name="comicvine" Series="155137" Issue="1045765" /></Book><Book Series="The Sentry" Number="4" Volume="2024" Year="2024"><Database Name="comicvine" Series="155137" Issue="1047087" /></Book></Books><Matchers /></ReadingList>
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Name>Part 2 - Gang War</Name>
+    <NumIssues>32</NumIssues>
+    <Source>Part 2 - Gang War.txt</Source>
+    <StartYear>2024</StartYear>
+    <Books>
+        <Book Series="Luke Cage: Gang War" Number="1" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154931" Issue="1031691" />
+        </Book>
+        <Book Series="Spider-Woman" Number="1" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154933" Issue="1031698" />
+        </Book>
+        <Book Series="Amazing Spider-Man: Gang War - First Strike" Number="1" Volume="2024"
+            Year="2024">
+            <Database Name="comicvine" Series="154930" Issue="1031689" />
+        </Book>
+        <Book Series="Luke Cage: Gang War" Number="2" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154931" Issue="1034340" />
+        </Book>
+        <Book Series="Luke Cage: Gang War" Number="3" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154931" Issue="1039580" />
+        </Book>
+        <Book Series="Luke Cage: Gang War" Number="4" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154931" Issue="1045763" />
+        </Book>
+        <Book Series="Amazing Spider-Man" Number="39" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="142577" Issue="1033199" />
+            <Database Name="metron" Series="3249" Issue="88807" />
+        </Book>
+        <Book Series="Daredevil: Gang War" Number="1" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155340" Issue="1034336" />
+        </Book>
+        <Book Series="Daredevil: Gang War" Number="2" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155340" Issue="1039578" />
+        </Book>
+        <Book Series="Deadly Hands of Kung Fu: Gang War" Number="1" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155694" Issue="1038293" />
+        </Book>
+        <Book Series="Deadly Hands of Kung Fu: Gang War" Number="2" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155694" Issue="1042827" />
+        </Book>
+        <Book Series="Deadly Hands of Kung Fu: Gang War" Number="3" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155694" Issue="1044778" />
+        </Book>
+        <Book Series="Miles Morales: Spider-Man" Number="13" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="146734" Issue="1034570" />
+        </Book>
+        <Book Series="Miles Morales: Spider-Man" Number="14" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="146734" Issue="1038292" />
+        </Book>
+        <Book Series="Amazing Spider-Man" Number="40" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="142577" Issue="1035952" />
+            <Database Name="metron" Series="3249" Issue="93118" />
+        </Book>
+        <Book Series="Amazing Spider-Man" Number="41" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="142577" Issue="1038980" />
+            <Database Name="metron" Series="3249" Issue="99399" />
+        </Book>
+        <Book Series="Miles Morales: Spider-Man" Number="15" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="146734" Issue="1039581" />
+        </Book>
+        <Book Series="Miles Morales: Spider-Man" Number="16" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="146734" Issue="1044780" />
+        </Book>
+        <Book Series="Spider-Woman" Number="2" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154933" Issue="1038391" />
+        </Book>
+        <Book Series="Punisher" Number="3" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154552" Issue="1042225" />
+        </Book>
+        <Book Series="Punisher" Number="4" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154552" Issue="1046271" />
+        </Book>
+        <Book Series="Amazing Spider-Man" Number="42" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="142577" Issue="1040933" />
+            <Database Name="metron" Series="3249" Issue="104089" />
+        </Book>
+        <Book Series="Spider-Woman" Number="3" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154933" Issue="1042224" />
+        </Book>
+        <Book Series="Spider-Woman" Number="4" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154933" Issue="1045767" />
+        </Book>
+        <Book Series="Jackpot" Number="1" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="156140" Issue="1040942" />
+        </Book>
+        <Book Series="Daredevil: Gang War" Number="3" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155340" Issue="1043834" />
+        </Book>
+        <Book Series="Amazing Spider-Man" Number="43" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="142577" Issue="1044773" />
+            <Database Name="metron" Series="3249" Issue="107637" />
+        </Book>
+        <Book Series="Amazing Spider-Man" Number="44" Volume="2022" Year="2024">
+            <Database Name="comicvine" Series="142577" Issue="1046261" />
+            <Database Name="metron" Series="3249" Issue="110046" />
+        </Book>
+        <Book Series="Daredevil: Gang War" Number="4" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155340" Issue="1047082" />
+        </Book>
+        <Book Series="The Sentry" Number="2" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155137" Issue="1039584" />
+        </Book>
+        <Book Series="The Sentry" Number="3" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155137" Issue="1045765" />
+        </Book>
+        <Book Series="The Sentry" Number="4" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="155137" Issue="1047087" />
+        </Book>
+    </Books>
+    <Matchers />
+</ReadingList>
--- Marvel/Master Reading Order/Marvel Guides/2021-2024 Timeless/Part 10 Gang War/[Marvel] 2021-2024 Part 10.3 - Symbiosis Necrosis.cbl
+++ Marvel/Master Reading Order/Marvel Guides/2021-2024 Timeless/Part 10 Gang War/[Marvel] 2021-2024 Part 10.3 - Symbiosis Necrosis.cbl
@@ -1,1 +1,50 @@
-<?xml version="1.0" encoding="utf-8"?><ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Name>Part 3 - Symbiosis Necrosis</Name><NumIssues>13</NumIssues><Source>Part 3 - Symbiosis Necrosis.txt</Source><StartYear>2023</StartYear><Books><Book Series="Carnage" Number="1" Volume="2024" Year="2024"><Database Name="comicvine" Series="154836" Issue="1030841" /></Book><Book Series="Venom" Number="26" Volume="2021" Year="2023"><Database Name="comicvine" Series="140084" Issue="1024059" /></Book><Book Series="Venom" Number="27" Volume="2021" Year="2024"><Database Name="comicvine" Series="140084" Issue="1027603" /></Book><Book Series="Venom" Number="28" Volume="2021" Year="2024"><Database Name="comicvine" Series="140084" Issue="1033213" /></Book><Book Series="Carnage" Number="2" Volume="2024" Year="2024"><Database Name="comicvine" Series="154836" Issue="1038386" /></Book><Book Series="Carnage" Number="3" Volume="2024" Year="2024"><Database Name="comicvine" Series="154836" Issue="1042824" /></Book><Book Series="Carnage" Number="4" Volume="2024" Year="2024"><Database Name="comicvine" Series="154836" Issue="1046264" /></Book><Book Series="Venom" Number="29" Volume="2021" Year="2024"><Database Name="comicvine" Series="140084" Issue="1038981" /></Book><Book Series="Venom" Number="30" Volume="2021" Year="2024"><Database Name="comicvine" Series="140084" Issue="1043842" /></Book><Book Series="Venom" Number="31" Volume="2021" Year="2024"><Database Name="comicvine" Series="140084" Issue="1047093" /></Book><Book Series="Carnage" Number="5" Volume="2024" Year="2024"><Database Name="comicvine" Series="154836" Issue="1047945" /></Book><Book Series="Venom" Number="32" Volume="2021" Year="2024"><Database Name="comicvine" Series="140084" Issue="1050578" /></Book><Book Series="Carnage" Number="6" Volume="2024" Year="2024"><Database Name="comicvine" Series="154836" Issue="1051120" /></Book></Books><Matchers /></ReadingList>
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Name>Part 3 - Symbiosis Necrosis</Name>
+    <NumIssues>13</NumIssues>
+    <Source>Part 3 - Symbiosis Necrosis.txt</Source>
+    <StartYear>2023</StartYear>
+    <Books>
+        <Book Series="Carnage" Number="1" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154836" Issue="1030841" />
+        </Book>
+        <Book Series="Venom" Number="26" Volume="2021" Year="2023">
+            <Database Name="comicvine" Series="140084" Issue="1024059" />
+        </Book>
+        <Book Series="Venom" Number="27" Volume="2021" Year="2024">
+            <Database Name="comicvine" Series="140084" Issue="1027603" />
+        </Book>
+        <Book Series="Venom" Number="28" Volume="2021" Year="2024">
+            <Database Name="comicvine" Series="140084" Issue="1033213" />
+        </Book>
+        <Book Series="Carnage" Number="2" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154836" Issue="1038386" />
+        </Book>
+        <Book Series="Carnage" Number="3" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154836" Issue="1042824" />
+        </Book>
+        <Book Series="Carnage" Number="4" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154836" Issue="1046264" />
+        </Book>
+        <Book Series="Venom" Number="29" Volume="2021" Year="2024">
+            <Database Name="comicvine" Series="140084" Issue="1038981" />
+        </Book>
+        <Book Series="Venom" Number="30" Volume="2021" Year="2024">
+            <Database Name="comicvine" Series="140084" Issue="1043842" />
+        </Book>
+        <Book Series="Venom" Number="31" Volume="2021" Year="2024">
+            <Database Name="comicvine" Series="140084" Issue="1047093" />
+        </Book>
+        <Book Series="Carnage" Number="5" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154836" Issue="1047945" />
+        </Book>
+        <Book Series="Venom" Number="32" Volume="2021" Year="2024">
+            <Database Name="comicvine" Series="140084" Issue="1050578" />
+        </Book>
+        <Book Series="Carnage" Number="6" Volume="2024" Year="2024">
+            <Database Name="comicvine" Series="154836" Issue="1051120" />
+        </Book>
+    </Books>
+    <Matchers />
+</ReadingList>
