1b4236164c41d4397244924270cc06722dda1bcd
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2025-04-15 10:45:46 +0100

    Update [Marvel] Heroes Reborn (1996) (WEB-CBRO).cbl

--- Marvel/Events/CBRO/1992-1999 - Part 5/[Marvel] Heroes Reborn (1996) (WEB-CBRO).cbl
+++ Marvel/Events/CBRO/1992-1999 - Part 5/[Marvel] Heroes Reborn (1996) (WEB-CBRO).cbl
@@ -3,77 +3,38 @@
 <Name>[Marvel] Heroes Reborn (1996) (WEB-CBRO)</Name>
 <NumIssues>63</NumIssues>
 <Books>
+<Book Series="Captain America" Number="1" Volume="1996" Year="1996">
+<Database Name="cv" Series="5776" Issue="42979" />
+</Book>
+<Book Series="Captain America" Number="2" Volume="1996" Year="1996">
+<Database Name="cv" Series="5776" Issue="43072" />
+</Book>
+<Book Series="Captain America" Number="3" Volume="1996" Year="1997">
+<Database Name="cv" Series="5776" Issue="65827" />
+</Book>
+<Book Series="Captain America" Number="4" Volume="1996" Year="1997">
+<Database Name="cv" Series="5776" Issue="65828" />
+</Book>
+<Book Series="Captain America" Number="5" Volume="1996" Year="1997">
+<Database Name="cv" Series="5776" Issue="65829" />
+</Book>
 <Book Series="Fantastic Four" Number="1" Volume="1996" Year="1996">
 <Database Name="cv" Series="9462" Issue="77803" />
 </Book>
 <Book Series="Fantastic Four" Number="2" Volume="1996" Year="1996">
 <Database Name="cv" Series="9462" Issue="77804" />
 </Book>
-<Book Series="Fantastic Four" Number="3" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77805" />
-</Book>
-<Book Series="Fantastic Four" Number="4" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77806" />
-</Book>
-<Book Series="Fantastic Four" Number="5" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77807" />
-</Book>
-<Book Series="Fantastic Four" Number="6" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77808" />
-</Book>
-<Book Series="Fantastic Four" Number="7" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77809" />
-</Book>
-<Book Series="Fantastic Four" Number="8" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77810" />
-</Book>
-<Book Series="Fantastic Four" Number="9" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77811" />
-</Book>
-<Book Series="Fantastic Four" Number="10" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77812" />
-</Book>
-<Book Series="Fantastic Four" Number="11" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77813" />
-</Book>
-<Book Series="Fantastic Four" Number="12" Volume="1996" Year="1997">
-<Database Name="cv" Series="9462" Issue="77814" />
-</Book>
 <Book Series="Avengers" Number="1" Volume="1996" Year="1996">
 <Database Name="cv" Series="6742" Issue="48257" />
+</Book>
+<Book Series="Fantastic Four" Number="3" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77805" />
 </Book>
 <Book Series="Avengers" Number="2" Volume="1996" Year="1996">
 <Database Name="cv" Series="6742" Issue="48258" />
 </Book>
 <Book Series="Avengers" Number="3" Volume="1996" Year="1997">
 <Database Name="cv" Series="6742" Issue="48259" />
-</Book>
-<Book Series="Avengers" Number="4" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48260" />
-</Book>
-<Book Series="Avengers" Number="5" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48261" />
-</Book>
-<Book Series="Avengers" Number="6" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48262" />
-</Book>
-<Book Series="Avengers" Number="7" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48263" />
-</Book>
-<Book Series="Avengers" Number="8" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48264" />
-</Book>
-<Book Series="Avengers" Number="9" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48265" />
-</Book>
-<Book Series="Avengers" Number="10" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48266" />
-</Book>
-<Book Series="Avengers" Number="11" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48267" />
-</Book>
-<Book Series="Avengers" Number="12" Volume="1996" Year="1997">
-<Database Name="cv" Series="6742" Issue="48268" />
 </Book>
 <Book Series="Iron Man" Number="1" Volume="1996" Year="1996">
 <Database Name="cv" Series="18800" Issue="111124" />
@@ -90,8 +51,32 @@
 <Book Series="Iron Man" Number="5" Volume="1996" Year="1997">
 <Database Name="cv" Series="18800" Issue="157728" />
 </Book>
+<Book Series="Avengers" Number="4" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48260" />
+</Book>
+<Book Series="Avengers" Number="5" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48261" />
+</Book>
+<Book Series="Fantastic Four" Number="4" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77806" />
+</Book>
+<Book Series="Fantastic Four" Number="5" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77807" />
+</Book>
+<Book Series="Fantastic Four" Number="6" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77808" />
+</Book>
+<Book Series="Avengers" Number="6" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48262" />
+</Book>
 <Book Series="Iron Man" Number="6" Volume="1996" Year="1997">
 <Database Name="cv" Series="18800" Issue="157729" />
+</Book>
+<Book Series="Captain America" Number="6" Volume="1996" Year="1997">
+<Database Name="cv" Series="5776" Issue="43574" />
+</Book>
+<Book Series="Fantastic Four" Number="7" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77809" />
 </Book>
 <Book Series="Iron Man" Number="7" Volume="1996" Year="1997">
 <Database Name="cv" Series="18800" Issue="157730" />
@@ -102,35 +87,38 @@
 <Book Series="Iron Man" Number="9" Volume="1996" Year="1997">
 <Database Name="cv" Series="18800" Issue="157732" />
 </Book>
+<Book Series="Avengers" Number="7" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48263" />
+</Book>
+<Book Series="Avengers" Number="8" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48264" />
+</Book>
+<Book Series="Captain America" Number="7" Volume="1996" Year="1997">
+<Database Name="cv" Series="5776" Issue="43680" />
+</Book>
+<Book Series="Avengers" Number="9" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48265" />
+</Book>
+<Book Series="Avengers" Number="10" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48266" />
+</Book>
+<Book Series="Avengers" Number="11" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48267" />
+</Book>
 <Book Series="Iron Man" Number="10" Volume="1996" Year="1997">
 <Database Name="cv" Series="18800" Issue="157733" />
 </Book>
 <Book Series="Iron Man" Number="11" Volume="1996" Year="1997">
 <Database Name="cv" Series="18800" Issue="157734" />
 </Book>
-<Book Series="Iron Man" Number="12" Volume="1996" Year="1997">
-<Database Name="cv" Series="18800" Issue="157735" />
+<Book Series="Fantastic Four" Number="8" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77810" />
 </Book>
-<Book Series="Captain America" Number="1" Volume="1996" Year="1996">
-<Database Name="cv" Series="5776" Issue="42979" />
+<Book Series="Fantastic Four" Number="9" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77811" />
 </Book>
-<Book Series="Captain America" Number="2" Volume="1996" Year="1996">
-<Database Name="cv" Series="5776" Issue="43072" />
-</Book>
-<Book Series="Captain America" Number="3" Volume="1996" Year="1997">
-<Database Name="cv" Series="5776" Issue="65827" />
-</Book>
-<Book Series="Captain America" Number="4" Volume="1996" Year="1997">
-<Database Name="cv" Series="5776" Issue="65828" />
-</Book>
-<Book Series="Captain America" Number="5" Volume="1996" Year="1997">
-<Database Name="cv" Series="5776" Issue="65829" />
-</Book>
-<Book Series="Captain America" Number="6" Volume="1996" Year="1997">
-<Database Name="cv" Series="5776" Issue="43574" />
-</Book>
-<Book Series="Captain America" Number="7" Volume="1996" Year="1997">
-<Database Name="cv" Series="5776" Issue="43680" />
+<Book Series="Fantastic Four" Number="10" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77812" />
 </Book>
 <Book Series="Captain America" Number="8" Volume="1996" Year="1997">
 <Database Name="cv" Series="5776" Issue="43770" />
@@ -143,6 +131,18 @@
 </Book>
 <Book Series="Captain America" Number="11" Volume="1996" Year="1997">
 <Database Name="cv" Series="5776" Issue="65831" />
+</Book>
+<Book Series="Fantastic Four" Number="11" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77813" />
+</Book>
+<Book Series="Fantastic Four" Number="12" Volume="1996" Year="1997">
+<Database Name="cv" Series="9462" Issue="77814" />
+</Book>
+<Book Series="Avengers" Number="12" Volume="1996" Year="1997">
+<Database Name="cv" Series="6742" Issue="48268" />
+</Book>
+<Book Series="Iron Man" Number="12" Volume="1996" Year="1997">
+<Database Name="cv" Series="18800" Issue="157735" />
 </Book>
 <Book Series="Captain America" Number="12" Volume="1996" Year="1997">
 <Database Name="cv" Series="5776" Issue="65832" />
