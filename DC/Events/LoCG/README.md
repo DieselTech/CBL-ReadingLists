f2ce5711f185231de4407a845b0215129db733b3
flips22 <1898114+flips22@users.noreply.github.com>
2025-06-10 20:20:36 -0500

    June 2025 LoCG Event Updates

--- DC/Events/LoCG/README.md
+++ DC/Events/LoCG/README.md
@@ -3,7 +3,7 @@ This is a collection of all the events from The League of Comic Geeks site:
 https://leagueofcomicgeeks.com/comics/events
 
 ## Updated:
-December 2023
+June 2025
 
 ## Notes:
-Some of the events are ongoing, depecited in the file name i.e. [2023-Present]  On the site, they have additional issues, but there are not IDs on comicvine yet. Once they are added to comicvine, the reading lists will be updated.
+Some of the events are ongoing, depecited in the file name i.e. [2024-Present]  On the site, they have additional issues, but there are not IDs on comicvine yet. Once they are added to comicvine, the reading lists will be updated.
---
+++ Marvel/Events/LoCG/[2024] The Infinity Watch (Marvel Comics)(LoCG).cbl
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[2024] The Infinity Watch (Marvel Comics)(LoCG)</Name>
+<NumIssues>9</NumIssues>
+<Books>
+<Book Series="Thanos Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="158598" Issue="1060862" />
+</Book>
+<Book Series="The Amazing Spider-Man Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="158693" Issue="1061866" />
+</Book>
+<Book Series="The Immortal Thor Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="158867" Issue="1063229" />
+</Book>
+<Book Series="Ms. Marvel Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="159124" Issue="1065483" />
+</Book>
+<Book Series="Wolverine Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="159243" Issue="1066711" />
+</Book>
+<Book Series="Incredible Hulk Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="159483" Issue="1067979" />
+</Book>
+<Book Series="Moon Knight Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="159607" Issue="1068657" />
+</Book>
+<Book Series="Spider-Boy Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="159811" Issue="1070133" />
+</Book>
+<Book Series="Avengers Annual" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="160079" Issue="1071598" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
