cee70530e260bedbf06191a25ca485f800dc4ddc
falo <1392563+falo2k@users.noreply.github.com>
2025-08-06 14:55:40 +0100

    Fixing a strange character in filename
    It made a basic print to stdout whinge about encoding, so I can't imagine it's healthy for all app consumers!

--- Marvel/Master Reading Order/Marvel Guides/2015-2018 ANAD Marvel/Part 06 Civil War II/[Marvel] 2015-2018 Part 6.3 The Accused and the Fallen (MG).cbl
+++ Marvel/Master Reading Order/Marvel Guides/2015-2018 ANAD Marvel/Part 06 Civil War II/[Marvel] 2015-2018 Part 6.3 The Accused and the Fallen (MG).cbl
@@ -1,6 +1,6 @@
 <?xml version='1.0' encoding='utf-8'?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <Name>[Marvel] 2015-2018 Part 6.3 The Accused and the Fallen  (MG)</Name>
+    <Name>[Marvel] 2015-2018 Part 6.3 The Accused and the Fallen (MG)</Name>
     <NumIssues>48</NumIssues>
     <Books>
         <Book Series="Nova" Number="8" Volume="2015" Year="2016">
