e014fae98fa528e41d9427271293888388f1da73
Cducharme84 <131784515+Cducharme84@users.noreply.github.com>
2025-08-24 12:08:09 -0400

    Deadworld (#393)
    Deadworld across the releases - one TPB `Realm of the Dead`  included as CV doesn't list them individually

---
+++ Other/unsorted/Deadworld.cbl
@@ -0,0 +1,245 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>Deadworld</Name>
+<NumIssues>79</NumIssues>
+<Books>
+<Book Series="Deadworld" Number="1" Volume="1986" Year="1986">
+<Database Name="cv" Series="23983" Issue="302296" />
+</Book>
+<Book Series="Deadworld" Number="2" Volume="1986" Year="1987">
+<Database Name="cv" Series="23983" Issue="302295" />
+</Book>
+<Book Series="Deadworld" Number="3" Volume="1986" Year="1987">
+<Database Name="cv" Series="23983" Issue="302294" />
+</Book>
+<Book Series="Deadworld" Number="4" Volume="1986" Year="1987">
+<Database Name="cv" Series="23983" Issue="302293" />
+</Book>
+<Book Series="Deadworld" Number="5" Volume="1986" Year="1987">
+<Database Name="cv" Series="23983" Issue="302292" />
+</Book>
+<Book Series="Deadworld" Number="6" Volume="1986" Year="1987">
+<Database Name="cv" Series="23983" Issue="302291" />
+</Book>
+<Book Series="Deadworld" Number="7" Volume="1986" Year="1988">
+<Database Name="cv" Series="23983" Issue="302290" />
+</Book>
+<Book Series="Deadworld" Number="8" Volume="1986" Year="1988">
+<Database Name="cv" Series="23983" Issue="302289" />
+</Book>
+<Book Series="Deadworld" Number="9" Volume="1986" Year="1988">
+<Database Name="cv" Series="23983" Issue="302288" />
+</Book>
+<Book Series="Deadworld" Number="10" Volume="1986" Year="1988">
+<Database Name="cv" Series="23983" Issue="302012" />
+</Book>
+<Book Series="Deadworld" Number="11" Volume="1986" Year="1989">
+<Database Name="cv" Series="23983" Issue="302013" />
+</Book>
+<Book Series="Deadworld" Number="12" Volume="1986" Year="1989">
+<Database Name="cv" Series="23983" Issue="142876" />
+</Book>
+<Book Series="Deadworld" Number="13" Volume="1986" Year="1989">
+<Database Name="cv" Series="23983" Issue="302014" />
+</Book>
+<Book Series="Deadworld" Number="14" Volume="1986" Year="1990">
+<Database Name="cv" Series="23983" Issue="302015" />
+</Book>
+<Book Series="Deadworld" Number="15" Volume="1986" Year="1990">
+<Database Name="cv" Series="23983" Issue="302016" />
+</Book>
+<Book Series="Deadworld" Number="16" Volume="1986" Year="1990">
+<Database Name="cv" Series="23983" Issue="142877" />
+</Book>
+<Book Series="Deadworld" Number="17" Volume="1986" Year="1990">
+<Database Name="cv" Series="23983" Issue="142878" />
+</Book>
+<Book Series="Deadworld" Number="18" Volume="1986" Year="1991">
+<Database Name="cv" Series="23983" Issue="142879" />
+</Book>
+<Book Series="Deadworld" Number="19" Volume="1986" Year="1991">
+<Database Name="cv" Series="23983" Issue="142880" />
+</Book>
+<Book Series="Deadworld" Number="20" Volume="1986" Year="1991">
+<Database Name="cv" Series="23983" Issue="142881" />
+</Book>
+<Book Series="Deadworld" Number="21" Volume="1986" Year="1991">
+<Database Name="cv" Series="23983" Issue="302017" />
+</Book>
+<Book Series="Deadworld" Number="22" Volume="1986" Year="1991">
+<Database Name="cv" Series="23983" Issue="142882" />
+</Book>
+<Book Series="Deadworld" Number="23" Volume="1986" Year="1991">
+<Database Name="cv" Series="23983" Issue="142998" />
+</Book>
+<Book Series="Deadworld" Number="24" Volume="1986" Year="1992">
+<Database Name="cv" Series="23983" Issue="142999" />
+</Book>
+<Book Series="Deadworld" Number="25" Volume="1986" Year="1992">
+<Database Name="cv" Series="23983" Issue="143000" />
+</Book>
+<Book Series="Deadworld" Number="26" Volume="1986" Year="1993">
+<Database Name="cv" Series="23983" Issue="205954" />
+</Book>
+<Book Series="Deadworld" Number="1" Volume="1992" Year="1992">
+<Database Name="cv" Series="44047" Issue="302738" />
+</Book>
+<Book Series="Deadworld: To Kill A King" Number="1" Volume="1992" Year="1992">
+<Database Name="cv" Series="24026" Issue="143018" />
+</Book>
+<Book Series="Deadworld" Number="2" Volume="1992" Year="1993">
+<Database Name="cv" Series="44047" Issue="302737" />
+</Book>
+<Book Series="Deadworld" Number="3" Volume="1992" Year="1993">
+<Database Name="cv" Series="44047" Issue="302736" />
+</Book>
+<Book Series="Deadworld" Number="4" Volume="1992" Year="1993">
+<Database Name="cv" Series="44047" Issue="302735" />
+</Book>
+<Book Series="Deadworld" Number="5" Volume="1992" Year="1993">
+<Database Name="cv" Series="44047" Issue="302734" />
+</Book>
+<Book Series="Deadworld: To Kill A King" Number="2" Volume="1992" Year="1993">
+<Database Name="cv" Series="24026" Issue="302740" />
+</Book>
+<Book Series="Deadworld" Number="6" Volume="1992" Year="1993">
+<Database Name="cv" Series="44047" Issue="302733" />
+</Book>
+<Book Series="Deadworld: To Kill A King" Number="3" Volume="1992" Year="1993">
+<Database Name="cv" Series="24026" Issue="302739" />
+</Book>
+<Book Series="Deadworld" Number="7" Volume="1992" Year="1994">
+<Database Name="cv" Series="44047" Issue="302732" />
+</Book>
+<Book Series="Deadworld" Number="8" Volume="1992" Year="1994">
+<Database Name="cv" Series="44047" Issue="302731" />
+</Book>
+<Book Series="Deadworld" Number="9" Volume="1992" Year="1994">
+<Database Name="cv" Series="44047" Issue="302730" />
+</Book>
+<Book Series="Deadworld" Number="10" Volume="1992" Year="1994">
+<Database Name="cv" Series="44047" Issue="302729" />
+</Book>
+<Book Series="Deadworld" Number="11" Volume="1992" Year="1994">
+<Database Name="cv" Series="44047" Issue="302728" />
+</Book>
+<Book Series="Deadworld" Number="12" Volume="1992" Year="1994">
+<Database Name="cv" Series="44047" Issue="302727" />
+</Book>
+<Book Series="Deadworld" Number="13" Volume="1992" Year="1994">
+<Database Name="cv" Series="44047" Issue="302726" />
+</Book>
+<Book Series="Deadworld" Number="14" Volume="1992" Year="1994">
+<Database Name="cv" Series="44047" Issue="302725" />
+</Book>
+<Book Series="Deadworld" Number="15" Volume="1992" Year="1995">
+<Database Name="cv" Series="44047" Issue="302724" />
+</Book>
+<Book Series="Realm of the Dead" Number="1" Volume="1993" Year="1993">
+<Database Name="cv" Series="44227" Issue="305398" />
+</Book>
+<Book Series="Roadkill: A Chronicle of the Deadworld" Number="1" Volume="1993" Year="1993">
+<Database Name="cv" Series="44213" Issue="304147" />
+</Book>
+<Book Series="Tattoo" Number="1" Volume="1994" Year="1994">
+<Database Name="cv" Series="48426" Issue="333704" />
+</Book>
+<Book Series="Tattoo" Number="2" Volume="1994" Year="0">
+<Database Name="cv" Series="48426" Issue="333849" />
+</Book>
+<Book Series="Tattoo" Number="3" Volume="1994" Year="0">
+<Database Name="cv" Series="48426" Issue="334075" />
+</Book>
+<Book Series="Tattoo" Number="4" Volume="1994" Year="1997">
+<Database Name="cv" Series="48426" Issue="334171" />
+</Book>
+<Book Series="The Dire Wolves: A Chronicle of the Deadworld" Number="1" Volume="1994" Year="1994">
+<Database Name="cv" Series="46350" Issue="319401" />
+</Book>
+<Book Series="Deadworld Chronicles: Plague" Number="1" Volume="1994" Year="0">
+<Database Name="cv" Series="46276" Issue="318817" />
+</Book>
+<Book Series="Deadworld: Necropolis" Number="1" Volume="1995" Year="1995">
+<Database Name="cv" Series="44228" Issue="305397" />
+</Book>
+<Book Series="Deadworld: Daemonstorm" Number="1" Volume="1997" Year="1997">
+<Database Name="cv" Series="46313" Issue="318984" />
+</Book>
+<Book Series="Daemonstorm" Number="1" Volume="1997" Year="0">
+<Database Name="cv" Series="24251" Issue="144355" />
+</Book>
+<Book Series="King Zombie" Number="1" Volume="1998" Year="1998">
+<Database Name="cv" Series="23750" Issue="142125" />
+</Book>
+<Book Series="King Zombie" Number="2" Volume="1998" Year="1999">
+<Database Name="cv" Series="23750" Issue="304353" />
+</Book>
+<Book Series="Deadworld" Number="1" Volume="2005" Year="2005">
+<Database Name="cv" Series="19353" Issue="116103" />
+</Book>
+<Book Series="Deadworld" Number="2" Volume="2005" Year="2005">
+<Database Name="cv" Series="19353" Issue="205958" />
+</Book>
+<Book Series="Deadworld" Number="3" Volume="2005" Year="2006">
+<Database Name="cv" Series="19353" Issue="205957" />
+</Book>
+<Book Series="Deadworld" Number="4" Volume="2005" Year="2006">
+<Database Name="cv" Series="19353" Issue="205956" />
+</Book>
+<Book Series="Deadworld" Number="5" Volume="2005" Year="2006">
+<Database Name="cv" Series="19353" Issue="205955" />
+</Book>
+<Book Series="Deadworld" Number="6" Volume="2005" Year="2006">
+<Database Name="cv" Series="19353" Issue="220550" />
+</Book>
+<Book Series="Deadworld: Frozen Over" Number="1" Volume="2007" Year="2007">
+<Database Name="cv" Series="23010" Issue="173016" />
+</Book>
+<Book Series="Deadworld: Frozen Over" Number="2" Volume="2007" Year="2008">
+<Database Name="cv" Series="23010" Issue="173015" />
+</Book>
+<Book Series="Deadworld: Frozen Over" Number="3" Volume="2007" Year="2008">
+<Database Name="cv" Series="23010" Issue="138751" />
+</Book>
+<Book Series="Deadworld: Frozen Over" Number="4" Volume="2007" Year="2010">
+<Database Name="cv" Series="23010" Issue="209281" />
+</Book>
+<Book Series="Deadworld: Slaughterhouse" Number="1" Volume="2008" Year="2008">
+<Database Name="cv" Series="23533" Issue="141174" />
+</Book>
+<Book Series="Deadworld: The Last Siesta" Number="1" Volume="2014" Year="2014">
+<Database Name="cv" Series="75600" Issue="459368" />
+</Book>
+<Book Series="Deadworld: War of the Dead" Number="1" Volume="2012" Year="2012">
+<Database Name="cv" Series="51116" Issue="349721" />
+</Book>
+<Book Series="Deadworld: War of the Dead" Number="2" Volume="2012" Year="2012">
+<Database Name="cv" Series="51116" Issue="349722" />
+</Book>
+<Book Series="Deadworld: War of the Dead" Number="3" Volume="2012" Year="2012">
+<Database Name="cv" Series="51116" Issue="351238" />
+</Book>
+<Book Series="Deadworld: War of the Dead" Number="4" Volume="2012" Year="2012">
+<Database Name="cv" Series="51116" Issue="354064" />
+</Book>
+<Book Series="Deadworld: War of the Dead" Number="5" Volume="2012" Year="2012">
+<Database Name="cv" Series="51116" Issue="355995" />
+</Book>
+<Book Series="Deadworld: Restoration" Number="1" Volume="2014" Year="2013">
+<Database Name="cv" Series="69691" Issue="435604" />
+</Book>
+<Book Series="Deadworld: Restoration" Number="2" Volume="2014" Year="2014">
+<Database Name="cv" Series="69691" Issue="440258" />
+</Book>
+<Book Series="Deadworld: Restoration" Number="3" Volume="2014" Year="2014">
+<Database Name="cv" Series="69691" Issue="444557" />
+</Book>
+<Book Series="Deadworld: Restoration" Number="4" Volume="2014" Year="2014">
+<Database Name="cv" Series="69691" Issue="447528" />
+</Book>
+<Book Series="Deadworld: Restoration" Number="5" Volume="2014" Year="2014">
+<Database Name="cv" Series="69691" Issue="450089" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
