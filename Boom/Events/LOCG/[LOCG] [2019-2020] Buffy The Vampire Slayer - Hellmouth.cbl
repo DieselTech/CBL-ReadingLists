b7f1ba91b7ca6e0c11e1ddfcc8af15a259151017
ThoughtzThruKeyz <shoesonfleek@gmail.com>
2025-09-15 00:19:18 -0500

    Create [LOCG] [2019-2020] Buffy The Vampire Slayer - Hellmouth.cbl

---
+++ Boom/Events/LOCG/[LOCG] [2019-2020] Buffy The Vampire Slayer - Hellmouth.cbl
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[LOCG] [2019-2020] Buffy The Vampire Slayer - Hellmouth</Name>
+<NumIssues>14</NumIssues>
+<Books>
+<Book Series="Buffy the Vampire Slayer" Number="8" Volume="2019" Year="2019">
+<Database Name="cv" Series="116678" Issue="718043" />
+</Book>
+<Book Series="Angel" Number="5" Volume="2019" Year="2019">
+<Database Name="cv" Series="118417" Issue="720112" />
+</Book>
+<Book Series="Hellmouth" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="121876" Issue="721727" />
+</Book>
+<Book Series="Angel" Number="6" Volume="2019" Year="2019">
+<Database Name="cv" Series="118417" Issue="723680" />
+</Book>
+<Book Series="Buffy the Vampire Slayer" Number="9" Volume="2019" Year="2019">
+<Database Name="cv" Series="116678" Issue="726037" />
+</Book>
+<Book Series="Hellmouth" Number="2" Volume="2019" Year="2019">
+<Database Name="cv" Series="121876" Issue="727091" />
+</Book>
+<Book Series="Angel" Number="7" Volume="2019" Year="2019">
+<Database Name="cv" Series="118417" Issue="728953" />
+</Book>
+<Book Series="Buffy the Vampire Slayer" Number="10" Volume="2019" Year="2019">
+<Database Name="cv" Series="116678" Issue="729616" />
+</Book>
+<Book Series="Hellmouth" Number="3" Volume="2019" Year="2019">
+<Database Name="cv" Series="121876" Issue="730295" />
+</Book>
+<Book Series="Angel" Number="8" Volume="2019" Year="2019">
+<Database Name="cv" Series="118417" Issue="731401" />
+</Book>
+<Book Series="Buffy the Vampire Slayer" Number="11" Volume="2019" Year="2020">
+<Database Name="cv" Series="116678" Issue="732816" />
+</Book>
+<Book Series="Hellmouth" Number="4" Volume="2019" Year="2020">
+<Database Name="cv" Series="121876" Issue="733772" />
+</Book>
+<Book Series="Buffy the Vampire Slayer" Number="12" Volume="2019" Year="2020">
+<Database Name="cv" Series="116678" Issue="736420" />
+</Book>
+<Book Series="Hellmouth" Number="5" Volume="2019" Year="2020">
+<Database Name="cv" Series="121876" Issue="737101" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
