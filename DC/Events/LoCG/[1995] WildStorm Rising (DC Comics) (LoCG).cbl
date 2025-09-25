86b3782903c1274b0e1cf109684d8ba7597886bc
ThoughtzThruKeyz <shoesonfleek@gmail.com>
2025-09-25 03:37:02 -0500

    WildStorm Rising (LOCG) (#406)
    Stormwatch and WildC.A.T.S teams end up at odds but have to stop fighting long enough to stop Helspont and Defile from collecting all the keys to unlock the Daemonite ship (buried on Earth for centuries) which could destroy Earth.
    
    https://leagueofcomicgeeks.com/comics/event/14475/wildstorm-rising

---
+++ DC/Events/LoCG/[1995] WildStorm Rising (DC Comics) (LoCG).cbl
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[1995] WildStorm Rising (DC Comics)(LoCG)</Name>
+<NumIssues>9</NumIssues>
+<Books>
+<Book Series="Team 7 - Objective: Hell" Number="1" Volume="1995" Year="1995">
+<Database Name="cv" Series="5676" Issue="40840" />
+</Book>
+<Book Series="Wildstorm Rising" Number="1" Volume="1996" Year="1995">
+<Database Name="cv" Series="18214" Issue="106706" />
+</Book>
+<Book Series="WildC.A.T.s: Covert Action Teams" Number="20" Volume="1992" Year="1995">
+<Database Name="cv" Series="6625" Issue="107608" />
+</Book>
+<Book Series="Union" Number="4" Volume="1995" Year="1995">
+<Database Name="cv" Series="20667" Issue="157948" />
+</Book>
+<Book Series="Gen 13" Number="2" Volume="1995" Year="1995">
+<Database Name="cv" Series="5667" Issue="122162" />
+</Book>
+<Book Series="Grifter" Number="1" Volume="1996" Year="1996">
+<Database Name="cv" Series="25866" Issue="152883" />
+</Book>
+<Book Series="Backlash" Number="8" Volume="1994" Year="1995">
+<Database Name="cv" Series="18355" Issue="107817" />
+</Book>
+<Book Series="Stormwatch" Number="22" Volume="1993" Year="1995">
+<Database Name="cv" Series="5174" Issue="130033" />
+</Book>
+<Book Series="Wildstorm Rising" Number="2" Volume="1996" Year="1995">
+<Database Name="cv" Series="18214" Issue="106705" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
