a064e6ce727c87e3f523bb31414b609bd5c05f9f
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2025-12-12 14:49:08 +0000

    Update [Marvel] Marvel Master Reading Order Part #12 (WEB-CBRO).cbl
    Removed Secret X-Men #1
    Added Last Annihilation event
    https://comicbookreadingorders.com/marvel/marvel-master-reading-order-part-12/
    https://comicbookreadingorders.com/marvel/events/last-annihilation-reading-order/

--- Marvel/Master Reading Order/CBRO - With Events/[Marvel] Marvel Master Reading Order Part #12 (WEB-CBRO).cbl
+++ Marvel/Master Reading Order/CBRO - With Events/[Marvel] Marvel Master Reading Order Part #12 (WEB-CBRO).cbl
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 <Name>[Marvel] Marvel Master Reading Order Part #12 (WEB-CBRO)</Name>
-<NumIssues>704</NumIssues>
+<NumIssues>710</NumIssues>
 <Books>
 <Book Series="Spider-Woman" Number="9" Volume="2020" Year="2021">
 <Database Name="cv" Series="125812" Issue="828864" />
@@ -674,6 +674,27 @@
 </Book>
 <Book Series="Marauders" Number="22" Volume="2019" Year="2021">
 <Database Name="cv" Series="122218" Issue="872876" />
+</Book>
+<Book Series="Guardians of the Galaxy" Number="16" Volume="2020" Year="2021">
+<Database Name="cv" Series="124447" Issue="872882" />
+</Book>
+<Book Series="S.W.O.R.D." Number="7" Volume="2020" Year="2021">
+<Database Name="cv" Series="132511" Issue="874522" />
+</Book>
+<Book Series="Guardians of the Galaxy" Number="17" Volume="2020" Year="2021">
+<Database Name="cv" Series="124447" Issue="880922" />
+</Book>
+<Book Series="Cable: Reloaded" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="138527" Issue="882047" />
+</Book>
+<Book Series="The Last Annihilation: Wiccan &amp; Hulkling" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="138659" Issue="882901" />
+</Book>
+<Book Series="The Last Annihilation: Wakanda" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="138937" Issue="884897" />
+</Book>
+<Book Series="Guardians of the Galaxy" Number="18" Volume="2020" Year="2021">
+<Database Name="cv" Series="124447" Issue="885611" />
 </Book>
 <Book Series="Silk" Number="1" Volume="2021" Year="2021">
 <Database Name="cv" Series="135005" Issue="840957" />
@@ -1559,9 +1580,6 @@
 </Book>
 <Book Series="S.W.O.R.D." Number="11" Volume="2020" Year="2022">
 <Database Name="cv" Series="132511" Issue="899166" />
-</Book>
-<Book Series="The Secret X-Men" Number="1" Volume="2022" Year="2022">
-<Database Name="cv" Series="141588" Issue="906626" />
 </Book>
 <Book Series="X-Men" Number="4" Volume="2021" Year="2021">
 <Database Name="cv" Series="137402" Issue="889466" />
