1f9f840c798c1b240e6fe312f3daa19d10264505
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2026-02-07 19:22:01 +0000

    Create [Marvel] Blade no Events (WEB-CBRO).cbl
    https://comicbookreadingorders.com/marvel/characters/blade-reading-order/ January updates and rename

---
+++ Marvel/Characters/Blade/[Marvel] Blade no Events (WEB-CBRO).cbl
@@ -0,0 +1,356 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[Marvel] Blade no Events (WEB-CBRO)</Name>
+<NumIssues>116</NumIssues>
+<Books>
+<Book Series="Tomb of Dracula" Number="10" Volume="1972" Year="1973">
+<Database Name="cv" Series="2582" Issue="13404" />
+</Book>
+<Book Series="Blade" Number="1" Volume="2002" Year="2002">
+<Database Name="cv" Series="21836" Issue="131645" />
+</Book>
+<Book Series="Blade" Number="2" Volume="2002" Year="2002">
+<Database Name="cv" Series="21836" Issue="131707" />
+</Book>
+<Book Series="Blade" Number="3" Volume="2002" Year="2002">
+<Database Name="cv" Series="21836" Issue="131708" />
+</Book>
+<Book Series="Blade" Number="4" Volume="2002" Year="2002">
+<Database Name="cv" Series="21836" Issue="131709" />
+</Book>
+<Book Series="Blade" Number="5" Volume="2002" Year="2002">
+<Database Name="cv" Series="21836" Issue="131710" />
+</Book>
+<Book Series="Blade" Number="6" Volume="2002" Year="2002">
+<Database Name="cv" Series="21836" Issue="131711" />
+</Book>
+<Book Series="Blade" Number="1" Volume="2006" Year="2006">
+<Database Name="cv" Series="18117" Issue="108513" />
+</Book>
+<Book Series="Blade" Number="2" Volume="2006" Year="2006">
+<Database Name="cv" Series="18117" Issue="108522" />
+</Book>
+<Book Series="Blade" Number="3" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="108521" />
+</Book>
+<Book Series="Blade" Number="4" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="106153" />
+</Book>
+<Book Series="Blade" Number="5" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="108514" />
+</Book>
+<Book Series="Blade" Number="6" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="108515" />
+</Book>
+<Book Series="Blade" Number="7" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="108517" />
+</Book>
+<Book Series="Blade" Number="8" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="108518" />
+</Book>
+<Book Series="Blade" Number="9" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="111354" />
+</Book>
+<Book Series="Blade" Number="10" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="110490" />
+</Book>
+<Book Series="Blade" Number="11" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="111635" />
+</Book>
+<Book Series="Blade" Number="12" Volume="2006" Year="2007">
+<Database Name="cv" Series="18117" Issue="114137" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="5" Volume="2008" Year="2008">
+<Database Name="cv" Series="21551" Issue="139070" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="6" Volume="2008" Year="2008">
+<Database Name="cv" Series="21551" Issue="140447" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="7" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="142006" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="8" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="145549" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="9" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="150033" />
+</Book>
+<Book Series="Captain Britain and MI:13 Annual" Number="1" Volume="2009" Year="2009">
+<Database Name="cv" Series="26621" Issue="159281" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="10" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="151830" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="11" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="153460" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="12" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="155336" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="13" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="157306" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="14" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="160449" />
+</Book>
+<Book Series="Captain Britain and MI:13" Number="15" Volume="2008" Year="2009">
+<Database Name="cv" Series="21551" Issue="164827" />
+</Book>
+<Book Series="Mighty Avengers" Number="1" Volume="2013" Year="2013">
+<Database Name="cv" Series="67223" Issue="425032" />
+</Book>
+<Book Series="Mighty Avengers" Number="2" Volume="2013" Year="2013">
+<Database Name="cv" Series="67223" Issue="427692" />
+</Book>
+<Book Series="Mighty Avengers" Number="3" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="432335" />
+</Book>
+<Book Series="Mighty Avengers" Number="4" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="436204" />
+</Book>
+<Book Series="Mighty Avengers" Number="5" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="442926" />
+</Book>
+<Book Series="Mighty Avengers" Number="6" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="444533" />
+</Book>
+<Book Series="Mighty Avengers" Number="7" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="446486" />
+</Book>
+<Book Series="Mighty Avengers" Number="8" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="447512" />
+</Book>
+<Book Series="Mighty Avengers" Number="9" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="450078" />
+</Book>
+<Book Series="Mighty Avengers" Number="10" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="454104" />
+</Book>
+<Book Series="Mighty Avengers" Number="11" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="456025" />
+</Book>
+<Book Series="Mighty Avengers" Number="12" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="460314" />
+</Book>
+<Book Series="Mighty Avengers" Number="13" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="462894" />
+</Book>
+<Book Series="Mighty Avengers" Number="14" Volume="2013" Year="2014">
+<Database Name="cv" Series="67223" Issue="466331" />
+</Book>
+<Book Series="Spirits of Vengeance" Number="1" Volume="2017" Year="2017">
+<Database Name="cv" Series="104818" Issue="626297" />
+</Book>
+<Book Series="Spirits of Vengeance" Number="2" Volume="2017" Year="2018">
+<Database Name="cv" Series="104818" Issue="636385" />
+</Book>
+<Book Series="Spirits of Vengeance" Number="3" Volume="2017" Year="2018">
+<Database Name="cv" Series="104818" Issue="644514" />
+</Book>
+<Book Series="Spirits of Vengeance" Number="4" Volume="2017" Year="2018">
+<Database Name="cv" Series="104818" Issue="654061" />
+</Book>
+<Book Series="Spirits of Vengeance" Number="5" Volume="2017" Year="2018">
+<Database Name="cv" Series="104818" Issue="658733" />
+</Book>
+<Book Series="Doctor Strange: Damnation" Number="1" Volume="2018" Year="2018">
+<Database Name="cv" Series="108809" Issue="660660" />
+</Book>
+<Book Series="Doctor Strange: Damnation" Number="2" Volume="2018" Year="2018">
+<Database Name="cv" Series="108809" Issue="662087" />
+</Book>
+<Book Series="Doctor Strange: Damnation" Number="3" Volume="2018" Year="2018">
+<Database Name="cv" Series="108809" Issue="663570" />
+</Book>
+<Book Series="Doctor Strange: Damnation" Number="4" Volume="2018" Year="2018">
+<Database Name="cv" Series="108809" Issue="667639" />
+</Book>
+<Book Series="Avengers" Number="12" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="696939" />
+</Book>
+<Book Series="Avengers" Number="14" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="700132" />
+</Book>
+<Book Series="Avengers" Number="15" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="701300" />
+</Book>
+<Book Series="Avengers" Number="16" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="702454" />
+</Book>
+<Book Series="Avengers" Number="17" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="703933" />
+</Book>
+<Book Series="Avengers" Number="21" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="713499" />
+</Book>
+<Book Series="Wolverine vs. Blade" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="120144" Issue="713527" />
+</Book>
+<Book Series="Avengers" Number="22" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="714654" />
+</Book>
+<Book Series="Avengers" Number="23" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="717513" />
+</Book>
+<Book Series="Avengers" Number="24" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="720157" />
+</Book>
+<Book Series="Avengers" Number="25" Volume="2018" Year="2019">
+<Database Name="cv" Series="110496" Issue="723852" />
+</Book>
+<Book Series="Avengers" Number="26" Volume="2018" Year="2020">
+<Database Name="cv" Series="110496" Issue="728415" />
+</Book>
+<Book Series="Avengers" Number="27" Volume="2018" Year="2020">
+<Database Name="cv" Series="110496" Issue="728969" />
+</Book>
+<Book Series="Avengers" Number="28" Volume="2018" Year="2020">
+<Database Name="cv" Series="110496" Issue="731505" />
+</Book>
+<Book Series="Avengers" Number="29" Volume="2018" Year="2020">
+<Database Name="cv" Series="110496" Issue="733821" />
+</Book>
+<Book Series="Avengers" Number="30" Volume="2018" Year="2020">
+<Database Name="cv" Series="110496" Issue="735514" />
+</Book>
+<Book Series="Strikeforce" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="121584" Issue="720175" />
+</Book>
+<Book Series="Strikeforce" Number="2" Volume="2019" Year="2019">
+<Database Name="cv" Series="121584" Issue="723866" />
+</Book>
+<Book Series="Strikeforce" Number="3" Volume="2019" Year="2020">
+<Database Name="cv" Series="121584" Issue="728431" />
+</Book>
+<Book Series="Strikeforce" Number="4" Volume="2019" Year="2020">
+<Database Name="cv" Series="121584" Issue="730348" />
+</Book>
+<Book Series="Strikeforce" Number="5" Volume="2019" Year="2020">
+<Database Name="cv" Series="121584" Issue="732892" />
+</Book>
+<Book Series="Strikeforce" Number="6" Volume="2019" Year="2020">
+<Database Name="cv" Series="121584" Issue="736499" />
+</Book>
+<Book Series="Strikeforce" Number="7" Volume="2019" Year="2020">
+<Database Name="cv" Series="121584" Issue="739568" />
+</Book>
+<Book Series="Strikeforce" Number="8" Volume="2019" Year="2020">
+<Database Name="cv" Series="121584" Issue="779011" />
+</Book>
+<Book Series="Strikeforce" Number="9" Volume="2019" Year="2020">
+<Database Name="cv" Series="121584" Issue="787356" />
+</Book>
+<Book Series="Heroes Reborn" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="135903" Issue="847511" />
+</Book>
+<Book Series="Heroes Return" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="137049" Issue="863929" />
+</Book>
+<Book Series="Darkhold Alpha" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="139292" Issue="886962" />
+</Book>
+<Book Series="The Darkhold: Blade" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="139835" Issue="891571" />
+</Book>
+<Book Series="The Darkhold Omega" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="140788" Issue="901413" />
+</Book>
+<Book Series="Death of Doctor Strange: Blade" Number="1" Volume="2021" Year="2022">
+<Database Name="cv" Series="140357" Issue="897359" />
+</Book>
+<Book Series="All-Out Avengers" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="144974" Issue="946192" />
+</Book>
+<Book Series="All-Out Avengers" Number="2" Volume="2022" Year="2022">
+<Database Name="cv" Series="144974" Issue="950788" />
+</Book>
+<Book Series="All-Out Avengers" Number="3" Volume="2022" Year="2023">
+<Database Name="cv" Series="144974" Issue="956852" />
+</Book>
+<Book Series="All-Out Avengers" Number="4" Volume="2022" Year="2023">
+<Database Name="cv" Series="144974" Issue="961935" />
+</Book>
+<Book Series="All-Out Avengers" Number="5" Volume="2022" Year="2023">
+<Database Name="cv" Series="144974" Issue="966420" />
+</Book>
+<Book Series="Midnight Suns" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="145040" Issue="946707" />
+</Book>
+<Book Series="Midnight Suns" Number="2" Volume="2022" Year="2022">
+<Database Name="cv" Series="145040" Issue="951651" />
+</Book>
+<Book Series="Midnight Suns" Number="3" Volume="2022" Year="2023">
+<Database Name="cv" Series="145040" Issue="956796" />
+</Book>
+<Book Series="Midnight Suns" Number="4" Volume="2022" Year="2023">
+<Database Name="cv" Series="145040" Issue="961941" />
+</Book>
+<Book Series="Midnight Suns" Number="5" Volume="2022" Year="2023">
+<Database Name="cv" Series="145040" Issue="966423" />
+</Book>
+<Book Series="Blade: Vampire Nation" Number="1" Volume="2022" Year="2023">
+<Database Name="cv" Series="146110" Issue="955111" />
+</Book>
+<Book Series="Bloodline: Daughter of Blade" Number="1" Volume="2023" Year="2023">
+<Database Name="cv" Series="147906" Issue="967789" />
+</Book>
+<Book Series="Bloodline: Daughter of Blade" Number="2" Volume="2023" Year="2023">
+<Database Name="cv" Series="147906" Issue="975704" />
+</Book>
+<Book Series="Bloodline: Daughter of Blade" Number="3" Volume="2023" Year="2023">
+<Database Name="cv" Series="147906" Issue="982350" />
+</Book>
+<Book Series="Bloodline: Daughter of Blade" Number="4" Volume="2023" Year="2023">
+<Database Name="cv" Series="147906" Issue="989655" />
+</Book>
+<Book Series="Bloodline: Daughter of Blade" Number="5" Volume="2023" Year="2023">
+<Database Name="cv" Series="147906" Issue="996356" />
+</Book>
+<Book Series="Blade" Number="1" Volume="2023" Year="2023">
+<Database Name="cv" Series="152317" Issue="1002169" />
+</Book>
+<Book Series="Blade" Number="2" Volume="2023" Year="2023">
+<Database Name="cv" Series="152317" Issue="1010937" />
+</Book>
+<Book Series="Blade" Number="3" Volume="2023" Year="2023">
+<Database Name="cv" Series="152317" Issue="1018018" />
+</Book>
+<Book Series="Blade" Number="4" Volume="2023" Year="2023">
+<Database Name="cv" Series="152317" Issue="1024042" />
+</Book>
+<Book Series="Blade" Number="5" Volume="2023" Year="2024">
+<Database Name="cv" Series="152317" Issue="1029677" />
+</Book>
+<Book Series="Blade" Number="6" Volume="2023" Year="2024">
+<Database Name="cv" Series="152317" Issue="1034333" />
+</Book>
+<Book Series="Blade" Number="7" Volume="2023" Year="2024">
+<Database Name="cv" Series="152317" Issue="1039576" />
+</Book>
+<Book Series="Blade" Number="8" Volume="2023" Year="2024">
+<Database Name="cv" Series="152317" Issue="1044776" />
+</Book>
+<Book Series="Blade" Number="9" Volume="2023" Year="2024">
+<Database Name="cv" Series="152317" Issue="1048664" />
+</Book>
+<Book Series="Blade" Number="10" Volume="2023" Year="2024">
+<Database Name="cv" Series="152317" Issue="1052927" />
+</Book>
+<Book Series="Blade: Red Band" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="160312" Issue="1073265" />
+</Book>
+<Book Series="Blade: Red Band" Number="2" Volume="2024" Year="2025">
+<Database Name="cv" Series="160312" Issue="1079274" />
+</Book>
+<Book Series="Blade: Red Band" Number="3" Volume="2024" Year="2025">
+<Database Name="cv" Series="160312" Issue="1086197" />
+</Book>
+<Book Series="Blade: Red Band" Number="4" Volume="2024" Year="2025">
+<Database Name="cv" Series="160312" Issue="1091252" />
+</Book>
+<Book Series="Blade: Red Band" Number="5" Volume="2024" Year="2025">
+<Database Name="cv" Series="160312" Issue="1096990" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
