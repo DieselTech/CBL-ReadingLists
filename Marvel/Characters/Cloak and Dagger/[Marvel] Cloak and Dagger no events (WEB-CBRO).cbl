4767479f18fe693cf485d4fdf7aa9883b0ffb255
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2026-02-09 13:37:03 +0000

    Create [Marvel] Cloak and Dagger no events (WEB-CBRO).cbl

---
+++ Marvel/Characters/Cloak and Dagger/[Marvel] Cloak and Dagger no events (WEB-CBRO).cbl
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[Marvel] Cloak and Dagger no events (WEB-CBRO)</Name>
+<NumIssues>62</NumIssues>
+<Books>
+<Book Series="The Spectacular Spider-Man" Number="64" Volume="1976" Year="1982">
+<Database Name="cv" Series="2870" Issue="22024" />
+</Book>
+<Book Series="The Spectacular Spider-Man" Number="69" Volume="1976" Year="1982">
+<Database Name="cv" Series="2870" Issue="22389" />
+</Book>
+<Book Series="The Spectacular Spider-Man" Number="70" Volume="1976" Year="1982">
+<Database Name="cv" Series="2870" Issue="22459" />
+</Book>
+<Book Series="The Spectacular Spider-Man" Number="81" Volume="1976" Year="1983">
+<Database Name="cv" Series="2870" Issue="23383" />
+</Book>
+<Book Series="The Spectacular Spider-Man" Number="82" Volume="1976" Year="1983">
+<Database Name="cv" Series="2870" Issue="23465" />
+</Book>
+<Book Series="Cloak and Dagger" Number="1" Volume="1983" Year="1983">
+<Database Name="cv" Series="3219" Issue="23534" />
+</Book>
+<Book Series="Cloak and Dagger" Number="2" Volume="1983" Year="1983">
+<Database Name="cv" Series="3219" Issue="23617" />
+</Book>
+<Book Series="Cloak and Dagger" Number="3" Volume="1983" Year="1983">
+<Database Name="cv" Series="3219" Issue="23718" />
+</Book>
+<Book Series="Cloak and Dagger" Number="4" Volume="1983" Year="1984">
+<Database Name="cv" Series="3219" Issue="23865" />
+</Book>
+<Book Series="The Spectacular Spider-Man" Number="94" Volume="1976" Year="1984">
+<Database Name="cv" Series="2870" Issue="24704" />
+</Book>
+<Book Series="The Spectacular Spider-Man" Number="95" Volume="1976" Year="1984">
+<Database Name="cv" Series="2870" Issue="24803" />
+</Book>
+<Book Series="The Spectacular Spider-Man" Number="96" Volume="1976" Year="1984">
+<Database Name="cv" Series="2870" Issue="24899" />
+</Book>
+<Book Series="Cloak and Dagger" Number="1" Volume="1985" Year="1985">
+<Database Name="cv" Series="3475" Issue="25678" />
+</Book>
+<Book Series="Cloak and Dagger" Number="2" Volume="1985" Year="1985">
+<Database Name="cv" Series="3475" Issue="25871" />
+</Book>
+<Book Series="Cloak and Dagger" Number="3" Volume="1985" Year="1985">
+<Database Name="cv" Series="3475" Issue="26076" />
+</Book>
+<Book Series="Cloak and Dagger" Number="4" Volume="1985" Year="1986">
+<Database Name="cv" Series="3475" Issue="26392" />
+</Book>
+<Book Series="Cloak and Dagger" Number="5" Volume="1985" Year="1986">
+<Database Name="cv" Series="3475" Issue="26596" />
+</Book>
+<Book Series="Cloak and Dagger" Number="6" Volume="1985" Year="1986">
+<Database Name="cv" Series="3475" Issue="26788" />
+</Book>
+<Book Series="Cloak and Dagger" Number="7" Volume="1985" Year="1986">
+<Database Name="cv" Series="3475" Issue="26986" />
+</Book>
+<Book Series="Cloak and Dagger" Number="8" Volume="1985" Year="1986">
+<Database Name="cv" Series="3475" Issue="27178" />
+</Book>
+<Book Series="Cloak and Dagger" Number="9" Volume="1985" Year="1986">
+<Database Name="cv" Series="3475" Issue="27379" />
+</Book>
+<Book Series="Cloak and Dagger" Number="10" Volume="1985" Year="1987">
+<Database Name="cv" Series="3475" Issue="27674" />
+</Book>
+<Book Series="Cloak and Dagger" Number="11" Volume="1985" Year="1987">
+<Database Name="cv" Series="3475" Issue="27896" />
+</Book>
+<Book Series="Marvel Graphic Novel" Number="34" Volume="1982" Year="1988">
+<Database Name="cv" Series="3144" Issue="134238" />
+</Book>
+<Book Series="Runaways" Number="11" Volume="2003" Year="2004">
+<Database Name="cv" Series="17988" Issue="106049" />
+</Book>
+<Book Series="Runaways" Number="12" Volume="2003" Year="2004">
+<Database Name="cv" Series="17988" Issue="106050" />
+</Book>
+<Book Series="Runaways" Number="9" Volume="2005" Year="2005">
+<Database Name="cv" Series="11872" Issue="104257" />
+</Book>
+<Book Series="Runaways" Number="10" Volume="2005" Year="2006">
+<Database Name="cv" Series="11872" Issue="106059" />
+</Book>
+<Book Series="Runaways" Number="11" Volume="2005" Year="2006">
+<Database Name="cv" Series="11872" Issue="106060" />
+</Book>
+<Book Series="Runaways" Number="12" Volume="2005" Year="2006">
+<Database Name="cv" Series="11872" Issue="106061" />
+</Book>
+<Book Series="The Amazing Spider-Man" Number="667" Volume="1963" Year="2011">
+<Database Name="cv" Series="2127" Issue="285544" />
+</Book>
+<Book Series="Spider-Island: Cloak &amp; Dagger" Number="1" Volume="2011" Year="2011">
+<Database Name="cv" Series="41955" Issue="285497" />
+</Book>
+<Book Series="Spider-Island: Cloak &amp; Dagger" Number="2" Volume="2011" Year="2011">
+<Database Name="cv" Series="41955" Issue="293399" />
+</Book>
+<Book Series="Spider-Island: Cloak &amp; Dagger" Number="3" Volume="2011" Year="2011">
+<Database Name="cv" Series="41955" Issue="299872" />
+</Book>
+<Book Series="The Amazing Spider-Man" Number="6" Volume="2015" Year="2016">
+<Database Name="cv" Series="85076" Issue="509696" />
+</Book>
+<Book Series="The Amazing Spider-Man" Number="7" Volume="2015" Year="2016">
+<Database Name="cv" Series="85076" Issue="513638" />
+</Book>
+<Book Series="The Amazing Spider-Man" Number="8" Volume="2015" Year="2016">
+<Database Name="cv" Series="85076" Issue="516076" />
+</Book>
+<Book Series="Cloak and Dagger" Number="1" Volume="2018" Year="2018">
+<Database Name="cv" Series="111450" Issue="672280" />
+</Book>
+<Book Series="Cloak and Dagger" Number="2" Volume="2018" Year="2018">
+<Database Name="cv" Series="111450" Issue="675973" />
+</Book>
+<Book Series="Cloak and Dagger" Number="3" Volume="2018" Year="2018">
+<Database Name="cv" Series="111450" Issue="678529" />
+</Book>
+<Book Series="Cloak and Dagger: Negative Exposure" Number="1" Volume="2018" Year="2019">
+<Database Name="cv" Series="116202" Issue="696101" />
+</Book>
+<Book Series="Cloak and Dagger: Negative Exposure" Number="2" Volume="2018" Year="2019">
+<Database Name="cv" Series="116202" Issue="698599" />
+</Book>
+<Book Series="Cloak and Dagger: Negative Exposure" Number="3" Volume="2018" Year="2019">
+<Database Name="cv" Series="116202" Issue="701933" />
+</Book>
+<Book Series="Absolute Carnage: Lethal Protectors" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="120945" Issue="717509" />
+</Book>
+<Book Series="Absolute Carnage: Lethal Protectors" Number="2" Volume="2019" Year="2019">
+<Database Name="cv" Series="120945" Issue="719431" />
+</Book>
+<Book Series="Absolute Carnage: Lethal Protectors" Number="3" Volume="2019" Year="2019">
+<Database Name="cv" Series="120945" Issue="723847" />
+</Book>
+<Book Series="Savage Avengers" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="143014" Issue="924796" />
+</Book>
+<Book Series="Savage Avengers" Number="2" Volume="2022" Year="2022">
+<Database Name="cv" Series="143014" Issue="929650" />
+</Book>
+<Book Series="Savage Avengers" Number="3" Volume="2022" Year="2022">
+<Database Name="cv" Series="143014" Issue="935749" />
+</Book>
+<Book Series="Savage Avengers" Number="4" Volume="2022" Year="2022">
+<Database Name="cv" Series="143014" Issue="942711" />
+</Book>
+<Book Series="Savage Avengers" Number="5" Volume="2022" Year="2022">
+<Database Name="cv" Series="143014" Issue="946719" />
+</Book>
+<Book Series="Savage Avengers" Number="6" Volume="2022" Year="2022">
+<Database Name="cv" Series="143014" Issue="949642" />
+</Book>
+<Book Series="Savage Avengers" Number="7" Volume="2022" Year="2023">
+<Database Name="cv" Series="143014" Issue="953026" />
+</Book>
+<Book Series="Savage Avengers" Number="8" Volume="2022" Year="2023">
+<Database Name="cv" Series="143014" Issue="960020" />
+</Book>
+<Book Series="Savage Avengers" Number="9" Volume="2022" Year="2023">
+<Database Name="cv" Series="143014" Issue="963993" />
+</Book>
+<Book Series="Savage Avengers" Number="10" Volume="2022" Year="2023">
+<Database Name="cv" Series="143014" Issue="973026" />
+</Book>
+<Book Series="Luke Cage: Gang War" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="154931" Issue="1031691" />
+</Book>
+<Book Series="Luke Cage: Gang War" Number="2" Volume="2024" Year="2024">
+<Database Name="cv" Series="154931" Issue="1034340" />
+</Book>
+<Book Series="Luke Cage: Gang War" Number="3" Volume="2024" Year="2024">
+<Database Name="cv" Series="154931" Issue="1039580" />
+</Book>
+<Book Series="Luke Cage: Gang War" Number="4" Volume="2024" Year="2024">
+<Database Name="cv" Series="154931" Issue="1045763" />
+</Book>
+<Book Series="Blood Hunters" Number="4" Volume="2024" Year="2025">
+<Database Name="cv" Series="159187" Issue="1076590" />
+</Book>
+<Book Series="Blood Hunters" Number="5" Volume="2024" Year="2025">
+<Database Name="cv" Series="159187" Issue="1083721" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
