d3441ba758739c9ed389547e4be735cda8774e27
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2026-02-14 16:53:28 +0000

    Update [Marvel] Daredevil no Events (WEB-CBRO).cbl

--- Marvel/Characters/Daredevil/[Marvel] Daredevil no Events (WEB-CBRO).cbl
+++ Marvel/Characters/Daredevil/[Marvel] Daredevil no Events (WEB-CBRO).cbl
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-<Name>[Marvel] Daredevil (WEB-CBRO) no Events</Name>
+<Name>[Marvel] Daredevil no Events (WEB-CBRO)</Name>
 <NumIssues>442</NumIssues>
 <Books>
 <Book Series="Daredevil: The Man Without Fear" Number="1" Volume="1993" Year="1993">
