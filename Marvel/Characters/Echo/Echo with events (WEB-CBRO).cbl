ca2c1316e79e245f6f04c3e07d38e952c9dff0b8
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2026-02-14 17:04:27 +0000

    Update Echo with events (WEB-CBRO).cbl

--- Marvel/Characters/Echo/Echo with events (WEB-CBRO).cbl
+++ Marvel/Characters/Echo/Echo with events (WEB-CBRO).cbl
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-<Name>Echo with events (WEB-CBRO)</Name>
+<Name>[Marvel] Echo with events (WEB-CBRO)</Name>
 <NumIssues>102</NumIssues>
 <Books>
 <Book Series="Daredevil" Number="9" Volume="1998" Year="1999">
