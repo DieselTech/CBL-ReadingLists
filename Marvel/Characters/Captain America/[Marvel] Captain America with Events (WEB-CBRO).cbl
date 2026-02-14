354f11c403b2397ae2a4b630610ed05d1b73629c
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2026-02-14 16:48:57 +0000

    Update [Marvel] Captain America with Events (WEB-CBRO).cbl

--- Marvel/Characters/Captain America/[Marvel] Captain America with Events (WEB-CBRO).cbl
+++ Marvel/Characters/Captain America/[Marvel] Captain America with Events (WEB-CBRO).cbl
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-<Name>Captain America with Events (WEB-CBRO)</Name>
+<Name>[Marvel] Captain America with Events (WEB-CBRO)</Name>
 <NumIssues>1111</NumIssues>
 <Books>
 <Book Series="Captain America" Number="255" Volume="1968" Year="1981">
