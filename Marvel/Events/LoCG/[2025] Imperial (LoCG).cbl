a5c8a78e3cdc63f9817ba8f396feebbeea8eea5b
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2026-03-05 09:18:57 +0000

    Add files via upload

---
+++ Marvel/Events/LoCG/[2025] Imperial (LoCG).cbl
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[2025] Imperial (LoCG)</Name>
+<NumIssues>10</NumIssues>
+<Books>
+<Book Series="Imperial" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="164848" Issue="1114015" />
+</Book>
+<Book Series="Imperial" Number="2" Volume="2025" Year="2025">
+<Database Name="cv" Series="164848" Issue="1120406" />
+</Book>
+<Book Series="Imperial" Number="3" Volume="2025" Year="2025">
+<Database Name="cv" Series="164848" Issue="1127210" />
+</Book>
+<Book Series="Imperial War: Black Panther" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="166450" Issue="1128920" />
+</Book>
+<Book Series="Imperial War: Planet She-Hulk" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="166451" Issue="1128921" />
+</Book>
+<Book Series="Imperial War: Exiles" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="166645" Issue="1130249" />
+</Book>
+<Book Series="Imperial War: Nova - Centurion" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="166900" Issue="1132001" />
+</Book>
+<Book Series="The Amazing Spider-Man" Number="13" Volume="2025" Year="2025">
+<Database Name="cv" Series="163325" Issue="1136076" />
+</Book>
+<Book Series="Imperial War: Imperial Guardians" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="167502" Issue="1137155" />
+</Book>
+<Book Series="Imperial" Number="4" Volume="2025" Year="2025">
+<Database Name="cv" Series="164848" Issue="1140611" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
