1d60ab1f3731ce33fe1df0d0332e12025da4d245
Matt Pickle <matthew.e.pickle@gmail.com>
2026-04-10 00:47:54 -0400

    update event Wolverine goes to hell locations

--- Marvel/Master Reading Order/CBRO - With Events/[Marvel] Marvel Master Reading Order Part #09 (WEB-CBRO).cbl
+++ Marvel/Master Reading Order/CBRO - With Events/[Marvel] Marvel Master Reading Order Part #09 (WEB-CBRO).cbl
@@ -3882,38 +3882,38 @@
 <Book Series="Wolverine: The Road to Hell" Number="1" Volume="2010" Year="2010">
 <Database Name="cv" Series="35279" Issue="232620" />
 </Book>
-<Book Series="Wolverine" Number="1" Volume="2010" Year="2010">
-<Database Name="cv" Series="35263" Issue="232524" />
-</Book>
 <Book Series="Daken: Dark Wolverine" Number="1" Volume="2010" Year="2010">
 <Database Name="cv" Series="35409" Issue="233620" />
-</Book>
-<Book Series="X-23" Number="1" Volume="2010" Year="2010">
-<Database Name="cv" Series="35496" Issue="234641" />
-</Book>
-<Book Series="Wolverine" Number="2" Volume="2010" Year="2010">
-<Database Name="cv" Series="35263" Issue="237113" />
 </Book>
 <Book Series="Daken: Dark Wolverine" Number="2" Volume="2010" Year="2010">
 <Database Name="cv" Series="35409" Issue="238326" />
 </Book>
-<Book Series="X-23" Number="2" Volume="2010" Year="2010">
-<Database Name="cv" Series="35496" Issue="239137" />
-</Book>
-<Book Series="Wolverine" Number="3" Volume="2010" Year="2011">
-<Database Name="cv" Series="35263" Issue="240986" />
-</Book>
 <Book Series="Daken: Dark Wolverine" Number="3" Volume="2010" Year="2011">
 <Database Name="cv" Series="35409" Issue="244790" />
+</Book>
+<Book Series="Wolverine" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="35263" Issue="232524" />
+</Book>
+<Book Series="Wolverine" Number="2" Volume="2010" Year="2010">
+<Database Name="cv" Series="35263" Issue="237113" />
+</Book>
+<Book Series="X-23" Number="1" Volume="2010" Year="2010">
+<Database Name="cv" Series="35496" Issue="234641" />
+</Book>
+<Book Series="X-23" Number="2" Volume="2010" Year="2010">
+<Database Name="cv" Series="35496" Issue="239137" />
 </Book>
 <Book Series="X-23" Number="3" Volume="2010" Year="2011">
 <Database Name="cv" Series="35496" Issue="244789" />
 </Book>
-<Book Series="Wolverine" Number="4" Volume="2010" Year="2011">
-<Database Name="cv" Series="35263" Issue="249226" />
-</Book>
 <Book Series="Wolverine" Number="5" Volume="2010" Year="2011">
 <Database Name="cv" Series="35263" Issue="258414" />
+</Book>
+<Book Series="Wolverine" Number="3" Volume="2010" Year="2011">
+<Database Name="cv" Series="35263" Issue="240986" />
+</Book>
+<Book Series="Wolverine" Number="4" Volume="2010" Year="2011">
+<Database Name="cv" Series="35263" Issue="249226" />
 </Book>
 <Book Series="Wolverine" Number="5.1" Volume="2010" Year="2011">
 <Database Name="cv" Series="35263" Issue="261260" />
