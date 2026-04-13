34de81ed67cd1b79f18f5b8cca4f92c13d754141
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2026-04-13 13:18:10 +0100

    1

---
+++ Boom/[Boom!] Slaughterverse (WEB-LOCG).cbl
@@ -0,0 +1,269 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>[Boom!] Slaughterverse (WEB-LOCG)</Name>
+<NumIssues>87</NumIssues>
+<Books>
+<Book Series="Something is Killing the Children" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="121102" Issue="718048" />
+</Book>
+<Book Series="Something is Killing the Children" Number="2" Volume="2019" Year="2019">
+<Database Name="cv" Series="121102" Issue="723092" />
+</Book>
+<Book Series="Something is Killing the Children" Number="3" Volume="2019" Year="2019">
+<Database Name="cv" Series="121102" Issue="728396" />
+</Book>
+<Book Series="Something is Killing the Children" Number="4" Volume="2019" Year="2019">
+<Database Name="cv" Series="121102" Issue="730301" />
+</Book>
+<Book Series="Something is Killing the Children" Number="5" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="735309" />
+</Book>
+<Book Series="Something is Killing the Children" Number="6" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="742042" />
+</Book>
+<Book Series="Something is Killing the Children" Number="7" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="767802" />
+</Book>
+<Book Series="Something is Killing the Children" Number="8" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="775052" />
+</Book>
+<Book Series="Something is Killing the Children" Number="9" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="791899" />
+</Book>
+<Book Series="Something is Killing the Children" Number="10" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="797936" />
+</Book>
+<Book Series="Something is Killing the Children" Number="11" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="813155" />
+</Book>
+<Book Series="Something is Killing the Children" Number="12" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="818997" />
+</Book>
+<Book Series="Something is Killing the Children" Number="13" Volume="2019" Year="2020">
+<Database Name="cv" Series="121102" Issue="822014" />
+</Book>
+<Book Series="Something is Killing the Children" Number="14" Volume="2019" Year="2021">
+<Database Name="cv" Series="121102" Issue="826513" />
+</Book>
+<Book Series="Something is Killing the Children" Number="15" Volume="2019" Year="2021">
+<Database Name="cv" Series="121102" Issue="829768" />
+</Book>
+<Book Series="Enter the House of Slaughter Free Comic Book Day 2021" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="138413" Issue="881341" />
+</Book>
+<Book Series="Something is Killing the Children" Number="16" Volume="2019" Year="2021">
+<Database Name="cv" Series="121102" Issue="856040" />
+</Book>
+<Book Series="Something is Killing the Children" Number="17" Volume="2019" Year="2021">
+<Database Name="cv" Series="121102" Issue="864418" />
+</Book>
+<Book Series="Something is Killing the Children" Number="18" Volume="2019" Year="2021">
+<Database Name="cv" Series="121102" Issue="875828" />
+</Book>
+<Book Series="Something is Killing the Children" Number="19" Volume="2019" Year="2021">
+<Database Name="cv" Series="121102" Issue="882557" />
+</Book>
+<Book Series="Something is Killing the Children" Number="20" Volume="2019" Year="2021">
+<Database Name="cv" Series="121102" Issue="885741" />
+</Book>
+<Book Series="House of Slaughter" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="139867" Issue="891733" />
+</Book>
+<Book Series="House of Slaughter" Number="2" Volume="2021" Year="2021">
+<Database Name="cv" Series="139867" Issue="895281" />
+</Book>
+<Book Series="House of Slaughter" Number="3" Volume="2021" Year="2021">
+<Database Name="cv" Series="139867" Issue="899154" />
+</Book>
+<Book Series="House of Slaughter" Number="4" Volume="2021" Year="2022">
+<Database Name="cv" Series="139867" Issue="904592" />
+</Book>
+<Book Series="House of Slaughter" Number="5" Volume="2021" Year="2022">
+<Database Name="cv" Series="139867" Issue="908358" />
+</Book>
+<Book Series="House of Slaughter" Number="6" Volume="2021" Year="2022">
+<Database Name="cv" Series="139867" Issue="924791" />
+</Book>
+<Book Series="House of Slaughter" Number="7" Volume="2021" Year="2022">
+<Database Name="cv" Series="139867" Issue="939177" />
+</Book>
+<Book Series="House of Slaughter" Number="8" Volume="2021" Year="2022">
+<Database Name="cv" Series="139867" Issue="944897" />
+</Book>
+<Book Series="House of Slaughter" Number="9" Volume="2021" Year="2022">
+<Database Name="cv" Series="139867" Issue="949038" />
+</Book>
+<Book Series="House of Slaughter" Number="10" Volume="2021" Year="2022">
+<Database Name="cv" Series="139867" Issue="952366" />
+</Book>
+<Book Series="Something is Killing the Children" Number="21" Volume="2019" Year="2022">
+<Database Name="cv" Series="121102" Issue="913309" />
+</Book>
+<Book Series="Something is Killing the Children" Number="22" Volume="2019" Year="2022">
+<Database Name="cv" Series="121102" Issue="919480" />
+</Book>
+<Book Series="Something is Killing the Children" Number="23" Volume="2019" Year="2022">
+<Database Name="cv" Series="121102" Issue="926649" />
+</Book>
+<Book Series="Something is Killing the Children" Number="24" Volume="2019" Year="2022">
+<Database Name="cv" Series="121102" Issue="931704" />
+</Book>
+<Book Series="Something is Killing the Children" Number="25" Volume="2019" Year="2022">
+<Database Name="cv" Series="121102" Issue="939192" />
+</Book>
+<Book Series="Something is Killing the Children" Number="26" Volume="2019" Year="2022">
+<Database Name="cv" Series="121102" Issue="955467" />
+</Book>
+<Book Series="Something is Killing the Children" Number="27" Volume="2019" Year="2022">
+<Database Name="cv" Series="121102" Issue="961208" />
+</Book>
+<Book Series="Something is Killing the Children" Number="28" Volume="2019" Year="2023">
+<Database Name="cv" Series="121102" Issue="963935" />
+</Book>
+<Book Series="Something is Killing the Children" Number="29" Volume="2019" Year="2023">
+<Database Name="cv" Series="121102" Issue="973519" />
+</Book>
+<Book Series="Something is Killing the Children" Number="30" Volume="2019" Year="2023">
+<Database Name="cv" Series="121102" Issue="979802" />
+</Book>
+<Book Series="Something is Killing the Children" Number="31" Volume="2019" Year="2023">
+<Database Name="cv" Series="121102" Issue="1002171" />
+</Book>
+<Book Series="Something is Killing the Children" Number="32" Volume="2019" Year="2023">
+<Database Name="cv" Series="121102" Issue="1009195" />
+</Book>
+<Book Series="Something is Killing the Children" Number="33" Volume="2019" Year="2023">
+<Database Name="cv" Series="121102" Issue="1016449" />
+</Book>
+<Book Series="Something is Killing the Children" Number="34" Volume="2019" Year="2023">
+<Database Name="cv" Series="121102" Issue="1026868" />
+</Book>
+<Book Series="Something is Killing the Children" Number="35" Volume="2019" Year="2023">
+<Database Name="cv" Series="121102" Issue="1032529" />
+</Book>
+<Book Series="Book of Slaughter" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="147138" Issue="962017" />
+</Book>
+<Book Series="House of Slaughter" Number="11" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="964839" />
+</Book>
+<Book Series="House of Slaughter" Number="12" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="971918" />
+</Book>
+<Book Series="House of Slaughter" Number="13" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="977221" />
+</Book>
+<Book Series="House of Slaughter" Number="14" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="984169" />
+</Book>
+<Book Series="House of Slaughter" Number="15" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="990074" />
+</Book>
+<Book Series="House of Slaughter" Number="16" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="1008781" />
+</Book>
+<Book Series="House of Slaughter" Number="17" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="1014060" />
+</Book>
+<Book Series="House of Slaughter" Number="18" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="1024705" />
+</Book>
+<Book Series="House of Slaughter" Number="19" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="1028784" />
+</Book>
+<Book Series="House of Slaughter" Number="20" Volume="2021" Year="2023">
+<Database Name="cv" Series="139867" Issue="1034579" />
+</Book>
+<Book Series="Book of Butcher" Number="1" Volume="2023" Year="2023">
+<Database Name="cv" Series="155732" Issue="1038464" />
+</Book>
+<Book Series="House of Slaughter" Number="21" Volume="2021" Year="2024">
+<Database Name="cv" Series="139867" Issue="1048192" />
+</Book>
+<Book Series="House of Slaughter" Number="22" Volume="2021" Year="2024">
+<Database Name="cv" Series="139867" Issue="1051187" />
+</Book>
+<Book Series="House of Slaughter" Number="23" Volume="2021" Year="2024">
+<Database Name="cv" Series="139867" Issue="1055633" />
+</Book>
+<Book Series="House of Slaughter" Number="24" Volume="2021" Year="2024">
+<Database Name="cv" Series="139867" Issue="1060319" />
+</Book>
+<Book Series="House of Slaughter" Number="25" Volume="2021" Year="2024">
+<Database Name="cv" Series="139867" Issue="1065529" />
+</Book>
+<Book Series="Something is Killing the Children" Number="36" Volume="2019" Year="2024">
+<Database Name="cv" Series="121102" Issue="1052991" />
+</Book>
+<Book Series="Something is Killing the Children" Number="37" Volume="2019" Year="2024">
+<Database Name="cv" Series="121102" Issue="1056395" />
+</Book>
+<Book Series="Something is Killing the Children" Number="38" Volume="2019" Year="2024">
+<Database Name="cv" Series="121102" Issue="1061050" />
+</Book>
+<Book Series="Something is Killing the Children" Number="39" Volume="2019" Year="2024">
+<Database Name="cv" Series="121102" Issue="1064342" />
+</Book>
+<Book Series="Something is Killing the Children" Number="40" Volume="2019" Year="2024">
+<Database Name="cv" Series="121102" Issue="1068043" />
+</Book>
+<Book Series="Something is Killing the Children" Number="0" Volume="2019" Year="2024">
+<Database Name="cv" Series="121102" Issue="1076627" />
+</Book>
+<Book Series="Book of Cutter" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="162183" Issue="1092939" />
+</Book>
+<Book Series="House of Slaughter" Number="26" Volume="2021" Year="2024">
+<Database Name="cv" Series="139867" Issue="1074027" />
+</Book>
+<Book Series="House of Slaughter" Number="27" Volume="2021" Year="2024">
+<Database Name="cv" Series="139867" Issue="1078605" />
+</Book>
+<Book Series="House of Slaughter" Number="28" Volume="2021" Year="2024">
+<Database Name="cv" Series="139867" Issue="1083768" />
+</Book>
+<Book Series="House of Slaughter" Number="29" Volume="2021" Year="2025">
+<Database Name="cv" Series="139867" Issue="1091263" />
+</Book>
+<Book Series="House of Slaughter" Number="30" Volume="2021" Year="2025">
+<Database Name="cv" Series="139867" Issue="1096403" />
+</Book>
+<Book Series="Hello Darkness" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="159025" Issue="1064323" />
+</Book>
+<Book Series="Hello Darkness" Number="3" Volume="2024" Year="2024">
+<Database Name="cv" Series="159025" Issue="1071752" />
+</Book>
+<Book Series="Hello Darkness" Number="5" Volume="2024" Year="2024">
+<Database Name="cv" Series="159025" Issue="1080125" />
+</Book>
+<Book Series="Hello Darkness" Number="8" Volume="2024" Year="2025">
+<Database Name="cv" Series="159025" Issue="1097039" />
+</Book>
+<Book Series="Hello Darkness" Number="10" Volume="2024" Year="2025">
+<Database Name="cv" Series="159025" Issue="1108830" />
+</Book>
+<Book Series="Hello Darkness" Number="12" Volume="2024" Year="2025">
+<Database Name="cv" Series="159025" Issue="1119171" />
+</Book>
+<Book Series="Something is Killing the Children" Number="41" Volume="2019" Year="2025">
+<Database Name="cv" Series="121102" Issue="1107383" />
+</Book>
+<Book Series="Something is Killing the Children" Number="42" Volume="2019" Year="2025">
+<Database Name="cv" Series="121102" Issue="1115830" />
+</Book>
+<Book Series="Something is Killing the Children" Number="43" Volume="2019" Year="2025">
+<Database Name="cv" Series="121102" Issue="1124671" />
+</Book>
+<Book Series="Something is Killing the Children" Number="44" Volume="2019" Year="2025">
+<Database Name="cv" Series="121102" Issue="1136122" />
+</Book>
+<Book Series="Something is Killing the Children" Number="45" Volume="2019" Year="2025">
+<Database Name="cv" Series="121102" Issue="1146780" />
+</Book>
+<Book Series="Something is Killing the Children" Number="46" Volume="2019" Year="2026">
+<Database Name="cv" Series="121102" Issue="1162499" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
