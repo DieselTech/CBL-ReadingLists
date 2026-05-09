6ab4791d920ae40afa6abd7c5b231f422cff11b6
Fletcher8788 <149801925+Fletcher8788@users.noreply.github.com>
2026-05-09 18:21:44 +0100

    Create Bacchus.cbl

---
+++ Other/unsorted/Dark Horse/Bacchus.cbl
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Name>Bacchus</Name>
+  <NumIssues>5</NumIssues>
+  <Books>
+    <Book Series="Bacchus" Number="1" Volume="2014" Year="2014">
+      <Database Name="cv" Series="76009" Issue="460707" />
+    </Book>
+    <Book Series="Bacchus" Number="2" Volume="2014" Year="2014">
+      <Database Name="cv" Series="76009" Issue="460708" />
+    </Book>
+    <Book Series="Bacchus" Number="3" Volume="2014" Year="2014">
+      <Database Name="cv" Series="76009" Issue="460709" />
+    </Book>
+    <Book Series="Bacchus" Number="4" Volume="2014" Year="2014">
+      <Database Name="cv" Series="76009" Issue="460710" />
+    </Book>
+    <Book Series="Bacchus" Number="5" Volume="2014" Year="2014">
+      <Database Name="cv" Series="76009" Issue="460711" />
+    </Book>
+  </Books>
+  <Matchers />
+</ReadingList>
