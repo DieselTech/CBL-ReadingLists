fa9dbb267e9b3697e1fa0ac388afd697c7572812
Noonie13 <93807387+Noonie13@users.noreply.github.com>
2026-08-29 09:17:23 -0500

    Update [2015-2016] Secret Wars (Marvel Comics)(LoCG).cbl
    Fixed dumb error in first line of xml code

--- Marvel/Events/LoCG/[2015-2016] Secret Wars (Marvel Comics)(LoCG).cbl
+++ Marvel/Events/LoCG/[2015-2016] Secret Wars (Marvel Comics)(LoCG).cbl
@@ -1,4 +1,4 @@
-xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="utf-8"?>
 <ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 <Name>[2015-2016] Secret Wars (Marvel Comics)(LoCG)</Name>
 <NumIssues>229</NumIssues>
